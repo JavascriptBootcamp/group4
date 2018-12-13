2acf203dccce8c306347859372c2a7bb931cfa3a
derldalfor100 <derldalfor100@gmail.com>
2018-12-13 19:14:20 +0200

    update 3

--- modules/01 JavaScript Programming/Assignments/1/answer.js
+++ modules/01 JavaScript Programming/Assignments/1/answer.js
@@ -1,6 +1,6 @@
 var animals;
 animals = "hello";
-console.log(typeof animals)update 2;
+console.info(typeof animals);
 animals = 214;
 animals = animals + 300;
 animals = ['dog', 'cat', 'lion'];// animals array
