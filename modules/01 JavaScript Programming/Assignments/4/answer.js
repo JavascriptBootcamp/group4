2dec7e139be0525f2e73edca0b65daf39f8a150a
derldalfor100 <derldalfor100@gmail.com>
2018-12-13 22:01:11 +0200

    Ans 4 updated

--- modules/01 JavaScript Programming/Assignments/4/answer.js
+++ modules/01 JavaScript Programming/Assignments/4/answer.js
@@ -1,9 +1,8 @@
 var x, y;
 x=5;
 y=5;
-++x;
-++x;
-y++;
-y++;
-console.log(x);
-console.log(y);
+x = ++x;
+x = ++x;
+y = y++;// assign to y the previous value so it remains 5
+y = y++;
+console.log(x, y);// x = 7, y = 5
