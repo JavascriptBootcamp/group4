10fa5a72087e9f6e3212ad0545bded5054135ae0
shimonhazan <44465329+shimonhazan@users.noreply.github.com>
2018-12-16 19:47:15 +0200

    Create answer.js

---
+++ modules/01 JavaScript Programming/Assignments/6/answer.js
@@ -0,0 +1,14 @@
+var x = 1998;
+var y = 2001;
+if(x>y){
+    console.log(x + "is a larger")
+}
+else{
+    console.log(y + "is a larger")
+}
+x=(x++);
+var bool1 = false;
+if(x===y){
+    bool1=true;
+}
+console.log(bool1)
