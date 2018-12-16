b38311554f57cacf041625670d67d8ec14e4d5d6
Ofek <31457314+OfekArbeli@users.noreply.github.com>
2018-12-16 19:47:13 +0200

    Create answer.js

---
+++ modules/01 JavaScript Programming/Assignments/6/answer.js
@@ -0,0 +1,12 @@
+var year = 1998;
+var newYear = 2001;
+
+if (year > newYear)
+    console.log(year + " is bigger");
+else
+    console.log(newYear + " is bigger");
+
+year++;
+
+var areEquals = (year === newYear);
+console.log(areEquals);
