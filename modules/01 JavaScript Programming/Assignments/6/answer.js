2c40b7f1469d72e9dce598cfb8acf16342c14106
derldalfor100 <derldalfor100@gmail.com>
2018-12-16 19:41:35 +0200

    Ans 6

---
+++ modules/01 JavaScript Programming/Assignments/6/answer.js
@@ -0,0 +1,11 @@
+var v1 =1998;
+var v2 = 2001;
+if(v1 > v2)
+    console.log("The bigger one is: " + v1);
+else
+    console.log("The bigger one is: " + v2);
+v1++;
+var ans = false;
+if(v1 == v2)
+    ans = true;
+console.log(ans);
