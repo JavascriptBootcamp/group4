e768930643e39945f2e7b1ab757e7af29c478846
Ofek <31457314+OfekArbeli@users.noreply.github.com>
2018-12-16 20:41:06 +0200

    Create answer.js

---
+++ modules/01 JavaScript Programming/Assignments/7/answer.js
@@ -0,0 +1,9 @@
+var x;
+var y = undefined;
+console.log(x);
+x==y?console.log("true"):console.log("false");
+x===y?console.log("true"):console.log("false");
+
+x = "some string";
+x==y?console.log("true"):console.log("false");
+x===y?console.log("true"):console.log("false");
