080f03285f7debd09237558a9b63f28a8e72b3f5
litalrab <38513468+litalrab@users.noreply.github.com>
2018-12-27 20:35:26 +0200

    Update answer11.js

--- modules/01 JavaScript Programming/Assignments/11/answer11.js
+++ modules/01 JavaScript Programming/Assignments/11/answer11.js
@@ -2,36 +2,45 @@
 //---
 //1. Create a while loop that iterates up to 50 while logging "boom" at multiples of 7
 //2. Create a while that iterates up to 100 while logging "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
-
-for(var i=1; i<=50;++i)
+var i=1;
+while( i<=50)
 { 
     if(i%7==0)
     {
         console.log(" boom");
         
     }
+    ;++i
 }
-for(var j=1; j<=100;++j)
+ var j=1;
+while( j<=100)
 { 
     if(j%7==0)
     {
         console.log(" fizz");
         
     }
+    ++j;
 }
-for(var j=1; j<=100;++j)
+ j=1;
+while( j<=100)
 { 
     if(j%5==0)
     {
         console.log(" buzz");
         
     }
-}
-for(var j=1; j<=100;++j)
+        ++j;
+
+} 
+j=1;
+while( j<=100)
 { 
     if(j%5==0 && j%3==0 )
     {
         console.log(" fizzbuzz");
         
     }
+        ++j;
+
 }
