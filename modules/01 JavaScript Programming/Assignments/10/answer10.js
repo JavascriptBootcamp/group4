68a53e2f0e47942ee86c578ed5d4a445dc8eb8f6
gabybuchnik <front16@sqlabs.co.il>
2018-12-27 19:32:55 +0200

    answer10Js

---
+++ modules/01 JavaScript Programming/Assignments/10/answer10.js
@@ -0,0 +1,10 @@
+var number = 10;
+do{
+    if(number % 2 === 0){
+        console.log("number",number, "is even");
+    }
+    else{
+        console.log("number",number, "is odd");
+    }
+    number++;
+}while(number>=10 && number<=20)
