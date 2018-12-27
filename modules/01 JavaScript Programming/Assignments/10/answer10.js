b54210176174ffa43b18ccaa4a85eb4b00ffa046
litalrab <38513468+litalrab@users.noreply.github.com>
2018-12-27 19:53:15 +0200

    answer10.js

---
+++ modules/01 JavaScript Programming/Assignments/10/answer10.js
@@ -0,0 +1,14 @@
+//Create a loop that prints "even or odd?" and then iterates from 10 to 20.
+// For each iteration, it will check if the current number is even or odd
+//, and report that to the screen (e.g. "2 is even")
+
+var numbers=[10,11,12,13,14,15,16,17,18,19,20]
+numbers.forEach
+numbers.forEach(function(num){
+    if(num%2==0)
+    {
+        console.log(num +"  is even");
+        
+    }
+    
+});
