b8e5e356d49553080fb3a8a2adc6d8a1838b25e3
AvrahamAdnahani <AvrahamAdnahani@gmail.com>
2018-12-27 19:36:00 +0200

    answer9

---
+++ modules/01 JavaScript Programming/Assignments/9/nestedLoop.js
@@ -0,0 +1,11 @@
+var star = '';
+for(var i=0;i<5;i++)
+{
+    for(var j=0;j<=i;j++)
+    {
+        star += '*';
+    }
+    console.log(star);
+    star = '';
+}
+   
---
+++ modules/01 JavaScript Programming/Assignments/9/printNames.js
@@ -0,0 +1,4 @@
+var arr = ["David", "Gabi", "Daniel", "Yossi", "Efi"];
+arr.forEach(function(element) {
+    console.log("Hello",element + "!");
+}, this);
