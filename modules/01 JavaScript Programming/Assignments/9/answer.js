1d3b7d48686ff505760daf3ce07f4b5e9ccee3e6
Ofek <31457314+OfekArbeli@users.noreply.github.com>
2018-12-27 18:34:37 +0200

    Create answer.js

---
+++ modules/01 JavaScript Programming/Assignments/9/answer.js
@@ -0,0 +1,16 @@
+for(var i =1;i<101;i++)
+{
+    console.log(i*2);
+}
+
+arr = ["David","Yoni"];
+arr.forEach(function(item){
+    console.log(`Hello ${item}!`)
+});
+
+var triangle = "";
+for(i=0;i<5;i++)
+{
+    triangle += "* "
+    console.log(triangle);
+}
