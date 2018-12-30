e13fed2d86e90e9443f9d5d2b202bbaa570901bc
litalrab <38513468+litalrab@users.noreply.github.com>
2018-12-30 22:20:16 +0200

    update

--- modules/01 JavaScript Programming/Assignments/16/answer16.js
+++ modules/01 JavaScript Programming/Assignments/16/answer16.js
@@ -7,60 +7,17 @@ var book= {
 
 
 }
-if(book.alreadyRead=== false)
+if(book.alreadyRead=== true)
 {
     process.stdout.write('You already read ');
-    for( var key in book )
-    {
-
-        
-        switch (key)
-        {
-        case "title":
-        process.stdout.write(book[key]);
-            //console.log(book[key]);
-            break;
-        case "author":
-        process.stdout.write(" by " +book[key]);
-        
-            //console.log("by" +book[key]);
-            
-            break;
-            default:
-        
-            break;
-
-        }
-    }
+  
 }
 else
     {
         process.stdout.write('You still need to read ');
         
-        for( var key in book )
-        {
-    
-            
-            switch (key)
-            {
-            case "title":
-            process.stdout.write(book[key]);
-                //console.log(book[key]);
-                break;
-            case "author":
-            process.stdout.write(" by " +book[key]);
-            
-                //console.log("by" +book[key]);
-                
-                break;
-                default:
-            
-                break;
-    
-            }
-        }
+      
 
     }
-
-
-
+console.log(book.title);
+process.stdout.write(" by " +book[key]);
