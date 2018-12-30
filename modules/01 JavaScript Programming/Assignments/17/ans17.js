36e5bea90e6643875769c3424800f87aeda8b9fa
evyatar <front15@sqlabs.co.il>
2018-12-30 20:57:55 +0200

    ans17

---
+++ modules/01 JavaScript Programming/Assignments/17/ans17.js
@@ -0,0 +1,9 @@
+
+var movie = {title:"shrek",duration:120,stars:["avi","moshe"]};
+
+function printMovie (movie){
+
+    console.log(movie.title+" lasts for "+movie.duration+" minutes. stars: "+movie.stars );
+}
+
+printMovie(movie);
