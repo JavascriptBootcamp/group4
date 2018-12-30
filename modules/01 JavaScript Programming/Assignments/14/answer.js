98ca248949b22a2f708e8ed93c6da4fd6b0b8226
DavidLev1 <44964757+DavidLev1@users.noreply.github.com>
2018-12-30 18:43:51 +0200

    Delete answer.js

--- modules/01 JavaScript Programming/Assignments/14/answer.js
+++
@@ -1,7 +0,0 @@
-var favoriteColors = ['green', 'blue', 'black', '#66ccff', '#ccffcc'];
-var colorNum = 0;
-
-favoriteColors.forEach ( color => {
-	colorNum++;
-	console.log(`My #${colorNum} choice is ${favoriteColors[colorNum-1]}`);
-});
