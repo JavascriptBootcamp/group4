b2605370277af6713924676b00ef1910798cec3a
derldalfor100 <derldalfor100@gmail.com>
2018-12-30 20:34:37 +0200

    Answer 17

---
+++ modules/01 JavaScript Programming/Assignments/17/answer.js
@@ -0,0 +1,11 @@
+var movie = {
+    title: 'The Hobbit',
+    duration: 2,
+    Stars: ['Leonardo DiCaprio', 'Brad Pitt', 'Angelina Jolie', 'Jennifer Aniston']
+};
+
+function printmovieInfo(movieObj){
+    console.log(movieObj.title,'last for',movieObj.duration,'minutes.',Object.keys(movieObj)[2],':',movieObj['Stars'].join());
+}
+
+printmovieInfo(movie);
