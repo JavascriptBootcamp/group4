6640b6e4c8b592b6b4fae831c2b1c3c360d40444
DavidLev1 <44964757+DavidLev1@users.noreply.github.com>
2018-12-30 18:43:00 +0200

    Create answer.js

---
+++ modules/01 JavaScript Programming/Assignments/16/answer.js
@@ -0,0 +1,24 @@
+
+var books = [
+    {
+        title: 'book1',
+        author: 'writer1',
+        alreadyRead: true
+    },
+    {
+        title: 'book2',
+        author: 'writer2',
+        alreadyRead: false
+    },
+    {
+        title: 'book3',
+        author: 'writer3',
+        alreadyRead: true
+    }
+];
+
+
+for (var i = 0; i < books.length; i++) {
+    if (books[i].alreadyRead) console.log('You already read ', books[i].title, ' by ', books[i].author);
+    else console.log('You still need to read ', books[i].title, ' by ', books[i].author);
+}
