f16b0df029ed7a621eba32eb68e6cb78d11a827b
danielbr08 <danielbr08@gmail.com>
2018-12-31 20:46:05 +0200

    Change input variable name

--- modules/01 JavaScript Programming/Assignments/19/validation.js
+++ modules/01 JavaScript Programming/Assignments/19/validation.js
@@ -7,11 +7,11 @@ var errorMessage =
     sumLess17Err : "The sum of all the digits must be greater than 16"
 };
 
-function validateCreditCard(password)
+function validateCreditCard(creditCardNumber)
 {
     var message = "", errMsgArr = new Array();
     var reduceChar = "-";
-    var digitsArr = (password.split("").filter(item=> item !== reduceChar));
+    var digitsArr = (creditCardNumber.split("").filter(item=> item !== reduceChar));
     var finalDigit = digitsArr.slice(digitsArr.length-1);
 
     function getSum(arr)
@@ -23,27 +23,34 @@ function validateCreditCard(password)
         });
         return sum;
     }
+
     if(digitsArr.some(digit=> isNaN(Number(digit)) ))
         errMsgArr.push(errorMessage.notDigitErr);
+
     if( digitsArr.length !== 16 )
         errMsgArr.push(errorMessage.digitsErr);
+
     if(digitsArr.every(digit=> digit === digitsArr[0]) )
         errMsgArr.push(errorMessage.allGigitsSameErr);
+
     if( (finalDigit % 2) !== 0 )
         errMsgArr.push(errorMessage.finalDigitsErr);
+
     if( getSum(digitsArr) <= 16 )
         errMsgArr.push(errorMessage.sumLess17Err);
 
+    // No errors
     if(errMsgArr.length === 0)
         return { valid: true, number: password };
+    // At least on error
     else
         return { valid: false, number: password, error: errMsgArr}
     //return errMessage === ""; // origin return
 }
 
-var inputPassword =['9999777788880000','6666666666661666','4444444444444444','1111111111111110','a92332119c011112','9999-7777-8888-0000','6666-6666-6666-1666'];
+var inputCreditCardNumbers =['9999777788880000','6666666666661666','4444444444444444','1111111111111110','a92332119c011112','9999-7777-8888-0000','6666-6666-6666-1666'];
 
-inputPassword.forEach(element => {
+inputCreditCardNumbers.forEach(element => {
     var result = validateCreditCard(element);
     console.log(result);
 });
