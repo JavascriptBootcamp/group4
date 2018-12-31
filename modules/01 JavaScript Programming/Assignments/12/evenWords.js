5716959eb59edd7bd8b727a16eaa87b69ea5b702
danielbr08 <danielbr08@gmail.com>
2018-12-31 12:01:04 +0200

    AddevenWords.js

---
+++ modules/01 JavaScript Programming/Assignments/12/evenWords.js
@@ -0,0 +1,16 @@
+function evenWords(str) {
+    var strWords = str.split(" ");
+    console.log(strWords);
+    var newStr = '';
+    for(var i=0;i<strWords.length;i++){
+        if(i % 2 !== 0)
+            newStr += strWords[i] + " ";
+    }
+    newStr = newStr.slice(0,newStr.length - 1);
+    return newStr;
+  }
+
+  console.log(evenWords('aaa bbbb asda 1331 ewe'));
+
+ // <p>Example input: <code>'aaa bbbb asda 1331 ewe'</code></p>
+ // <p>Example output: <code>'bbbb 1331'</code></p>
