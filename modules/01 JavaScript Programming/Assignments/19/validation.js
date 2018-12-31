b05d61f191a0f8bedc4331d6837d31e88e3586a7
danielbr08 <danielbr08@gmail.com>
2018-12-31 20:54:57 +0200

    small changes

--- modules/01 JavaScript Programming/Assignments/19/validation.js
+++ modules/01 JavaScript Programming/Assignments/19/validation.js
@@ -2,7 +2,7 @@ var errorMessage =
 {
     digitsErr : "Number must be 16 digits",
     notDigitErr: "All of digits must be numbers",
-    allGigitsSameErr: "You must have at least two different digits represented",
+    allDigitsSameErr: "You must have at least two different digits represented",
     finalDigitsErr: "The final digit must be even",
     sumLess17Err : "The sum of all the digits must be greater than 16"
 };
@@ -30,8 +30,8 @@ function validateCreditCard(creditCardNumber)
     if( digitsArr.length !== 16 )
         errMsgArr.push(errorMessage.digitsErr);
 
-    if(digitsArr.every(digit=> digit === digitsArr[0]) )
-        errMsgArr.push(errorMessage.allGigitsSameErr);
+    if(digitsArr.every(digit=> digit === finalDigit) )
+        errMsgArr.push(errorMessage.allDigitsSameErr);
 
     if( (finalDigit % 2) !== 0 )
         errMsgArr.push(errorMessage.finalDigitsErr);
@@ -41,14 +41,14 @@ function validateCreditCard(creditCardNumber)
 
     // No errors
     if(errMsgArr.length === 0)
-        return { valid: true, number: password };
+        return { valid: true, number: creditCardNumber };
     // At least on error
     else
-        return { valid: false, number: password, error: errMsgArr}
+        return { valid: false, number: creditCardNumber, error: errMsgArr}
     //return errMessage === ""; // origin return
 }
 
-var inputCreditCardNumbers =['9999777788880000','6666666666661666','4444444444444444','1111111111111110','a92332119c011112','9999-7777-8888-0000','6666-6666-6666-1666'];
+var inputCreditCardNumbers =['9999777788880000','6666666666661666','4444444444444444','1111111111111110','a111111111111110','a92332119c011112','9999-7777-8888-0000','6666-6666-6666-1666'];
 
 inputCreditCardNumbers.forEach(element => {
     var result = validateCreditCard(element);
