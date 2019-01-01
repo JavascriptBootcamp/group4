5b6f7e386a38e760171f231a01d5a5b95adf8b55
Moshiko <moshiko.albhari@gmail.com>
2019-01-02 00:57:21 +0200

    Create answer20.js

---
+++ modules/01 JavaScript Programming/Assignments/20/answer20.js
@@ -0,0 +1,12 @@
+var arr = [1, 2, 5, 7];
+
+var filtered = function (arr,fn) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(fn(arr[i]));
+    }
+}
+var checkEven = function (value) {
+    return value % 2 === 0;
+}
+
+filtered(arr,checkEven);
