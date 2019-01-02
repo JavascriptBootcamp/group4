b37c27873c13e861f091eaaf86d6d4e5913bdc04
Moshiko <moshiko.albhari@gmail.com>
2019-01-02 13:34:00 +0200

    Update answer20

--- modules/01 JavaScript Programming/Assignments/20/answer20.js
+++ modules/01 JavaScript Programming/Assignments/20/answer20.js
@@ -1,4 +1,4 @@
-var arr = [1, 2, 5, 7];
+/*var arr = [1, 2, 5, 7];
 
 var filtered = function (arr, fn) {
     for (var i = 0; i < arr.length; i++)
@@ -9,4 +9,17 @@ var checkEven = function (value) {
     return value % 2 === 0;
 }
 
-filtered(arr, checkEven);
+filtered(arr, checkEven);*/
+
+var arr = [1, 2, 5, 7];
+
+function myFilter(arr, fnc) {
+
+        return arr.filter(fnc)
+}
+
+var filtered = myFilter(arr, function (item) {
+        return item % 2 === 0;
+});
+
+console.log(filtered);
