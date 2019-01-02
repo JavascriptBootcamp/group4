cdd03de05125a3c4d9245fab5737dbc36994e5ef
DavidLev1 <44964757+DavidLev1@users.noreply.github.com>
2019-01-02 11:03:09 +0200

    Delete answer.js

--- modules/01 JavaScript Programming/Assignments/8/answer.js
+++
@@ -1,6 +0,0 @@
-var bloodPressure = 70;
-var isNormal = false;
-var isNormal = (bloodPressure > 80  && bloodPressure  < 120) ;
-console.log(isNormal); // false
-var isNormal = (bloodPressure < 80  || bloodPressure  > 120) ;
-console.log(isNormal); // true
