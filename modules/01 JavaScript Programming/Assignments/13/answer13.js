317faf23047f91d13ab86480045cbac38a3f10e5
litalrab <38513468+litalrab@users.noreply.github.com>
2019-01-03 18:29:03 +0200

    answer13.js

---
+++ modules/01 JavaScript Programming/Assignments/13/answer13.js
@@ -0,0 +1,21 @@
+function isPoli(str)
+{
+
+    for(var i=0; i<str.length/2;i++)
+    {
+  
+            if(str[i]!==str[str.length-1-i])
+            {
+                return false;
+                
+            }
+
+
+    }
+    return true;
+
+
+}
+
+console.log(isPoli("abvba"));
+console.log(isPoli("acvba"));
