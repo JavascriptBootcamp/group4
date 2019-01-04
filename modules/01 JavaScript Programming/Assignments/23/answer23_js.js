7ead0d40b4a338751897847c933ed183f1b3ed22
aviyot <aviyot@gmail.com>
2019-01-04 17:37:24 +0200

    answer 23 javascript

--- modules/01 JavaScript Programming/Assignments/23/answer23_js.js
+++ modules/01 JavaScript Programming/Assignments/23/answer23_js.js
@@ -1,6 +1,6 @@
 
 function Word(str){
-
+    this.length;
     this.setLength = function(){
      this.length = str.length;
     }
@@ -8,12 +8,22 @@ function Word(str){
 
 function findLongestString(sentence){
 
+var maxLength = 0;
+var theBigSizeWord =  "";
  splitedWords = sentence.split(" ");
- for (var i = 0 ; i < splitedWords.length;i++)
- var word =  new Word(splitedWords[i]);
- 
- console.log(splitedWords);
 
+ for (var i = 0 ; i < splitedWords.length ; i++){
+
+    var word =  new Word(splitedWords[i]);
+    word.setLength();
+
+    if(word.length  >  maxLength){
+      maxLength = word.length;
+      theBigSizeWord = splitedWords[i];
+    }
+ }
+ 
+ console.log(theBigSizeWord);
 }
 
 findLongestString('Web Development Tutorial');
