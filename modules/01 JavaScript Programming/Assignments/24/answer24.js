5c1c3679553c7290cc1953281608b1f563b87508
GuyAgiv <guyagiv@gmail.com>
2019-01-06 19:13:34 +0200

    update

--- modules/01 JavaScript Programming/Assignments/24/answer24.js
+++ modules/01 JavaScript Programming/Assignments/24/answer24.js
@@ -1,6 +1,25 @@
 function RandomWord (chars){
     
+
+   if(!Array.isArray(chars)){
+       console.log("chars should be an array.");
+       return;
+   }
+   
+   for(var k=0;k<chars.length;k++){
+       if( typeof(chars[k]) !== "string"){
+        console.log("chars should include only strings.");
+        return;
+       }
+   }
+
     this.randomize = function(len){
+
+      if(typeof len !== "number"){
+        console.log("length should be a number");
+        return;
+      }
+
       var str = "";
       for(var j=0;j<len;j++){
       var i = Math.floor(Math.random()*chars.length);    
@@ -12,6 +31,8 @@ function RandomWord (chars){
 
 
 var word = new RandomWord(['a','b','g','u']);
+//var word = new RandomWord("g");
+//var word = new RandomWord(['a','b','g','u',5]);
 var result = word.randomize(6);
 console.log(result);
 result = word.randomize(2);
