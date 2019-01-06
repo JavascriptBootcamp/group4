fc7b7581ab9081e8f3a31dc1360a9d3d77ee4174
Avraham Adnahani <44324749+AviAdnahani@users.noreply.github.com>
2019-01-06 19:14:09 +0200

    Delete answer13.js

--- modules/01 JavaScript Programming/Assignments/13/answer13.js
+++
@@ -1,9 +0,0 @@
-function testPalindrom(str) {
-    n = str.length;
-    for (var i=0; i<n ; i++) {
-        if (str[i] !== str[n-i-1]){
-            return false;
-        }
-    }
-    return true;
-}
