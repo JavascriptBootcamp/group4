f6dedf09817be2a027146a79ad0f586dd0abf83f
mariyastash <mariyastashevski@gmail.com>
2019-01-06 19:16:10 +0200

    validation added answer24.js

--- modules/01 JavaScript Programming/Assignments/24/answer24.js
+++ modules/01 JavaScript Programming/Assignments/24/answer24.js
@@ -1,10 +1,20 @@
 function RandomWord(arr) {
     this.arr = arr;
 
+    if(!Array.isArray(arr)){
+        return '"chars" should be an array';
+    }
     this.randomize = function (length) {
         var chars = arr.join("");
         var randomString = "";
+
+        if(isNaN(length)){
+            return '"length" should be a number';
+        }
         for (var i = 0; i < length; i++) {
+            if(! isNaN(chars[i])){
+                return '"chars" should include only strings';
+            }
             randomString += chars.charAt(Math.floor(Math.random() * arr.length));
         }
         return randomString;
