289efb1f4dd76fa4adf58338a7c94a47a731c27d
litalrab <38513468+litalrab@users.noreply.github.com>
2019-01-06 20:17:36 +0200

    Update answer29.js

--- modules/01 JavaScript Programming/Assignments/29/answer29.js
+++ modules/01 JavaScript Programming/Assignments/29/answer29.js
@@ -1,1 +1,13 @@
+function Video (title, uploader,seconds )
+{
+    this.title=title;
+    this.uploader=uploader;
+    this.seconds=seconds;
+    this.watch=function()
+    {
+        console.log ( "You watched all "+this.seconds+" seconds of "+ this.title +"!");
+    };
 
+}
+var myVideo= new Video("Otters Holding Hands","lit",60);
+myVideo.watch();
