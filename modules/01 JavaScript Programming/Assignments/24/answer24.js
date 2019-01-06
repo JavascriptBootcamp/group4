8a02338c0b474fa7f55a24998aeb2158d0ad7aa0
mariyastash <mariyastashevski@gmail.com>
2019-01-06 18:54:33 +0200

    bug fixed answer24.js

--- modules/01 JavaScript Programming/Assignments/24/answer24.js
+++ modules/01 JavaScript Programming/Assignments/24/answer24.js
@@ -5,15 +5,14 @@ function RandomWord(arr) {
         var chars = arr.join("");
         var randomString = "";
         for (var i = 0; i < length; i++) {
-            randomString += chars.charAt(Math.floor(Math.random() * length));
+            randomString += chars.charAt(Math.floor(Math.random() * arr.length));
         }
-
         return randomString;
     }
 }
 
-var word = new RandomWord(['a','b','g','u']);
-var result = word.randomize(6);
+var word = new RandomWord(['a','b','g','u', 'a', 'k']);
+var result = word.randomize(10);
 console.log(result);
 result = word.randomize(2);
 console.log(result);
