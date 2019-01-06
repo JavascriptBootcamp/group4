98295a2987eafb2e8a6cd4bc5680419c7572eddf
unknown <dudusabag73@gmail.com>
2019-01-06 23:42:26 +0200

    Ans29

---
+++ modules/01 JavaScript Programming/Assignments/29/Ans29.js
@@ -0,0 +1,36 @@
+function Video(title, uploader ,seconds ){
+    this.title = title;
+    this.uploader = uploader;
+    this.seconds = seconds;
+
+    this.watch = function watch(amounts){
+        this.seconds = amounts;
+        console.log("You watched all " + this.seconds + " seconds of "+ this.title + "!");
+    }
+} 
+try{
+    vid1 = new Video("Metrix","Some guy",60);
+    vid2 = new Video("Avatar","Some other guy",67);
+}
+catch(err){
+    console.log("Oops... An error occured");
+}
+
+vids = [["Metrix","Some guy",60],["Avatar","Some other guy",67],["Thor","Avi",50],["Thor2","David",52],["Thor3","Dan",50]]
+
+console.log("");
+console.log("");
+vid1.watch(999);
+console.log("");
+console.log("");
+
+
+
+vidsObj = []
+
+for(var i=0; i<vids.length; i++){
+
+    vidsObj.push(new Video(vids[i][0],vids[i][1],vids[i][2]));
+
+}
+console.log("check push",vidsObj);
