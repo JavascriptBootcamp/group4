ff2aba172f030a4b4da04e0647608853f33c2f42
derldalfor100 <derldalfor100@gmail.com>
2019-01-06 18:39:31 +0200

    Answer 24

---
+++ modules/01 JavaScript Programming/Assignments/24/answer.js
@@ -0,0 +1,28 @@
+function RandomWord(chars){
+    //this.length;
+    this.randomize = function(length){
+        if(!Array.isArray(chars)){
+            return '"chars" should be an array';
+        }
+        var checkNonString = chars.some((value) => {
+            return typeof value !== 'string'
+        });
+        if(checkNonString){
+            return '"chars" should include only strings';
+        }
+        if(!Number.isInteger(length)){
+            return '"length" should be a number';
+        }
+        var str = '';
+        for(var i =0; i < length; i++){
+            var j = Math.floor(Math.random() * chars.length);
+            str += chars[j];
+        }
+        return str;
+    }
+}
+
+
+var randomWord = new RandomWord(['a', 'b', 'd', 'f']);
+
+console.log(randomWord.randomize(3));
