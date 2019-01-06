b94b038bdeae6fefe425c36892310b913bcea637
Avraham Adnahani <44324749+AviAdnahani@users.noreply.github.com>
2019-01-06 17:34:04 +0200

    Update answer23.js
    added "var word" to save the biggist word

--- modules/01 JavaScript Programming/Assignments/23/answer23.js
+++ modules/01 JavaScript Programming/Assignments/23/answer23.js
@@ -10,12 +10,14 @@ function findLongestString (str) {
     var strSplit = str.split(" ");
     var max = 0;
     var size;
+    var word;
     for (var i=0; i<strSplit.length ; i++) {
         size = new Word(strSplit[i]).length;
         if (max < size) {
             max = size;
+            word = strSplit[i];
         }
     }
-    return max;
+    return word;
 }
 
