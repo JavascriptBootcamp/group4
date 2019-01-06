8a98dcf40cf73279d88fa7eaa19a99cd0aeb5ebe
litalrab <38513468+litalrab@users.noreply.github.com>
2019-01-06 20:23:09 +0200

    Update answer29.js

--- modules/01 JavaScript Programming/Assignments/29/answer29.js
+++ modules/01 JavaScript Programming/Assignments/29/answer29.js
@@ -7,7 +7,21 @@ function Video (title, uploader,seconds )
     {
         console.log ( "You watched all "+this.seconds+" seconds of "+ this.title +"!");
     };
+    this.watch=function(newseconds)
+    {
+        console.log ( "You watched all "+newseconds+" seconds of "+ this.title +"!");
+    };
 
 }
 var myVideo= new Video("Otters Holding Hands","lit",60);
 myVideo.watch();
+
+var data=[];
+for(var i=0;i<5;i++)
+{
+   // var myVideo= new Video("Otters Holding Hands","lit",60);
+    data.push(myVideo);
+
+
+}
+myVideo.watch(80);
