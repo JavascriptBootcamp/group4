4e42e4bcf31daa7191899360eab48d2f071062b3
aviyot <aviyot@gmail.com>
2019-01-07 06:25:01 +0200

    final commit

--- modules/01 JavaScript Programming/Assignments/29/answer29_js.js
+++ modules/01 JavaScript Programming/Assignments/29/answer29_js.js
@@ -1,19 +1,35 @@
 
 function Video(title, uploader, second){
-    this.title = title;
-    this.uploader = uploader;
-    this.second = second;
-    
 
+    //privte propeirts
+    title = title;
+    uploader = uploader;
+    second = second;
+
+    //global function
     this.watch = function() {
-        console.log(`You watched all ${this.second} seconds of ${this.title}!`);
+        
+        try{
+
+        console.log(`You watched all ${second} seconds of ${title}!`);
+
+        }
+        catch(err){
+
+            console.log("Oops... An error occured");
+        }
+
+        finally {
+
+        }
     }
 } 
+
 
 try {
 var video1 = new Video("Titanic", "Aviyot" ,120);
 var video2 = new Video("Roki", "getahun" ,90);
-video.watch();
+video1.watch();
 video2.watch();
 }
 
