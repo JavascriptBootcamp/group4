7c58808e5408ddffe70b3bbeef8cb9622d2c2a5a
aviyot <aviyot@gmail.com>
2019-01-07 06:06:04 +0200

    commit 7-1-19

--- modules/01 JavaScript Programming/Assignments/23/answer23_js.js
+++ modules/01 JavaScript Programming/Assignments/23/answer23_js.js
@@ -1,29 +1,43 @@
 
 function Word(str){
-    this.length;
+    //this.length = 5;
     this.setLength = function(){
-     this.length = str.length;
+        this.length = str.length;
+        console.log("i am Word", this.length);
     }
+
 }
 
-function findLongestString(sentence){
-
-var maxLength = 0;
-var theBigSizeWord =  "";
- splitedWords = sentence.split(" ");
-
- for (var i = 0 ; i < splitedWords.length ; i++){
-
-    var word =  new Word(splitedWords[i]);
-    word.setLength();
-
-    if(word.length  >  maxLength){
-      maxLength = word.length;
-      theBigSizeWord = splitedWords[i];
-    }
+ var Obj = function() {
+     //this.length =  10;
+    this.setLength = function(){
+    this.length = 10;
+    console.log("i am Obj",this.length);
+       }
  }
  
- console.log(theBigSizeWord);
-}
+ var fun  = function(){
+    this.length = 100;
+     console.log(this.length);
+     
+ }
 
-findLongestString('Web Development Tutorial');
+ 
+
+word1 = new Word("hii");
+obj1 = new Obj();
+
+//word1.setLength();
+//obj1.setLength();
+
+fun();
+
+word1.setLength();
+obj1.setLength();
+fun.call(word1);
+fun.call(obj1);
+/* 
+console.log("before "+ word1.length);
+obj1.setLength.call(word1);
+console.log("after "+ word1.length);
+ */
