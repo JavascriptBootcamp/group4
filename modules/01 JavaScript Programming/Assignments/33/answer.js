5a275a8190b5228fd2fa6fc51eeef33a82768aa4
derldalfor100 <derldalfor100@gmail.com>
2019-01-10 21:04:52 +0200

    answer.js

---
+++ modules/01 JavaScript Programming/Assignments/33/answer.js
@@ -0,0 +1,101 @@
+function Node(data, next) {
+    this.data = data;
+    this.next = next
+}
+
+function LinkedList(){
+    this.head = null;
+
+    this.getFirst = function(){
+        return this.head;
+    }
+
+    this.getLast =function(){
+        var node;
+        if (!this.head) {
+            return null;
+        }
+        node = this.head;
+        while (node.next){
+            node = node.next;
+        }
+        return node;
+    }
+
+    this.insertFirst = function(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    this.insertLast = function(data){
+        var last = this.getLast();
+        if (last) {
+            last.next = new Node(data, null);
+        }
+        else {
+            this.head = new Node(data, null);
+        }
+    }
+
+    this.printAll = function() {
+        var head = this.head;
+        if (!head) {
+            return null;
+        }
+        while (head) {
+            console.log(head.data);
+            head = head.next;
+        }
+    }
+}
+
+function circular(list){
+    var node1 = list.getFirst();
+    //var listTillNow = new LinkedList();
+    var node2 = list.getFirst();
+    var nodeLast;
+    var i=0;
+    while(!node1){
+        //node2 = node1;
+        node1 = node1.next;
+        var node3 = list.getFirst();
+        i++;
+        for(var j=0; j < i; j++){
+            if(node1 === node3){
+                return true;
+            }
+            node3 = node3.next;
+        }
+    }
+
+    return false;
+}
+
+const l = new LinkedList();
+const a = new Node('red');
+const b = new Node('yellow');
+const c = new Node('green');
+l.head = a;
+a.next = b;
+b.next = c;
+c.next = b;
+// ll.insertLast('z');
+// ll.insertLast('y');
+// ll.printAll();
+
+function midpoint(list){
+    var slow = list.getFirst();
+    var fast = list.getFirst();
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+
+//console.log("midpoint is", midpoint(ll)); 
+
+console.log(circular(l));
+
+l.printAll();
