551a01914844cbf49c129ae266adb7cf14242402
David Lev <front1@sqlabs.co.il>
2019-01-10 18:19:21 +0200

    JS includes() method on array

---
+++ includes.js
@@ -0,0 +1,13 @@
+/*
+כאשר מופעלת על מערך: מקבלת פרמטר ובודקת האם הפרמטר הזה זהה ללפחות אחת מאיברי המערך.
+אם כן - מוחזר true
+אחרת מוחזר  false
+*/
+
+
+var names = ["Beny Man", "David Good", "Koko Comipoki", "Kuzinaki Kuzuki"];
+var result = names.includes("Koko Comipoki");
+//var result = names.includes("Some Name");
+
+console.log(result);
+
