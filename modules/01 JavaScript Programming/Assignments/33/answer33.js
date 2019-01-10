9a22a5c051510dacd5df267b597cfb95f18154b0
MatanDiner <matand0390@gmail.com>
2019-01-11 01:18:29 +0200

    add answer33

---
+++ modules/01 JavaScript Programming/Assignments/33/answer33.js
@@ -0,0 +1,90 @@
+function Node(data, next) {
+    this.data = data;
+    this.next = next
+}
+
+function LinkedList(){
+    this.head = null;
+
+    this.getFirst = function(){
+        return this.head;
+    }
+
+    this.getLast =function(){
+        var node;
+        if (!this.head) {
+            return null;
+        }
+        node = this.head;
+        while (node.next){
+            node = node.next;
+        }
+        return node;
+    }
+
+    this.insertFirst = function(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    this.insertLast = function(data){
+        var last = this.getLast();
+        if (last) {
+            last.next = new Node(data, null);
+        }
+        else {
+            this.head = new Node(data, null);
+        }
+    }
+
+    this.printAll = function() {
+        var head = this.head;
+        if (!head) {
+            return null;
+        }
+        while (head) {
+            console.log(head.data);
+            head = head.next;
+        }
+    }
+}
+
+function midpoint(list){
+    var slow = list.getFirst();
+    var fast = list.getFirst();
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+
+//console.log("midpoint is", midpoint(ll));
+
+
+function circular(list){
+
+var current = list.getFirst();
+var before = list.getFirst();
+
+while(current && current.next){
+
+current = current.next.next;
+if(current == before){
+    return true;
+}
+before = current;
+}
+return false;
+}
+
+const l = new LinkedList();
+const a = new Node('red');
+const b = new Node('yellow');
+const c = new Node('green');
+l.head = a;
+a.next = b;
+b.next = c;
+c.next = b;
+console.log(circular(l));
