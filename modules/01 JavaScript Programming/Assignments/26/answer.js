24669471bad0b7c2375f812def634c0773711406
derldalfor100 <derldalfor100@gmail.com>
2019-01-11 19:42:41 +0200

    answer 26

---
+++ modules/01 JavaScript Programming/Assignments/26/answer.js
@@ -0,0 +1,24 @@
+function viceVersa(keySuffix, valueSuffix){
+    var values = Object.keys(this);
+    var keys = Object.values(this);
+    var cloneObj = {};
+    keys.forEach((key, i) => {
+        cloneObj[key] = values[i];
+    });
+    if(keySuffix && valueSuffix){
+        cloneObj[keySuffix] = valueSuffix;
+    }
+    return cloneObj;
+}
+
+var obj1 = {a1 : 'hi guy', 23 : true};
+var obj2 = {'tr22' : 'ffff4 1', 25 : true, 30 : false};
+var obj3 = {kate : 'perry', 'hi' : true, 34 : null};
+
+console.log(viceVersa.call(obj1));// change the context
+console.log(viceVersa.call(obj2));
+console.log(viceVersa.call(obj3));
+
+console.log(viceVersa.apply(obj1, ['a2', 'bye girl']));
+console.log(viceVersa.apply(obj2, ['gogogo', 'backback']));
+console.log(viceVersa.apply(obj3, ['false', 44]));
