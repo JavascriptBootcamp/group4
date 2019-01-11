65f42ea4622c24538b08fc5455217d9dce3a2f4e
aviyot <aviyot@gmail.com>
2019-01-11 13:53:33 +0200

    add check by function

--- modules/01 JavaScript Programming/Assignments/32/answer32_js.js
+++ modules/01 JavaScript Programming/Assignments/32/answer32_js.js
@@ -19,23 +19,23 @@ function Stack(){
     }
 }
 
- var str = "abccb";
-var strArr = str.split("");
 
+var checkPalindrome  =  function(str){
+    
+var strArr = str.split("");
 var queue = new Queue();
 var stack = new Stack();
 
-strArr.forEach(function(item ,i){
+strArr.forEach(function(item){
    stack.push(item);
    queue.add(item);
 })
 var isPalindrom = strArr.every( function(item, i,arr){
-    //console.log(i , queue.remove() , stack.pop());
-    if(i < arr.length /2) 
+    //console.log(i,queue.remove(),stack.pop() )
+    if(i < arr.length /2)
       return queue.remove() == stack.pop();
     else
     return true;
-    
 } );
 
 if (isPalindrom)
@@ -43,3 +43,10 @@ if (isPalindrom)
 else
 console.log(str + " Is Not Palindrom");
 
+}
+
+var str1 = "abccba";
+var str2 = "abccb";
+
+checkPalindrome(str1);
+checkPalindrome(str2);
