b5975e8690461f1f3c45ea0d6726c605629a4f73
amir <ameer@hotmail.co.il>
2019-01-12 21:14:34 +0200

    targil21

---
+++ modules/01 JavaScript Programming/Assignments/21/answer21.js
@@ -0,0 +1,11 @@
+function maxValueInArray(arr) {
+    var max = arr[0];
+    arr.forEach(function(item) {
+        if (item > max){
+            max = item;
+        }
+    })
+    return max;
+}
+
+console.log(maxValueInArray([0, 20, 30, 40, 10])); 
