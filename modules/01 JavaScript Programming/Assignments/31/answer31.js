cc6d5fdf5cb120a5f31cf49da196107984277c56
mariyastash <mariyastashevski@gmail.com>
2019-01-12 15:08:42 +0200

    exeption added

--- modules/01 JavaScript Programming/Assignments/31/answer31.js
+++ modules/01 JavaScript Programming/Assignments/31/answer31.js
@@ -1,21 +1,32 @@
-function Queue(){
-    var arr = [];
+function Queue() {
+    this.arr = [];
 
-    this.add = function(value){
-        arr.push(value);
+    this.add = function (value) {
+        if (!value) {
+            throw "Please enter any value";
+        }
+
+        this.arr.push(value);
     }
 
-    this.remove = function(){
-        return arr.shift();
+    this.remove = function () {
+        if (this.arr.length === 0) {
+            throw "Queue is empty!";
+        }
+
+        return this.arr.shift();
     }
 }
 
-
-
-
 var queue = new Queue();
-queue.add(1);
-queue.add(3);
-queue.add(14);
 
-queue.remove(); //Output: 1
+try {
+    queue.add(1);
+    queue.add(3);
+    queue.add(14);
+
+    queue.remove(); //Output: 1
+}
+catch (exeption) {
+    console.log("Error:", exeption);
+}
