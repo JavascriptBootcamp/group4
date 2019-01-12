959be8b0bd7761162a97912d1a073f7bec201e9f
ameer89 <44411703+ameer89@users.noreply.github.com>
2019-01-12 21:38:46 +0200

    answer 20

---
+++ modules/01 JavaScript Programming/Assignments/20/answer20 by amir.js.js
@@ -0,0 +1,18 @@
+
+var arr = [1, 2, 5, 7];
+
+function myFilter(arr,callback){
+    var filteredArr=[];
+
+    for (var i = 0 ; i < arr.length ; i ++){
+        if (callback(arr[i]))
+            filteredArr.push(arr[i]);
+    }
+   return filteredArr;
+}
+
+var filtered = myFilter(arr, function(item){
+     return item % 2 === 0;
+});
+
+console.log(filtered);
