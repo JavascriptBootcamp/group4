6ebb02a5233a13c5d5ddf3f37c775b8c4c28155f
derldalfor100 <derldalfor100@gmail.com>
2019-01-12 14:55:20 +0200

    answer 32

---
+++ modules/01 JavaScript Programming/Assignments/32/answer.js
@@ -0,0 +1,28 @@
+function isPalindrome(str) {
+    var isPalindrome = true;
+    try{
+        if(typeof str !== 'string'){
+            isPalindrome = false;
+            throw 'not a string!';
+        }
+        var stack = [];
+        var queue = [];
+        for(var i=0; i < str.length / 2; i++){
+            stack.push(str[i]);
+            queue.unshift(str[str.length - 1 - i]);
+        }
+        while(stack.length !== 0){
+            if(stack.pop() !== queue.shift()){
+                isPalindrome = false;
+                break;
+            }
+        }
+    }catch(ex){
+        console.log(ex);
+    }finally{
+        return isPalindrome;
+    }
+}
+
+var str = 'dabaabad';
+console.log(isPalindrome(str));
