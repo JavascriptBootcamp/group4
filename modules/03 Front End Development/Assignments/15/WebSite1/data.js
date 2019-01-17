735e8395a976d98677a7d68494f58cb78559e095
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-01-17 12:24:10 +0200

    Add files via upload

---
+++ modules/03 Front End Development/Assignments/15/WebSite1/data.js
@@ -0,0 +1,93 @@
+﻿var question = {
+
+    q1: {
+        question: "1.witch kind of sport,there is no ball ?",
+        1: "football",
+        2: "swim",
+        3: "basketball",
+        4: "soccer",
+        correct: "2"
+    },
+
+    q2: {
+        question: "2.When was the recognition of the State of Israel ?",
+        1: "1948",
+        2: "2000",
+        3: "1950",
+        4: "1967",
+        correct: "1"
+    },
+
+    q3: {
+        question: "3.Who was Israel's first prime minister ?",
+        1: "David Ben Gorion",
+        2: "Bibi",
+        3: "Rabin",
+        4: "Begin",
+        correct: "1"
+    },
+
+    q4: {
+        question: "4.What is the capital of Israel ?",
+        1: "New York",
+        2: "Netanya",
+        3: "Jerusalem",
+        4: "Tel Aviv",
+        correct: "3"
+    },
+
+    q5: {
+        question: "5.What is the capital of Spain ?",
+        1: "Barcelona",
+        2: "Madrid",
+        3: "Tel Aviv",
+        4: "New York",
+        correct: "2"
+    },
+
+    q6: {
+        question: "6.What is the capital of France ?",
+        1: "Pariz",
+        2: "Madrid",
+        3: "Tel Aviv",
+        4: "New York",
+        correct: "1"
+    },
+
+    q7: {
+        question: "7.What animal is under water ?",
+        1: "Lion",
+        2: "Fish",
+        3: "Cat",
+        4: "Dog",
+        correct: "2"
+    },
+
+    q8: {
+        question: "8.Who is the best friend of man ?",
+        1: "Lion",
+        2: "Fish",
+        3: "Cat",
+        4: "Dog",
+        correct: "4"
+    },
+
+    q9: {
+        question: "9.What is unusual ?",
+        1: "Lion",
+        2: "Fish",
+        3: "Cat",
+        4: "Dog",
+        correct: "2"
+    },
+
+    q10: {
+        question: "10.What is the capital of Egypt ?",
+        1: "Cairo",
+        2: "Madrid",
+        3: "Tel Aviv",
+        4: "New York",
+        correct: "1"
+    }
+
+}
---
+++ modules/03 Front End Development/Assignments/15/WebSite1/func.js
@@ -0,0 +1,93 @@
+﻿data = {};
+time = 10
+index = 0;
+function init(i) {
+    index = i;
+    if (i == 1) {
+        initTime();
+    }
+    else { time = 11; }
+    var k = 0;
+    var quiz = document.getElementById("quiz");
+    var str = "";
+    str += "<table>";
+        str += "<tr>";
+        str += "<td colspan='2'>";
+        str += question["q" + i].question ;
+        str += "</td>";
+        for (var j = 1; j < 5; j = j + 2) {
+            str += "<tr>";
+            str += "<td id = '"+i+"-"+j+"' onclick = switch_q("+i+","+j+",this)>";
+            str += j + "." + question["q" + i][j];
+            str += "</td>";
+            k = j + 1;
+            str += "<td  id = '" + i + "-" + k + "' onclick = switch_q(" + i + ","+ k +",this)>";
+            str += j+1 + "." + question["q" + i][j + 1];
+            str += "</td>";
+            str += "</tr>";
+        }
+    
+        str += "</table>";
+        if (i == 10) {
+            str += "<input type='hidden' value='1547375825757'>";
+            str += "<button onclick='postData()'>Submit</button>";
+        }
+        quiz.innerHTML = str;
+}
+
+
+
+function switch_q(quest,answer,obj){
+
+    var td = document.getElementById(obj.id);
+    if (answer == question["q" + quest]["correct"]) {
+        td.style.color = "green";
+    }
+    else {
+        td.style.color = "red";
+    }
+    data[quest] = {
+        question: question["q" + quest]["question"],
+        answer: question["q" + quest][answer]
+    }
+    if (quest < 10) {
+        setTimeout(function () { init(quest + 1) }, 3000);
+    }
+
+}
+
+function initTime() {
+    
+    document.getElementById("time").innerHTML = `Time: ${time}`;
+    h = setInterval(function () { setTime() }, 1000);
+};
+
+function setTime() {
+    if (time <= 1) {
+        data[index] = {
+            question: question["q" + index]["question"],
+            answer: "No Answer"
+        }
+        clearInterval(h); 
+        if (index < 10) {
+            init(index + 1);
+            initTime(index + 1);
+        }
+    }
+    time -= 1;
+    document.getElementById("time").innerHTML = `Time: ${time}`;;
+}
+
+
+function postData() {
+    var http = new XMLHttpRequest();
+    var url = 'http://localhost:3000/quiz';
+    http.open("POST", url, true);
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState == 4 && http.status == 200)
+        console.log(http.responseText);
+    }
+
+    http.send(data);
+}
