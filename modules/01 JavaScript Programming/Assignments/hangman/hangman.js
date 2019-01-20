28b7638cd2827becef8cc7fa213a004c98979a83
DavidSabag <dudusabag73@gmail.com>
2019-01-20 16:26:36 +0200

    hangman sol

--- modules/01 JavaScript Programming/Assignments/hangman/hangman.js
+++ modules/01 JavaScript Programming/Assignments/hangman/hangman.js
@@ -1,43 +1,45 @@
-
 function getFindsStr(phrase, letters){
     var res = [];
-    for(var i=0; i< phrase.split('').length; i++){
-    
-        for(var j=0; j<letters.length; i++){
-            if(phrase[i] === letters[j]){
-                res.push(phrase[i]);
-                break;
-            }
-            else{
-                res.push("_");
-                break;
-            }
-        }
-    }
+    phrase.split('').forEach(p => {
+        if(letters.join('').includes(p))
+            res.push(p);
+        else if(p === ' ')
+            res.push(' ');
+        else
+            res.push('_');
+    });
     return res.join('');
 }
 
-function getGessFromUser(){
-
+function isWon(phrase, letters){
+    return !getFindsStr(phrase,letters).includes('_');
 }
+
 
 function runGame(phrases){
-    var phrase = phrases[Math.floor(Math.random()*phrases.length)];
     var life = 10;
-    while(life > 10){
-        console.log("You have " + life + "left!");
-        sPhrase = phrase.split('');
-        
-
-
-
-        life--;
-    }
-    return phrase;
+    var gessLst = [];
+    var phrase = phrases[Math.floor(Math.random()*phrases.length)];
+    console.log("Wellcome to hangman Game.");
+    console.log("You have " + life + " lifes left please enter your gess:");
+    var stdin = process.openStdin();
+        stdin.addListener("data", function(gess) {
+            gessLst = gessLst.concat(gess.toString().trim().split(''));
+            console.log(getFindsStr(phrase,gessLst));
+            
+            if(isWon(phrase,gessLst)) {
+                console.log("You Won!!!!");
+                process.exit();
+            }
+            life--;
+            if(life === 0){
+                console.log("You Lost!!!");
+                process.exit();
+            }
+            console.log("You have " + life + " lifes left please enter your gess:");            
+        });
 }
+    
 
-
-
-
-// var items = ['messi','ronaldo','Game of Throns','Led Zepplin'];
-// console.log(runGame(items));
+var phrases = ['messi ronaldo','bambam'];
+runGame(phrases);
