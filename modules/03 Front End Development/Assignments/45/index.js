e2d0fc1a5304a40729b15a9f655c095936c49b3f
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-02-08 22:24:09 +0200

    Update index.js

--- modules/03 Front End Development/Assignments/45/index.js
+++ modules/03 Front End Development/Assignments/45/index.js
@@ -1,4 +1,3 @@
-
 arr = [];
 startIndex=0;
 endIndex=12 ;
@@ -14,33 +13,50 @@ var obj = {
     link:link
 }
 arr.push(obj);
-numOfPages = arr.length / 12;
+showNumOfPages();
+load(page);
+}
+
+function showNumOfPages(){
+numOfPages = Math.ceil(arr.length / 12);
+var footer = document.getElementById("footer");
+footer.innerHTML = "";
 for(var i=0;i<numOfPages;i++){
     var span = document.createElement("span");
+    var num = Number(i)+1;
     if(numOfPages===1){
-        span.innerText = i;
+        span.innerText = num + "   ";  
     }
     else{
-        span.innerText = i + ">>>";
+        span.innerText = num + "   ";
     }
-  //  span.onclick = function(){
-   //     
-    //  }
+    span.onclick = function(){
+        page = this.innerText[0];
+        load(this.innerText[0]);   
+      }
+      footer.appendChild(span);
+}
 }
 
 function load(page){
 if(arr.length === 0){return;}
     endIndex = page * 12;
     startIndex = endIndex - 12;
-    var ph = document.getElementById("ph")
+    var ph = document.getElementById("ph");
+    ph.innerHTML = "";
     for(var i=startIndex;i<endIndex;i++){
+        if(arr[i] === undefined ){
+            break;
+        }
         var a = document.createElement("a");
         var img = document.createElement("img");
-        a.appendChild(img);
         a.href = arr[i].link;
+        a.target = "_blank";
         img.src = arr[i].url;
-        ph.appendChild(img);
-
+        img.style.width = "300px";
+        img.style.height = "200px";
+        a.appendChild(img);
+        ph.appendChild(a);
     }
 
 }
