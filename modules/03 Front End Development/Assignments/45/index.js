03d63d301055a09ea5789fc9de79a1a1ffd41712
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-02-09 15:06:10 +0200

    Update index.js

--- modules/03 Front End Development/Assignments/45/index.js
+++ modules/03 Front End Development/Assignments/45/index.js
@@ -1,58 +1,52 @@
 arr = [];
-startIndex=0;
-endIndex=12 ;
-numOfPages=1;
+startIndex = 0;
+endIndex = 12;
+numOfPages = 1;
 page = 1;
-function add(e){
+function add(e) {
     e.preventDefault();
-var url = document.getElementById("url").value;
-var link = document.getElementById("link").value;
+    var url = document.getElementById("url").value;
+    var link = document.getElementById("link").value;
 
-var obj = {
-    url:url,
-    link:link
-}
-arr.push(obj);
-showNumOfPages();
-load(page);
-}
-
-function showNumOfPages(){
-numOfPages = Math.ceil(arr.length / 12);
-var footer = document.getElementById("footer");
-footer.innerHTML = "";
-for(var i=0;i<numOfPages;i++){
-    var span = document.createElement("span");
-    var sp = document.createElement("span");
-    sp.innerText = ">>>"; 
-    sp.style.color = "blue";
-    span.style.cursor = "pointer";
-    var num = Number(i)+1;
-    if(numOfPages===1){
-        span.innerText = num ;  
+    var obj = {
+        url: url,
+        link: link
     }
-    else{
-        if(i>0){
-        footer.appendChild(sp);
+    arr.push(obj);
+    showNumOfPages();
+    load(page);
+}
+
+function showNumOfPages() {
+    numOfPages = Math.ceil(arr.length / 12);
+    var footer = document.getElementById("footer");
+    footer.innerHTML = "";
+    for (var i = 0; i < numOfPages; i++) {
+        var span = document.createElement("span");
+        span.style.cursor = "pointer";
+        if (i > 0) {
+            var sp = document.createElement("span");
+            sp.innerText = ">>>";
+            sp.style.color = "blue";
+            footer.appendChild(sp);
         }
-        span.innerText = num ;
+        span.innerText = Number(i) + 1;
+        span.onclick = function () {
+            page = this.innerText[0];
+            load(this.innerText[0]);
+        }
+        footer.appendChild(span);
     }
-    span.onclick = function(){
-        page = this.innerText[0];
-        load(this.innerText[0]);   
-      }
-      footer.appendChild(span);
-}
 }
 
-function load(page){
-if(arr.length === 0){return;}
+function load(page) {
+    if (arr.length === 0) { return; }
     endIndex = page * 12;
     startIndex = endIndex - 12;
     var ph = document.getElementById("ph");
     ph.innerHTML = "";
-    for(var i=startIndex;i<endIndex;i++){
-        if(arr[i] === undefined ){
+    for (var i = startIndex; i < endIndex; i++) {
+        if (arr[i] === undefined) {
             break;
         }
         var a = document.createElement("a");
