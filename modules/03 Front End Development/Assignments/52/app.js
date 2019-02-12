788c30ed5ecd342397cb2d0b20a66915a1ef7224
danielbr08@gmail.com <daniel.brosh@sapiens.com>
2019-02-12 19:38:36 +0200

    Add css_q_52

---
+++ modules/03 Front End Development/Assignments/52/app.js
@@ -0,0 +1,25 @@
+var emailArr = [
+	{ from: "dani", subject: "surfing article", date: "23-02-20" },
+	{ from: "yosi", subject: "files sending", date: "11-10-18" },
+	{ from: "maly", subject: "homework exsercise", date: "23-02-18" },
+	{ from: "dalya", subject: "invitation for wedding", date: "15-07-19" },
+	{ from: "zuri", subject: "Business meeting", date: "12-02-19" },
+	{ from: "gaby", subject: "surfing article", date: "23-02-20" },
+	{ from: "yosi", subject: "files sending", date: "11-10-18" },
+	{ from: "maly", subject: "homework exsercise", date: "23-02-18" },
+	{ from: "dalya", subject: "invitation for wedding", date: "15-07-19" },
+    { from: "zuri", subject: "Business meeting", date: "12-02-19" }];
+    
+var emailArrOut = [
+	{ from: "dani", subject: "surfing article", date: "23-02-20" },
+	{ from: "dani", subject: "files sending", date: "11-10-18" },
+	{ from: "dani", subject: "homework exsercise", date: "23-02-18" },
+	{ from: "dani", subject: "invitation for wedding", date: "15-07-19" },
+    { from: "dani", subject: "Business meeting", date: "12-02-19" }];
+    
+var contacts = [
+	{ firstName: "yosi", lastName: "buchnik", email: "yosibu@gamil.com" },
+	{ firstName: "maly", lastName: "ninio", email: "malyninio@gamil.com" },
+	{ firstName: "dalya", lastName: "buchnik", email: "dalyabuchnik@gamil.com" },
+	{ firstName: "zuri", lastName: "buchnik", email: "buchnikzuri@gamil.com" },
+	{ firstName: "nissan", lastName: "ninio", email: "nissanninio@gamil.com" }];
