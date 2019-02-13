f28a396e23e8ff2d087796017b763bf4f7f06238
aviyot <aviyot@gmail.com>
2019-02-13 08:17:09 +0200

    intial answer 52 html

---
+++ modules/03 Front End Development/Assignments/52/js52.js
@@ -0,0 +1,40 @@
+function sortTable(n) {
+    var rows, i, x, y;
+    var switchcount = 0;
+    var dir ="asc";
+    var table = document.querySelector("#contacts_table");
+    var switching = true;
+    var shouldSwitch;
+
+    while (switching){
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].querySelectorAll("TD")[n];
+        y = rows[i + 1].querySelectorAll("TD")[n];
+
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        switchcount ++; 
+      } else {
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
