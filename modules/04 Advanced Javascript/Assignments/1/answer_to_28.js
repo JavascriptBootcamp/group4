dcfdb2948161b27580f797bd74cfcbb951cd3a9a
DavidLev1 <44964757+DavidLev1@users.noreply.github.com>
2019-03-03 00:35:26 +0200

    Create answer_to_28.js

---
+++ modules/04 Advanced Javascript/Assignments/1/answer_to_28.js
@@ -0,0 +1,36 @@
+function Video(title, uploader, seconds) {
+
+    try {
+        this.title = title;
+        this.uploader = uploader;
+        this.seconds = seconds;
+        
+        Video.prototype.watch = function(seconds) {
+            try {
+                this.seconds = seconds;
+                console.log(`You watched all ${this.seconds} seconds of ${this.title}`);    
+            }
+            catch(error) {
+                console.log('Oops... An error occured');
+            }
+        }
+
+    }
+    catch(error) {
+        console.log('Oops... An error occured');
+    }
+    
+}
+
+var videoObj1 = new Video('Video 1', 'Uploader 1', 111);
+videoObj1.watch(7887878);
+
+
+var videoObj2 = new Video('Video 2', 'Uploader 2', 222);
+
+
+var data = [];
+
+for (var i = 3; i <= 7; i++) {
+    new Video('video ' + i, 'Uploader ' + i, 'i'+'i'+'i');
+}
