12d287c90ebbe9410080107655e8bd5112529779
gabybuchnik <44199356+gabybuchnik@users.noreply.github.com>
2019-03-03 15:36:35 +0200

    answer59html

---
+++ modules/03 Front End Development/Assignments/59/app.js
@@ -0,0 +1,24 @@
+
+var legend = document.querySelectorAll("legend");
+var expAll = document.getElementById("exp-all");
+expAll.onclick = expandAll;
+
+for (var key in legend) {
+    if (legend.hasOwnProperty(key)) {
+        legend[key].onclick = toggle;
+    }
+}
+
+function expandAll(e){
+    var collapse = document.querySelectorAll(".collapse"); 
+    for (var key in collapse) {
+        if (collapse.hasOwnProperty(key)) {
+            // collapse[key].classList.add("collapse");
+        }
+    }
+    expAll.innerText = "collapse all";
+}
+function toggle(e) {
+    var collapse = e.target.nextElementSibling.classList;
+    collapse.toggle("collapse");
+}
