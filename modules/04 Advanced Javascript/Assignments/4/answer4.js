3476d262ef579c633cc951e19be98cb0ba0f70f4
Amitay <amitayl@gmail.com>
2019-03-03 15:20:26 +0200

    Create answer4.js

---
+++ modules/04 Advanced Javascript/Assignments/4/answer4.js
@@ -0,0 +1,12 @@
+Array.prototype.max = function() {
+    return Math.max.apply(null, this);
+  };
+  
+  Array.prototype.min = function() {
+    return Math.min.apply(null, this);
+  };
+
+  var arr = [1, 22, 50, 7];
+var minimum = arr.min();
+var maximum = arr.max();
+console.log("Minimum:", minimum, "Maximum:", maximum);
