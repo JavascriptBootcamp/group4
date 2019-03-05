5ee4ba3e9f0aa5444f558003409af5493c71b302
YakirSolomon <yakir@soluto.com>
2019-03-05 10:59:58 +0200

    Delete answer.js

--- modules/04 Advanced Javascript/Assignments/3/answer.js
+++
@@ -1,7 +0,0 @@
-Array.prototype.mySliceFunction = function(start, end){
-    return this.slice(start,end+1);
-}
-
-var arr = [1, 2, 5, 7];
-var sliced = arr.mySliceFunction(1,2);
-console.log(sliced);
