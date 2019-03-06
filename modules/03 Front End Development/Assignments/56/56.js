59a0a35b9b43e688aa295aa11b485cb4603b4993
Atiya <matana@medcon.com>
2019-03-06 19:29:16 +0200

    front-dev-ex-56 answer update - slide  functionality

--- modules/03 Front End Development/Assignments/56/56.js
+++ modules/03 Front End Development/Assignments/56/56.js
@@ -2,35 +2,54 @@ var pics = ["imgs/pic1.jpg", "imgs/pic2.jpg", "imgs/pic3.jpg", "imgs/pic4.jpg", 
 var slider = document.querySelector("#slider");
 var big_pic_container = document.querySelector("#big_pic_container");
 
+function remove_hidden_Imgs() {
+    slider.querySelectorAll(".hide").forEach(hiddenImg => {
+        slider.removeChild(hiddenImg);
+    });
+
+    big_pic_container.querySelectorAll(".hide").forEach(hiddenImg => {
+        big_pic_container.removeChild(hiddenImg);
+    });
+}
 
 function move_left() {
+    remove_hidden_Imgs();
 
+    //change the pic array to be like the slider
+    var movedPic = pics.pop();
+    pics.unshift(movedPic);
+
+
+    //change the slider in the ui
+    var lastImg = slider.lastChild;
+    var clone_lastImg = lastImg.cloneNode();
+    lastImg.classList.add("hide");
+    slider.prepend(clone_lastImg);
+
+
+    //change the big img in the ui
+    var lastImg_big = big_pic_container.lastChild;
+    lastImg_big.classList.add("hide");
+    var newImg = creatImgElement(pics[0], "big_pic");
+    big_pic_container.prepend(newImg);
 }
 
 function move_right() {
+    remove_hidden_Imgs();
+
     //change the pic array to be like the slider
     var movedPic = pics.shift();
     pics.push(movedPic);
 
-    //check if the first img is hidden - if hidden remove from the list
-    if (slider.childNodes[0].classList.value.includes("hide")) {
-        slider.removeChild(slider.childNodes[0]);
-    }
-
     //change the slider in the ui
-    var firstImg = slider.childNodes[0];
+    var firstImg = slider.firstChild;
     var clone_firstImg = firstImg.cloneNode();
     firstImg.classList.add("hide");
     slider.appendChild(clone_firstImg);
 
-    //check if the first big img is hidden - if hidden remove from the list
-    if (big_pic_container.childNodes[0].classList.value.includes("hide")) {
-        big_pic_container.removeChild(big_pic_container.childNodes[0]);
-    }
-
     //change the big img in the ui
-    var firstImg = big_pic_container.childNodes[0];
-    firstImg.classList.add("hide");
+    var firstImg_big = big_pic_container.firstChild;
+    firstImg_big.classList.add("hide");
     var newImg = creatImgElement(pics[0], "big_pic");
     big_pic_container.appendChild(newImg);
 }
