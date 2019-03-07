7108a5b5e223ed8e147699f1e3278142d84a9df8
gabybuchnik <gabybuchnik@gmail.com>
2019-03-07 20:25:01 +0200

    answer1es6

---
+++ modules/05 ES6 and Typescript/1/answer16.js
@@ -0,0 +1,29 @@
+
+let arrOfBooks = [{
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        alreadyRead: true
+    },
+    {
+        title: "The Hobbit 2",
+        author: "J.R.R. Tolkien 2",
+        alreadyRead: false
+    },
+    {
+        title: "The Hobbit 3",
+        author: "J.R.R. Tolkien 3",
+        alreadyRead: true
+    },
+];
+
+let [b1,b2,b3] = arrOfBooks
+console.log(`deconstructing sample ${b1.title}`);
+
+arrOfBooks.forEach((item) => {
+    if (item.alreadyRead === true) {
+        console.log("You already read", item.title, "by", item.author);
+    }
+    else {
+        console.log("You still need to read", item.title, "by", item.author);
+    }
+});
