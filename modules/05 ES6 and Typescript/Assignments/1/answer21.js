92a949133b89ea97c6de58ced352f1473125094b
litalrab <38513468+litalrab@users.noreply.github.com>
2019-03-07 20:22:03 +0200

    Update answer21.js

--- modules/05 ES6 and Typescript/Assignments/1/answer21.js
+++ modules/05 ES6 and Typescript/Assignments/1/answer21.js
@@ -1,19 +1,16 @@
-const evenWords= (str) => {
-    // var input= 'aaa bbbb  '
-    // var strArr=input.split(' ');
-    let strArr=str.split(' ');
-    let strAns="";
-     for(let i=1;i<=strArr.length;++i)
-     {  
- 
-         if(i%2==0)
-         {
-             strAns+=`${strArr[i-1]} `;
-         }
- 
-     }
-     console.log(strAns);
-    return strAns;
-   }
+const max = (...arr) =>  {
 
-   evenWords('aaa bbbb  ')
+    let maxValue;
+    for (let i = 0; i < arr.length; i++) {
+        if (i === 0) {
+            maxValue = arr[0];
+        } else {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+        }
+    }
+    return maxValue;
+}
+const myArr=[-12, -34, 0, -56, -1]
+console.log(max(...myArr));
