cc83e236e782d0ca09e48f723c6a5c0c0789c81f
front21 <front21@sqlabs.co.il>
2019-03-07 20:23:03 +0200

    ES6 ans2

---
+++ modules/05 ES6 and Typescript/Assignments/1/ES6_q1.js
@@ -0,0 +1,38 @@
+
+//16
+var books = [
+    {title: "The Hobbit",author: "J.R.R. Tolkien",alreadyRead: true},
+    {title: "Harry Potter",author: "J.K Rolling",alreadyRead: false},
+    {title: "The Lord Of The Rings",author: "Yakir Solomon",alreadyRead: true},
+]
+
+for (let i=0;i<books.length;i++){
+    console.log(` ${books[i].title} by ${books[i].author}`);
+}
+
+for (let i=0;i<books.length;i++){
+
+    if(books[i].alreadyRead)
+        console.log(`You already read  ${books[i].title} by  ${books[i].author}`);
+    else
+        console.log(`You still need to read  ${books[i].title}  by ${books[i].author}`);
+}
+//21
+const max = (arr) => {
+
+    let maxValue;
+    let index = 0;
+    for(let elem of arr){
+        if(index === 0){
+            maxValue = elem;
+        }else{
+            if(elem > maxValue){
+                maxValue = elem;
+            }
+        }
+        index++;
+    }
+    return maxValue;
+}
+
+console.log(max([-12,-34,0,-56,-1]));
