63cecb4dc284a00f1588370eace0df2f16b5348b
gabybuchnik <44199356+gabybuchnik@users.noreply.github.com>
2019-03-10 07:53:37 +0200

    answer16js

--- modules/05 ES6 and Typescript/1/answer16.js
+++ modules/05 ES6 and Typescript/1/answer16.js
@@ -1,22 +1,22 @@
 
 let arrOfBooks = [{
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        alreadyRead: true
-    },
-    {
-        title: "The Hobbit 2",
-        author: "J.R.R. Tolkien 2",
-        alreadyRead: false
-    },
-    {
-        title: "The Hobbit 3",
-        author: "J.R.R. Tolkien 3",
-        alreadyRead: true
-    },
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    alreadyRead: true
+},
+{
+    title: "The Hobbit 2",
+    author: "J.R.R. Tolkien 2",
+    alreadyRead: false
+},
+{
+    title: "The Hobbit 3",
+    author: "J.R.R. Tolkien 3",
+    alreadyRead: true
+},
 ];
 
-let [b1,b2,b3] = arrOfBooks
+let [b1, b2, b3] = arrOfBooks
 console.log(`deconstructing sample ${b1.title}`);
 
 arrOfBooks.forEach((item) => {
