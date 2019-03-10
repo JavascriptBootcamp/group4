24ae25eba044ecfbb32975d756201c287f29322e
gabybuchnik <44199356+gabybuchnik@users.noreply.github.com>
2019-03-10 07:52:48 +0200

    answer21js

--- modules/05 ES6 and Typescript/1/answer21.js
+++ modules/05 ES6 and Typescript/1/answer21.js
@@ -1,26 +1,34 @@
-
 const max = (arr, ...args) => {
     let max = arr[0];
     console.log(`initial max ${max}`);
     arr.forEach((item, index) => {
-        if (args[index]) {
-            console.log(args[index]);
-        }
         if (index === 0) {
             return;
         }
-        else {
-            if (item > max) {
-                max = item;
-            }
+        else if (item > max) {
+            max = item;
         }
     });
     return max;
 }
+
+const max2 = (...args) => {
+    let max = args[0];
+    console.log(`initial max ${max}`);
+    args.forEach((item, index) => {
+        if (index === 0) {
+            return;
+        }
+        else if (item > max) {
+            max = item;
+        }
+    });
+    return max;
+}
+
 let arr1 = [12, 34, 56, 1];
 let arr2 = [-12, -34, 0, -56, -1];
 let [a, b, c, d] = arr1;
 let [e, f, g, h] = arr2;
 console.log(max([a, b, c, d]), "gaby", "test", "args");
-console.log(max([e, f, g, h]));
-
+console.log(max2(e, f, g, h));
