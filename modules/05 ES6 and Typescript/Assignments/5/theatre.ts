34bf6022bec718bd1b0cd26143f407061e65214f
gabybuchnik <gabybuchnik@gmail.com>
2019-03-11 15:06:14 +0200

    answer5

---
+++ modules/05 ES6 and Typescript/Assignments/5/theatre.ts
@@ -0,0 +1,47 @@
+export interface iTheatre {
+    openingHourSetter(openingHour: number): void;
+    closingHourSetter(closingHour: number): void;
+    calculatOpeningHours(): number;
+    printAllProperties(): void;
+}
+import { iTheatre } from "./theatre";
+export class Theatre implements iTheatre{
+    public _name: string;
+    public _totalSeats: number;
+    private _openingHour: number;
+    private _closingHour: number;
+    constructor(name, openingHour = 8, closingHour = 18, totalSeats?) {
+        this._name = name;
+        this._totalSeats = totalSeats;
+        this._openingHour = openingHour;
+        this._closingHour = closingHour;
+    }
+    public openingHourSetter(openingHour: number): void {
+        if (openingHour >= 8 && openingHour <= 12) {
+            this._openingHour = openingHour;
+        }
+    }
+    public closingHourSetter(closingHour: number): void {
+        if (closingHour >= 18 && closingHour <= 22) {
+            this._closingHour = closingHour;
+        }
+    }
+    public calculatOpeningHours(): number {
+        return this._closingHour - this._openingHour;
+    }
+    public printAllProperties(): void {
+        let log = document.getElementById("log");
+        log.innerText += `
+        name : ${this._name}
+        open at : ${this._openingHour}
+        closing at : ${this._closingHour}`;
+    }
+}
+let t1 = new Theatre("cinema city", 10, 21);
+let t2 = new Theatre("yes planet", undefined, undefined, 250);
+t1.openingHourSetter(13);
+t1.closingHourSetter(19);
+console.log(t1.calculatOpeningHours());
+console.log(t2.calculatOpeningHours());
+t1.printAllProperties();
+t2.printAllProperties();
