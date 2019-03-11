240651337c2183c91e3bb6e90580f1b9f4993a64
derldalfor100 <derldalfor100@gmail.com>
2019-03-11 13:10:48 +0200

    init

---
+++ modules/03 Front End Development/Assignments/55/script.js
@@ -0,0 +1,106 @@
+function Board(_images) {
+    var remainMatches = _images.length;
+    var images = _images.concat(_images);
+    var selectedFirstCard = null, selectedSecondCard = null;
+    var cards = [];
+    var matches = [];
+    (function shuffle() { //IIFE
+        var randomImage, randomIndex;
+        while (images.length > 0) {
+            randomIndex = Math.floor(Math.random() * images.length);
+            randomImage = images.splice(randomIndex, 1);
+            cards.push(randomImage[0].image);
+        }
+    })();
+    this.display = function (boardEl) {
+        var tr, td, btn, currentIndex = 0;
+        var table = document.createElement("table");
+        for (var i = 0; i < 4; i++) {
+            tr = document.createElement("tr");
+            for (var j = 0; j < 4; j++) {
+                td = document.createElement("td");
+                btn = document.createElement("button");
+                btn.id = "image" + currentIndex++;
+                btn.style.width = "100px";
+                btn.style.height = "100px";
+                btn.style.backgroundColor = "grey";
+                btn.onclick = onCardClick;
+                td.appendChild(btn);
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+        boardEl.appendChild(table);
+    }
+    function onCardClick(event) {
+        var currentCardIndex = Number(event.target.id.replace("image", ""));
+        showImage(event.target, currentCardIndex);
+        event.target.disabled = true;
+        setSelectedCards(currentCardIndex);
+    }
+    function setSelectedCards(currentCardIndex) {
+        if (selectedFirstCard !== null) {// changed because index 0
+            selectedSecondCard = currentCardIndex;
+            checkMatch();
+        }
+        else {
+            selectedFirstCard = currentCardIndex;
+        }
+    }
+    function showImage(element, currentCardIndex) {
+        element.style.backgroundImage = "url('" + cards[currentCardIndex] + "')";
+        element.style.backgroundSize = "cover";
+    }
+    function checkMatch(){
+        if (cards[selectedFirstCard] === cards[selectedSecondCard]) {
+            remainMatches--;
+            checkWin();
+            matches.push(selectedFirstCard);
+            matches.push(selectedSecondCard);
+            selectedFirstCard = null;
+            selectedSecondCard = null;
+        }
+        else {
+            toggleState(true);
+            setTimeout(function(){
+                resetImages(document.getElementById("image" + selectedFirstCard), document.getElementById("image" + selectedSecondCard));
+                selectedFirstCard = null;
+                selectedSecondCard = null;
+                toggleState(false);
+            }, 1000);
+        }
+    }
+    function toggleState(enable) {
+        for (var i=0;i<cards.length;i++){
+            if (!matches.includes(i)){// the change
+                document.getElementById("image" + i).disabled = enable;
+            }
+        }
+    }
+    function resetImages(firstCardElement, secondCardElement) {
+        firstCardElement.disabled = false;
+        firstCardElement.style.backgroundImage = "";
+        secondCardElement.disabled = false;
+        secondCardElement.style.backgroundImage = "";
+    }
+    function checkWin() {
+        if (remainMatches === 0){
+            document.getElementById("message").innerHTML = "<h1>Moshiko is the King!</h1><h4>You Won!</h4>";
+        }
+    }
+}
+
+function Card(animal) {
+    this.image = animal + '.png';
+}
+
+var catCard = new Card('cat');
+var dogCard = new Card('dog');
+var goldfishCard = new Card('goldfish');
+var guineaPigCard = new Card('guinea_pig');
+var kittenCard = new Card('kitten');
+var mouseCard = new Card('mouse');
+var puppyCard = new Card('puppy');
+var rabbitCard = new Card('rabbit');
+var board = new Board([catCard, dogCard, goldfishCard, guineaPigCard, kittenCard, mouseCard, puppyCard, rabbitCard]);
+board.display(document.getElementById("board"));
