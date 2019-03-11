dd371bb5c3e8b236a3ad0bfb5683eaf22859f8fa
danielbr08@gmail.com <daniel.brosh@sapiens.com>
2019-03-11 16:38:56 +0200

    css_q_59

--- modules/03 Front End Development/Assignments/59/app.js
+++ modules/03 Front End Development/Assignments/59/app.js
@@ -14,28 +14,35 @@ function openOrCloseCat(e){
     for(var legend in posLegends){
         if(posLegends.hasOwnProperty(legend)){
             if(sign === '+'){
-                console.log(posLegends[legend]);
-                posLegends[legend].className = "positive";
+                console.log(posLegends[legend].id,posLegends[legend].className);
+                //posLegends[legend].className = "positive";
             }
             else{
-                console.log(posLegends[legend]);
+                console.log(posLegends[legend].id,posLegends[legend].className);
                 posLegends[legend].className = "negative";
-
             }
         }
     }
 
-    for(var legend in negLegends){
-        if(negLegends.hasOwnProperty(legend)){
-            if(sign === '+'){
-                console.log(negLegends[legend]);
-                negLegends[legend].className = "positive";
-            }
-            else{
-                console.log(negLegends[legend]);
-                negLegends[legend].className = "negative";
+    // for(var legend in negLegends){
+    //     if(negLegends.hasOwnProperty(legend)){
+    //         if(sign === '+'){
+    //             //negLegends[legend].className = "positive";
+    //         }
+    //         else{
+    //             //negLegends[legend].className = "negative";
 
-            }
-        }
+    //         }
+    //     }
+    // }
+}
+
+function hideOrShow(event){
+    let contentDiv = event.target.nextElementSibling;
+    if(contentDiv.className === "positive"){
+        contentDiv.className = "negative";
+    }
+    else{//(contentDiv.className === "negative"){
+        contentDiv.className = "positive";
     }
 }
