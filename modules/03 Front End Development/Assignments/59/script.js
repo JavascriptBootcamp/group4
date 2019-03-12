24ca63cb34cee03d5369b343a96c9c77142cd4a0
shimonhazan <44465329+shimonhazan@users.noreply.github.com>
2019-03-13 00:38:05 +0200

    Create script.js

---
+++ modules/03 Front End Development/Assignments/59/script.js
@@ -0,0 +1,32 @@
+    function toggle_visibility(id) {
+      var plusMin = document.querySelector("#" + id + " #plusMin");
+      var e = document.querySelector("#" + id + " .hidden");
+      if (e.style.display == 'block') {
+        e.style.display = 'none';
+        plusMin.innerText = "+";
+      }
+      else {
+        e.style.display = 'block';
+        plusMin.innerText = "-";
+      }
+    }
+    function Expand(event) {
+      if (event.target.value == "Expand All") {
+        var e = document.querySelectorAll(".hidden");
+        for (var i = 0; i < e.length; i++) {
+          e[i].style.display = 'block';
+          idParent = e[i].parentElement.id;
+          document.querySelector("#" + idParent + " #plusMin").innerText = "-";
+          event.target.value = "Collapse All";
+        }
+      }
+      else {
+        var e = document.querySelectorAll(".hidden");
+        for (var i = 0; i < e.length; i++) {
+          e[i].style.display = 'none';
+          idParent = e[i].parentElement.id;
+          document.querySelector("#" + idParent + " #plusMin").innerText = "+";
+          event.target.value = "Expand All";
+        }
+      }
+    }
