12385b44d3adab8de04ba8a3b7d0b22b0342261e
Shani Staretz <shakimi88@gmail.com>
2019-03-13 20:57:49 +0200

    myAnswer04_01

---
+++ modules/04 Advanced Javascript/Assignments/1/myAnswer29.js
@@ -0,0 +1,23 @@
+function Video(title, uploader, seconds){
+    this.title = title;
+    this.uploader = uploader;
+    this.seconds = seconds;
+
+    BankAccount.prototype.watch = function (sec) {
+        try {
+            console.log(`You watched all ${sec} seconds of ${this.title}!`);
+        } catch (error) {
+            console.log("Oops... An error occured");
+        }
+    }
+}
+
+var data = []
+
+for (var i = 0; i < 5; i++) {
+    var v = new Video("video "+i, "The neerd youtuber", 60 + Math.floor(Math.random() * (60)));
+    data.push(v);  
+}
+
+data[0].watch(5);
+data[1].watch(24);
