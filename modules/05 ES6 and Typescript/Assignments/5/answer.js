de672835f6bbbcdda836d53e8c6457908ec56c5e
derldalfor100 <derldalfor100@gmail.com>
2019-03-15 16:22:25 +0200

    DOM

--- modules/05 ES6 and Typescript/Assignments/5/answer.js
+++ modules/05 ES6 and Typescript/Assignments/5/answer.js
@@ -24,17 +24,17 @@ var Theatre = /** @class */ (function () {
         return this.closing_hour - this.opening_hour;
     };
     Theatre.prototype.printProperties = function () {
-        console.log('Name:', this.name);
-        console.log('Opening hour:', this.opening_hour);
-        console.log('Closing hour:', this.closing_hour);
+        document.body.innerHTML += "<h1>Name: " + this.name + "</h1>";
+        document.body.innerHTML += "<h1>Opening hour: " + this.opening_hour + "</h1>";
+        document.body.innerHTML += "<h1>Closing hour: " + this.closing_hour + "</h1>";
         if (this.total_seats)
-            console.log('Total seats:', this.total_seats);
+            document.body.innerHTML += "<h1>Total seats: " + this.total_seats + "</h1>";
     };
     return Theatre;
 }());
 var t1 = new Theatre('Habima', 7);
-var t2 = new Theatre('Lessin', 9, 19, 100);
-t1.getTotalHours();
-t2.getTotalHours();
+var t2 = new Theatre('Lessin', 9, 20, 100);
+alert(t1.name + ' opens ' + t1.getTotalHours() + ' hours.');
+alert(t2.name + ' opens ' + t2.getTotalHours() + ' hours.');
 t1.printProperties();
 t2.printProperties();
