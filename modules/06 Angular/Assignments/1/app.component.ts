95a56d81fd9283e7e81ff0ccfe59a6e1827ee253
DavidLev1 <44964757+DavidLev1@users.noreply.github.com>
2019-03-17 00:40:26 +0200

    Create app.component.ts

---
+++ modules/06 Angular/Assignments/1/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'answer 1 in module 06 Angular';
+
+  // 1. create a variable “x”
+  // 2. set its value as 5
+  // 3. pre - increment its value and assign the result to "x" 5 times
+  // 4. log the value of "x"
+  _x: number = 5;
+
+  constructor() {
+    this._x = 5;
+  }
+
+  ngOnInit() {
+    for (let i = 0; i < 5; i++) {
+      ++this._x;
+    }
+  }
+}
