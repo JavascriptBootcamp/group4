380bd467c947a38fec17f35b33598e260d818586
gabybuchnik <gabybuchnik@gmail.com>
2019-03-17 07:44:39 +0200

    fix

---
+++ modules/05 ES6 and Typescript/Assignments/6/myplayer.js
@@ -0,0 +1,131 @@
+"use strict";
+exports.__esModule = true;
+var Musician = /** @class */ (function () {
+    function Musician(name, age, instruments) {
+        this.name = name;
+        this.age = age;
+        this.instruments = instruments;
+        this.name = name;
+        this.age = age;
+        this.instruments = instruments;
+    }
+    Musician.prototype.play = function (song) {
+        console.log("musician : " + this.name + " is now playing " + song);
+    };
+    Musician.prototype.next = function () {
+        if (songs[index + 1]) {
+            index++;
+        }
+        else {
+            index = 0;
+        }
+        console.log("musician " + this.name + " is now playing " + songs[index]);
+    };
+    Musician.prototype.prev = function () {
+        if (songs[index - 1]) {
+            index--;
+        }
+        else {
+            index = songs.length - 1;
+        }
+        console.log("musician " + this.name + " is now playing " + songs[index]);
+    };
+    return Musician;
+}());
+exports.Musician = Musician;
+var Band = /** @class */ (function () {
+    function Band(name, musician) {
+        this.name = name;
+        this.musician = musician;
+        this.name = name;
+        this.musician = musician;
+    }
+    Band.prototype.play = function (song) {
+        console.log("band : " + this.name + " is now playing " + song);
+    };
+    Band.prototype.next = function () {
+        if (songs[index + 1]) {
+            index++;
+        }
+        else {
+            index = 0;
+        }
+        console.log("band " + this.name + " is now playing " + songs[index]);
+    };
+    Band.prototype.prev = function () {
+        if (songs[index - 1]) {
+            index--;
+        }
+        else {
+            index = songs.length - 1;
+        }
+        console.log("band " + this.name + " is now playing " + songs[index]);
+    };
+    return Band;
+}());
+exports.Band = Band;
+var Radio = /** @class */ (function () {
+    function Radio(brand, color, price) {
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+    }
+    Radio.prototype.play = function (song) {
+        console.log("radio : " + this.brand + " is now playing " + song);
+    };
+    Radio.prototype.next = function () {
+        if (songs[index + 1]) {
+            index++;
+        }
+        else {
+            index = 0;
+        }
+        console.log("radio " + this.brand + " is now playing " + songs[index]);
+    };
+    Radio.prototype.prev = function () {
+        if (songs[index - 1]) {
+            index--;
+        }
+        else {
+            index = songs.length - 1;
+        }
+        console.log("radio " + this.brand + " is now playing " + songs[index]);
+    };
+    return Radio;
+}());
+exports.Radio = Radio;
+var instrument;
+(function (instrument) {
+    instrument["Guitar"] = "Guitar";
+    instrument["Drums"] = "Drums";
+    instrument["Bass"] = "Bass";
+    instrument["None"] = "None";
+})(instrument = exports.instrument || (exports.instrument = {}));
+var color;
+(function (color) {
+    color["red"] = "red";
+    color["blue"] = "blue";
+    color["yellow"] = "yellow";
+})(color = exports.color || (exports.color = {}));
+var songs = ["song1", "song2", "song3", "song4",], index = 0;
+var module = require("./myplayer");
+var radio1 = new module.Radio("poineer", color.blue, 350);
+radio1.play(songs[index]);
+radio1.next();
+radio1.next();
+radio1.prev();
+var musician1 = new module.Musician("gaby", 30, instrument.Guitar);
+var musician2 = new module.Musician("yosi", 40, instrument.Drums);
+var musician3 = new module.Musician("maly", 37, instrument.None);
+var musician4 = new module.Musician("nissan", 46, instrument.Bass);
+musician1.play(songs[index]);
+musician2.prev();
+musician3.next();
+musician4.play(songs[index]);
+var band1 = new module.Band("bandRock", [musician1, musician2, musician3, musician4]);
+band1.play(songs[index]);
+band1.prev();
+band1.next();
