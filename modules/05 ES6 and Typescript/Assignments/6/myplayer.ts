0e4d5cefacca21b5425dd1e5a1f34dbcd8c93996
gabybuchnik <gabybuchnik@gmail.com>
2019-03-17 07:47:15 +0200

    fix

--- modules/05 ES6 and Typescript/Assignments/6/myplayer.ts
+++ modules/05 ES6 and Typescript/Assignments/6/myplayer.ts
@@ -1,7 +1,7 @@
 export interface Iplay {
     play(song: string): void;
-    next(): [string, void];
-    prev(): [string, void];
+    next(): void;
+    prev(): void;
 }
 export class Musician implements Iplay {
     constructor(public name: string, public age: number, public instruments: instrument) {
@@ -12,23 +12,23 @@ export class Musician implements Iplay {
     public play(song: string) {
         console.log(`musician : ${this.name} is now playing ${song}`);
     }
-    public next(): [string, void] {
+    public next(): void {
         if (songs[index + 1]) {
             index++;
         }
         else {
             index = 0;
         }
-        return [`musician : `, console.log(`${this.name} is now playing ${songs[index]}`)];
+        console.log(`musician ${this.name} is now playing ${songs[index]}`);
     }
-    public prev(): [string, void] {
+    public prev(): void {
         if (songs[index - 1]) {
             index--;
         }
         else {
             index = songs.length - 1;
         }
-        return [`musician : `, console.log(`${this.name} is now playing ${songs[index]}`)];
+        console.log(`musician ${this.name} is now playing ${songs[index]}`);
     }
 }
 export class Band implements Iplay {
@@ -39,23 +39,23 @@ export class Band implements Iplay {
     public play(song: string) {
         console.log(`band : ${this.name} is now playing ${song}`);
     }
-    public next(): [string, void] {
+    public next(): void {
         if (songs[index + 1]) {
             index++;
         }
         else {
             index = 0;
         }
-        return [`band : `, console.log(`${this.name} is now playing ${songs[index]}`)];
+        console.log(`band ${this.name} is now playing ${songs[index]}`);
     }
-    public prev(): [string, void] {
+    public prev(): void {
         if (songs[index - 1]) {
             index--;
         }
         else {
             index = songs.length - 1;
         }
-        return [`band : `, console.log(`${this.name} is now playing ${songs[index]}`)];
+        console.log(`band ${this.name} is now playing ${songs[index]}`);
     }
 }
 export class Radio implements Iplay {
@@ -67,23 +67,23 @@ export class Radio implements Iplay {
     public play(song: string) {
         console.log(`radio : ${this.brand} is now playing ${song}`);
     }
-    public next(): [string, void] {
+    public next(): void {
         if (songs[index + 1]) {
             index++;
         }
         else {
             index = 0;
         }
-        return [`radio :`, console.log(`${this.brand} is now playing ${songs[index]}`)];
+        console.log(`radio ${this.brand} is now playing ${songs[index]}`);
     }
-    public prev(): [string, void] {
+    public prev(): void {
         if (songs[index - 1]) {
             index--;
         }
         else {
             index = songs.length - 1;
         }
-        return [`radio :`, console.log(`${this.brand} is now playing ${songs[index]}`)];
+        console.log(`radio ${this.brand} is now playing ${songs[index]}`);
     }
 }
 export enum instrument {
@@ -102,19 +102,19 @@ let songs: Array<string> = ["song1", "song2", "song3", "song4",], index = 0;
 import * as module from "./myplayer";
 let radio1: module.Radio = new module.Radio("poineer", color.blue, 350);
 radio1.play(songs[index]);
-let [r] = radio1.next();
-[r] = radio1.next();
-[r] = radio1.prev();
+radio1.next();
+radio1.next();
+radio1.prev();
 let musician1: module.Musician = new module.Musician("gaby", 30, instrument.Guitar);
 let musician2: module.Musician = new module.Musician("yosi", 40, instrument.Drums);
 let musician3: module.Musician = new module.Musician("maly", 37, instrument.None);
 let musician4: module.Musician = new module.Musician("nissan", 46, instrument.Bass);
 musician1.play(songs[index]);
-let [m] = musician2.prev();
-[m] = musician3.next();
+musician2.prev();
+musician3.next();
 musician4.play(songs[index]);
 let band1: module.Band = new module.Band("bandRock", [musician1, musician2, musician3, musician4]);
 band1.play(songs[index]);
-let [b] = band1.prev();
-[b] = band1.next();
+band1.prev();
+band1.next();
 
