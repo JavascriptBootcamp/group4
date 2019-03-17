bf0d59b5e8e9973736c0f87c10c0953657e05306
shimonhazan <shimoonhazan@gmail.com>
2019-03-17 21:04:09 +0200

    temp-ans

---
+++ modules/06 Angular/Assignments/4/answer04-assinment12/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component ,OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'answer04-assinment12';
+  str:string;
+  strArr:string[];
+  strAns:string;
+  ans:string;
+  constructor(str:string){
+    this.str="";
+  }
+  ngOnInit(){
+    this.ans="sdas"
+    this.ans =this.evenWords('aaa bbbb  ')
+  }
+  evenWords(str:string):string {
+    // var input= 'aaa bbbb  '
+    // var strArr=input.split(' ');
+     this.strArr=str.split(' ');
+     this.strAns="";
+     for(let i=1;i<=this.strArr.length;++i)
+     {  
+ 
+         if(i%2==0)
+         {
+             this.strAns+=this.strArr[i-1]+" ";
+         }
+ 
+     }
+     //console.log(strAns);
+    return this.strAns;
+   }
+}
