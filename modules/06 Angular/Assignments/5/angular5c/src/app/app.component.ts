fb9e5795cc651f679e88cbdf3537053be3556eea
gabybuchnik <gabybuchnik@gmail.com>
2019-03-18 14:02:55 +0200

    angular5c

---
+++ modules/06 Angular/Assignments/5/angular5c/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  obj:object[];
+  constructor(){
+    this.obj = [
+      {href:"https://en.wikipedia.org/wiki/Amsterdam",imgSrc:"https://upload.wikimedia.org/wikipedia/commons/b/be/KeizersgrachtReguliersgrachtAmsterdam.jpg"},
+      {href:"https://en.wikipedia.org/wiki/Moon",imgSrc:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/631px-FullMoon2010.jpg"},
+      {href:"https://en.wikipedia.org/wiki/Tel_Aviv",imgSrc:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Tel_Aviv_Promenade_panoramics.jpg/800px-Tel_Aviv_Promenade_panoramics.jpg"},
+      {href:"https://en.wikipedia.org/wiki/Prague",imgSrc:"https://www.prague.eu/img/pragueeu/u/property/logo-for-share-v2.jpg"},
+      {href:"https://en.wikipedia.org/wiki/Holon",imgSrc:"https://www.israel21c.org/wp-content/uploads/2016/06/Photo-of-Beersheva-by-Leonard-ZhukovskyShutterstock.com_-1000x657.jpg"},
+      {href:"https://en.wikipedia.org/wiki/Amsterdam",imgSrc:"https://upload.wikimedia.org/wikipedia/commons/b/be/KeizersgrachtReguliersgrachtAmsterdam.jpg"},
+      {href:"https://en.wikipedia.org/wiki/Amsterdam",imgSrc:"https://upload.wikimedia.org/wikipedia/commons/b/be/KeizersgrachtReguliersgrachtAmsterdam.jpg"},
+      {href:"https://en.wikipedia.org/wiki/Amsterdam",imgSrc:"https://upload.wikimedia.org/wikipedia/commons/b/be/KeizersgrachtReguliersgrachtAmsterdam.jpg"}
+    ]
+  }
+}
