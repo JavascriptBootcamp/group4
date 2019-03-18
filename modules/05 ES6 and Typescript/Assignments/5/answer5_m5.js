25fa924098a854bc6dcd671da6cb34640ee53033
aviyot <aviyot@gmail.com>
2019-03-18 22:51:26 +0200

    answer 5 on module 5 es6 and Typescript

--- modules/05 ES6 and Typescript/Assignments/5/answer5_m5.js
+++ modules/05 ES6 and Typescript/Assignments/5/answer5_m5.js
@@ -3,20 +3,32 @@ var Thearte = /** @class */ (function () {
         if (openHour === void 0) { openHour = 8; }
         if (closeHour === void 0) { closeHour = 18; }
         this.name = name;
-        this.openHour = openHour;
-        this.closeHour = closeHour;
+        this.setOpen(openHour);
+        this.setClose(closeHour);
     }
+    Thearte.prototype.setOpen = function (openTime) {
+        if (openTime < 8 || openTime > 12)
+            this.openHour = 8;
+        else
+            this.openHour = openTime;
+    };
+    Thearte.prototype.setClose = function (closeTime) {
+        if (closeTime < 18 || closeTime > 22)
+            this.closeHour = 22;
+        else
+            this.closeHour = closeTime;
+    };
     Thearte.prototype.calcOpenDuring = function () {
         return this.closeHour - this.openHour;
     };
     Thearte.prototype.printToDom = function () {
-        document.body.innerHTML += "\n        <div>\n          <p>theatre's name : " + this.name + "</p>\n          <p>opening hour : " + this.openHour + "</p>\n          <p>closing hour :" + this.closeHour + "</p>\n       </div>\n        ";
+        document.body.innerHTML += "\n        <div>\n          <p>theatre's name :" + this.name + "</p>\n          <p>opening hour : " + this.openHour + "</p>\n          <p>closing hour :" + this.closeHour + "</p>\n       </div>\n        ";
     };
     return Thearte;
 }());
-var theatre1 = new Thearte("habima", 10, 21, 300);
+var theatre1 = new Thearte("habima", 10, 23, 300);
 console.log(theatre1.calcOpenDuring());
 theatre1.printToDom();
-var theatre2 = new Thearte("kameri", 9, 20, 500);
+var theatre2 = new Thearte("kameri", 7, 20);
 console.log(theatre2.calcOpenDuring());
 theatre2.printToDom();
--- modules/05 ES6 and Typescript/Assignments/5/answer5_m5.ts
+++ modules/05 ES6 and Typescript/Assignments/5/answer5_m5.ts
@@ -2,13 +2,32 @@ class Thearte {
 
     name :string;
     openHour:number; 
-    closeHour:number; 
+    closeHour:number;
+    seats?:number; 
 
-    constructor(name:string,openHour:number=8,closeHour:number=18,seats:number){
+    constructor(name:string, openHour:number = 8, 
+                closeHour:number = 18, seats?:number){
+        
         this.name = name;
-        this.openHour = openHour;
-        this.closeHour = closeHour;
+        this.setOpen(openHour);
+        this.setClose(closeHour);
     }
+
+      setOpen(openTime:number){
+        if (openTime < 8 || openTime > 12)
+          this.openHour = 8;
+        else
+        this.openHour = openTime;
+
+
+      }
+
+      setClose(closeTime:number){
+        if(closeTime < 18 || closeTime > 22)
+        this.closeHour = 22;
+        else
+        this.closeHour = closeTime;
+      }
 
     calcOpenDuring():number{
       return this.closeHour-this.openHour;
@@ -17,18 +36,18 @@ class Thearte {
     printToDom():void{
         document.body.innerHTML += `
         <div>
-          <p>theatre's name : ${this.name}</p>
-          <p>opening hour : ${this.openHour}</p>
-          <p>closing hour :${this.closeHour}</p>
+          <p>theatre's name: ${this.name}</p>
+          <p>opening hour :  ${this.openHour}</p>
+          <p>closing hour :  ${this.closeHour}</p>
        </div>
         `
     }
 }
 
-const theatre1 = new Thearte("habima",10 , 21,300);
+const theatre1 = new Thearte("Habima",10 , 23, 300);
 console.log(theatre1.calcOpenDuring());
 theatre1.printToDom();
 
-const theatre2 = new Thearte("kameri",9 , 20,500);
+const theatre2 = new Thearte("Kameri",7 ,20);
 console.log(theatre2.calcOpenDuring());
 theatre2.printToDom();
