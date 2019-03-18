40220acfc1013baa6ef81dc4733a360388039502
gabybuchnik <gabybuchnik@gmail.com>
2019-03-18 09:16:28 +0200

    angular4c

---
+++ modules/06 Angular/Assignments/4/angular4c/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string;
+  duration: number;
+  stars: string[];
+  constructor() {
+    this.title = ' "IT" ';
+    this.duration = 180;
+    this.stars = ["Bill", "Sophia", "Chosen"];
+  }
+}
