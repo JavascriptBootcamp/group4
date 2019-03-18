c543d851960b236b67ea2af12797c3d893b881bf
gabybuchnik <gabybuchnik@gmail.com>
2019-03-18 11:54:39 +0200

    fix to object

--- modules/06 Angular/Assignments/4/angular4b/src/app/app.component.ts
+++ modules/06 Angular/Assignments/4/angular4b/src/app/app.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string;
-  servings: number;
-  ingredients: string[];
+  recipe: object;
   constructor() {
-    this.title = "pizza",
-      this.servings = 13,
-      this.ingredients = ["water ", "sugar", "salt", "salt", "olive oil", "flour"]
+    this.recipe = {
+      title: "pizza",
+      servings: 13,
+      ingredients: ["water ", "sugar", "salt", "salt", "olive oil", "flour"]
+    }
   }
 }
