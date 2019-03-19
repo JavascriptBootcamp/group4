da06bdae608efa86111f33ca3c00a1953227599a
Moshiko <moshiko.albhari@gmail.com>
2019-03-19 15:01:34 +0200

    Create Ng Answer 2-3

---
+++ modules/06 Angular/Assignments/2/angular2-a3/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title: string = 'angular2-a3';
+  x: number;
+  y: number;
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+  }
+
+  ngOnInit(): void {
+    this.x = ++this.x;
+    this.x = ++this.x;
+    this.y = this.y++;
+    this.y = this.y++;
+  }
+}
