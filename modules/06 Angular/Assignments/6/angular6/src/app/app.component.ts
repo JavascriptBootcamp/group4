ca393f1fb9e20a9601c95ef4e5a8ea4385a3428c
gabybuchnik <gabybuchnik@gmail.com>
2019-03-20 09:23:35 +0200

    fix

--- modules/06 Angular/Assignments/6/angular6/src/app/app.component.ts
+++ modules/06 Angular/Assignments/6/angular6/src/app/app.component.ts
@@ -15,51 +15,5 @@ export class AppComponent implements OnInit {
   }
   ngOnInit() {
     this.obj.sort((a, b) => (a.firsName > b.firsName) ? 1 : ((a.firsName < b.firsName) ? -1 : 0));
-    let table = this.createTable();
-    this.insertValue(table);
-  }
-  createTable() {
-    let myApp = document.getElementById("my-app");
-    let table = document.createElement("table");
-    let tr = document.createElement("tr");
-    let thFirstName = document.createElement("th");
-    let thLastName = document.createElement("th");
-    let thAddress = document.createElement("th");
-    let thPhone = document.createElement("th");
-    let thMail = document.createElement("th");
-    thFirstName.innerText = "First Name";
-    thLastName.innerText = "Last Name";
-    thAddress.innerText = "Address";
-    thPhone.innerText = "Phone";
-    thMail.innerText = "Mail";
-    tr.appendChild(thFirstName);
-    tr.appendChild(thLastName);
-    tr.appendChild(thAddress);
-    tr.appendChild(thPhone);
-    tr.appendChild(thMail);
-    table.appendChild(tr);
-    myApp.appendChild(table);
-    return table;
-  }
-  insertValue(table) {
-    for (let key of this.obj) {
-      let tr = document.createElement("tr");
-      let tdFirstName = document.createElement("td");
-      let tdLastName = document.createElement("td");
-      let tdAddress = document.createElement("td");
-      let tdPhone = document.createElement("td");
-      let tdMail = document.createElement("td");
-      tdFirstName.innerText = `${key.firsName}`;
-      tdLastName.innerText = `${key.lastName}`;
-      tdAddress.innerText = `${key.address}`;
-      tdPhone.innerText = `${key.phone}`;
-      tdMail.innerText = `${key.mail}`;
-      tr.appendChild(tdFirstName);
-      tr.appendChild(tdLastName);
-      tr.appendChild(tdAddress);
-      tr.appendChild(tdPhone);
-      tr.appendChild(tdMail);
-      table.appendChild(tr);
-    }
   }
 }
