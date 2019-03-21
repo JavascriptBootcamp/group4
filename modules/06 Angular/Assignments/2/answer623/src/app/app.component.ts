a03dc1c67d64e5113ab1c93d8b12c50c3c05f0bb
litalrab <litalrab@gamil.com>
2019-03-21 12:07:39 +0200

    create

---
+++ modules/06 Angular/Assignments/2/answer623/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'answer623';
+   x:number;
+   y:number;
+   constructor()
+   {
+    this.x=0; 
+    this.y=0;
+
+   }
+   ngOnInit()
+   {
+
+    this.x = ++this.x;
+    this.x = ++this.x;
+    this.y = this.y++;
+    this.y = this.y++;
+
+
+   }
+
+}
