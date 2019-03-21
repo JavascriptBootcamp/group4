39a8428f9e8ae31436c39c0892bb0d4b17aa5920
Moshiko <moshiko.albhari@gmail.com>
2019-03-22 00:37:31 +0200

    Create NG Answer5-3

---
+++ modules/06 Angular/Assignments/5/angular5-a3/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular5-a3';
+  books: Book[] = [];
+  constructor() {
+    this.books.push(new Book("IT", "Stephen King", "September 15, 1986", "Viking", 138, "perfect author, perfect book."));
+    this.books.push(new Book("The Dark Tower: The Gunslinger", "Stephen King", "June 10, 1982", "Grant", 300, "perfect author, perfect series."));
+    this.books.push(new Book("The Dark Tower Book II: The Drawing of the Three", "Stephen King", "May, 1987", "Grant", 463, "perfect author, perfect series."));
+    this.books.push(new Book("The Dark Tower III: The Waste Lands", "Stephen King", "August 1991", "Grant", 512, "perfect author, perfect series."));
+  }
+
+}
+class Book {
+  private title: string;
+  private author: string;
+  private publicationDate: string;
+  private publisher: string;
+  private page: number;
+  private comments: string;
+  constructor(title: string, author: string, publicationDate: string, publisher: string, page: number, comments: string) {
+    this.title = title;
+    this.author = author;
+    this.publicationDate = publicationDate;
+    this.publisher = publisher;
+    this.page = page;
+    this.comments = comments;
+  }
+}
