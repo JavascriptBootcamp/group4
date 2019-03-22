060126320c23084724d111e23380b8e01644900e
Evyatar <eviadk@gmail.com>
2019-03-22 15:29:31 +0200

    Rename modules/06 Angular/Assignments/5/app.component.ts to modules/06 Angular/Assignments/5/16/app.component.ts

---
+++ modules/06 Angular/Assignments/5/16/app.component.ts
@@ -0,0 +1,16 @@
+import { Component ,OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent   {
+title="q 16";
+  books = [
+    {title: "The Hobbit",author: "J.R.R. Tolkien",alreadyRead: true},
+    {title: "Harry Potter",author: "J.K Rolling",alreadyRead: false},
+    {title: "The Lord Of The Rings",author: "Yakir Solomon",alreadyRead: true},
+]
+
+}
