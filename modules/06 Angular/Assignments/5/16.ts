3cb867de38dd4d3c9ad8a6092f6899119553260b
Malak Khamaissi <mkhmayse@gmail.com>
2019-03-23 18:50:22 +0200

    Create 16.ts

---
+++ modules/06 Angular/Assignments/5/16.ts
@@ -0,0 +1,20 @@
+import { Component , OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+ books:object[];
+
+      ngOnInit(){      
+   this.books = [
+    {title: "The Hobbit",author: "J.R.R. Tolkien",alreadyRead: true},
+    {title: "Harry Potter",author: "J.K Rolling",alreadyRead: false},
+    {title: "The Lord Of The Rings",author: "Yakir Solomon",alreadyRead: true},
+]
+   }
+
+
+}
