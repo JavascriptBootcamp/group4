017849aba7255f4b1287565063562702442eded4
Evyatar <eviadk@gmail.com>
2019-03-23 20:58:21 +0200

    Update app.component.ts

--- modules/06 Angular/Assignments/5/2/app.component.ts
+++ modules/06 Angular/Assignments/5/2/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent   {
   title = "links";
   
-  books = [
+  links = [
     {link:"https://en.wikipedia.org/wiki/Amsterdam",img: "https://upload.wikimedia.org/wikipedia/commons/b/be/KeizersgrachtReguliersgrachtAmsterdam.jpg"},
     {link:"https://en.wikipedia.org/wiki/Moon",img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/631px-FullMoon2010.jpg"},
     {link:"https://en.wikipedia.org/wiki/Tel_Aviv",img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Tel_Aviv_Promenade_panoramics.jpg/800px-Tel_Aviv_Promenade_panoramics.jpg"}
