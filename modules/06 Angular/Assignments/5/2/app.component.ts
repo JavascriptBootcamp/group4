7f80bf77980e67753e0b1f8a0dce3f5fb9cd28a4
Evyatar <eviadk@gmail.com>
2019-03-23 20:53:09 +0200

    Create app.component.ts

---
+++ modules/06 Angular/Assignments/5/2/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent   {
+  title = "links";
+  
+  books = [
+    {link:"https://en.wikipedia.org/wiki/Amsterdam",img: "https://upload.wikimedia.org/wikipedia/commons/b/be/KeizersgrachtReguliersgrachtAmsterdam.jpg"},
+    {link:"https://en.wikipedia.org/wiki/Moon",img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/631px-FullMoon2010.jpg"},
+    {link:"https://en.wikipedia.org/wiki/Tel_Aviv",img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Tel_Aviv_Promenade_panoramics.jpg/800px-Tel_Aviv_Promenade_panoramics.jpg"}
+
+]
+
+
+}
