df4cd34572f406270773db6906540524d96b53e4
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-03-23 14:15:54 +0200

    Update app.component.ts

--- modules/06 Angular/Assignments/4/recipe/src/app/app.component.ts
+++ modules/06 Angular/Assignments/4/recipe/src/app/app.component.ts
@@ -1,45 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Irecipe{
+  title : string,
+  servings : number,
+  ingredients : string[]
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-interface Irecipe{
-  title : string,
-  servings : number
-  ingredients : string[]
-}
-
-export class AppComponent {
+export class AppComponent{
   
- public recipe:Irecipe
+public recipe:Irecipe;
 
- constructor(){
-this.recipe.title = "pizza";
-this.recipe.servings = 13
-this.recipe.ingredients = ["water ","sugar","salt","salt","olive oil","flour"]
- }
+constructor(){
+this.recipe = {
+title : "pizza",
+servings : 13,
+ingredients : ["water ","sugar","salt","salt","olive oil","flour"]
+}  
 
-switching(recipe:Irecipe):any{
-  for( let key in recipe )
-  {
-      switch (key)
-      {
-        case "servings":
-          return "Serves: "+ recipe[key];
-          break;
-        case "ingredients":
-         for(let item of recipe.ingredients)
-          {
-            return item;
-              
-          }
-          break;
-          default:
-          console.log(recipe[key]);
-      }
-  }
 }
+
 }
