8d5c79c175479e1cec94e3d264b9d39087e55589
MatanDiner <matand0390@gmail.com>
2019-03-25 12:59:25 +0200

    06_q_7 update

--- modules/06 Angular/Assignments/7/bloodPressure/src/app/app.bloodpressure.ts
+++ modules/06 Angular/Assignments/7/bloodPressure/src/app/app.bloodpressure.ts
@@ -4,10 +4,10 @@ import {Pipe,PipeTransform} from '@angular/core'
     name:'normalbloodpressure'
 })
 export class bloodpressure implements PipeTransform{
-    transform(val:number):boolean{
+    transform(val:number):string{
         let isNormal = false;
         (val >= 80 && val <= 120) ? isNormal = true : isNormal;
         (val <= 80 || val >= 120) ? isNormal : isNormal = true;
-         return isNormal;
+         return isNormal?" has Normal blood Pressure":"has Not Normal blood Pressure";
     }
 }
