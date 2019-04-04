c7096d161367f8b4facaed87bbb62187a1906fd7
matan <front4@sqlabs.co.il>
2019-04-04 20:53:37 +0300

    angular-ex-15 answer

---
+++ modules/06 Angular/Assignments/15/answer/src/app/components/image/image.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.css']
+})
+export class ImageComponent implements OnInit {
+  
+  @Input() src:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
