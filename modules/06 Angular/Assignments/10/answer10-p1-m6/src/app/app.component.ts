92206cea1e1e42bcd98e49668dc3c8cde5a914bf
aviyot <aviyot@gmail.com>
2019-04-04 07:55:22 +0300

    intial answer

--- modules/06 Angular/Assignments/10/answer10-p1-m6/src/app/app.component.ts
+++ modules/06 Angular/Assignments/10/answer10-p1-m6/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'answer10-p1-m6';
+  frName="avi";
+  lsName ="get";  
+  hobbies = ["swim","read","learn","run"];
+ 
+ 
+  onSubmit(data:NgForm){
+    console.log(data);
+  }
 }
--- modules/06 Angular/Assignments/10/answer10-p1-m6/src/app/app.module.ts
+++ modules/06 Angular/Assignments/10/answer10-p1-m6/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
