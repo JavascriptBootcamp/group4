e8156d3328bf9df828b5ed6ead63ecdea1034093
EvyatarD <eviadk@gmail.com>
2019-04-05 13:14:36 +0300

    add 15

--- modules/06 Angular/Assignments/15/Instush/src/app/app.component.ts
+++ modules/06 Angular/Assignments/15/Instush/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 export class Img {
-  like:number;
-  image:string;
+  like: number;
+  image: string;
 }
 
 @Component({
@@ -13,12 +13,23 @@ export class Img {
 export class AppComponent {
   title = 'Instush';
   images: Img[];
+  condition: boolean = false;
+  localS:string = null;
 
-  constructor(){
-    this.images=[{like:0,image:"https://www.masa.co.il/wp-content/uploads/2016/02/ca628b9995e09a044f73cbb2a60e657d_1-open.jpg"},
-    {like:0,image:"https://img.mako.co.il/2017/12/28/pisaphotography_x5.jpg"}
-    ,{like:0,image:"https://edge.media.datahc.com/HI402816181.jpg"}];
+  constructor() {
 
+    this.images = [{ like: 0, image: "https://www.edreams.com/images/landingpages/vacation/640x480/london_640x480.jpg" },
+    { like: 0, image: "http://london.eazycity.com/media/gallery/image/l/o/london_city1.jpg" }
+      , { like: 0, image: "https://magazine.northeast.aaa.com/wp-content/uploads/2019/01/2019_TRA_GettyImages_853177728_London.jpg" }
+      , { like: 0, image: "https://www.cisabroad.com/wp-content/uploads/2016/10/6376773707_323520b2d2_b-640x480.jpg" }
+      , { like: 0, image: "https://imgcld.yatra.com/ytimages/image/upload/t_seo_Holidays_w_640_h_480_c_fill_g_auto_q_auto:good_f_jpg/v1444998622/London.jpg" }
+      , { like: 0, image: "https://img.etimg.com/photo/60060406/london-big-ben2_reuters.jpg" }];
+
+    this.localS = localStorage.getItem("likes");
   }
+
+
+
+
 
 }
--- modules/06 Angular/Assignments/15/Instush/src/app/image/image.component.ts
+++ modules/06 Angular/Assignments/15/Instush/src/app/image/image.component.ts
@@ -9,24 +9,23 @@ export class ImageComponent implements OnInit {
 
   @Input() img: Img;
 
-  constructor() { }
-
   clickLike(img: Img) {
     img.like++;
+    localStorage.setItem("likes",document.getElementById("wraper").innerHTML );
   }
-
 
   public test() {
     if ((this.img.like > 0) && (this.img.like < 10))
       return "medium";
     else if (this.img.like >= 10)
-    return "large";
-      else return "small";
+      return "large";
+    else return "small";
 
   }
-
 
   ngOnInit() {
+
   }
 
+ 
 }
