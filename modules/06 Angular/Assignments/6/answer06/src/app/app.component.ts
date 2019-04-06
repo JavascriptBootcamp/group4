05ed52d6b89e25b298d9da3c43cc344e5dd3efcd
mariyastash <mariyastashevski@gmail.com>
2019-04-06 12:20:53 +0300

    old solution

--- modules/06 Angular/Assignments/6/answer06/src/app/app.component.ts
+++ modules/06 Angular/Assignments/6/answer06/src/app/app.component.ts
@@ -8,3 +8,52 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'answer06';
 }
+
+
+
+/*use group4/Modules/03 Front End Development/Assignments/11
+
+generate the table using Typescript
+
+sort by first name using Array.prototype.sort().
+
+display the results in an Angular application.*/
+
+
+
+/*<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Person</title>
+</head>
+<body>
+    <table border="1">
+        <tr bgcolor="#FFCCFF">
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email Address</th>
+        </tr>
+
+        <tr>
+            <td>Mariya</td>
+            <td>Stash</td>
+            <td>NY</td>
+            <td>+9087865914</td>
+            <td><a href="mailto:user@mail.com">user@mail.com</a></td>
+        </tr>
+
+        <tr>
+                <td>Sam</td>
+                <td>Smith</td>
+                <td>LA</td>
+                <td>+99843216514</td>
+                <td><a href="mailto:sam@gookle.com">sam@gookle.com</a></td>
+            </tr>
+    </table>
+</body>
+</html> */
