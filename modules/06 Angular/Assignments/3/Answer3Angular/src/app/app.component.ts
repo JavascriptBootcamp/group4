a24bf921b0e89193a4cc3353d30e91893fb61d7b
Guyagiv <guyagiv@gmail.com>
2019-04-07 23:37:15 +0300

    first commit

---
+++ modules/06 Angular/Assignments/3/Answer3Angular/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+  food: string[];
+  beverage: string[];
+  colors: string[];
+
+  constructor()
+  {
+    this.food = ['hamburger', 'BBQ', 'salad'];
+    this.beverage = ['beer', 'vodka', 'coke'];
+    this.colors = ['blue', 'green', 'black'];
+  }
+
+  converToSuffixNumber(num){
+    switch (num) {
+      case 1:
+        return 'st'
+      case 2:
+        return 'nd'
+      case 3:
+        return 'rd';
+      case 4:
+        return 'th';
+    }
+  }
+}
