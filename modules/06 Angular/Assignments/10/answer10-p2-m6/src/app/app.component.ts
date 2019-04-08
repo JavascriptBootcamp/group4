95d6862e042f6ae2dcf7d1750c57660936e1a086
aviyot <aviyot@gmail.com>
2019-04-08 06:06:27 +0300

    answer 10 m6 p1  cont...

--- modules/06 Angular/Assignments/10/answer10-p2-m6/src/app/app.component.ts
+++ modules/06 Angular/Assignments/10/answer10-p2-m6/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'answer10-p2-m6';
+export class AppComponent implements OnInit{
+
+  registrationForm:FormGroup;
+
+  ngOnInit(){
+    this.registrationForm = new FormGroup({
+      personInformtion:new FormGroup({
+             fristName:new FormControl(null),
+              lastName:new FormControl(null),
+              age:new FormControl(null)
+      }),
+      credentials:new FormGroup({
+        userName:new FormControl(null),
+        password:new FormControl(null)
+      }),
+      contact:new FormGroup({
+        tel:new FormControl(null),
+        email:new FormControl(null)
+      }),
+      misc:new FormGroup({
+        comments:new FormControl(null),
+        hobbies:new FormControl(null)
+      })
+    })
+  }
+
+  onSubmit(){
+   console.log(this.registrationForm);
+  }
 }
