9abd3d63f241df79a80e75fbbd77b190924c7ab9
aviyot <aviyot@gmail.com>
2019-04-08 18:32:00 +0300

    answer cont...

--- modules/06 Angular/Assignments/11/answer11-p1-m6/src/app/app.component.ts
+++ modules/06 Angular/Assignments/11/answer11-p1-m6/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Books } from './books'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'answer11-p1-m6';
+  favoritesBooks: Books[]=[];
+  
+  ngOnInit() {
+    this.favoritesBooks.push({
+      title: 'המוסד השמיימי',
+      authors: ['אייזק אסימוב'],
+      publishedDate: 1951,
+      publishers: ['עם עובד'],
+      pageCount: 300,
+      comments: '>אולי זה קצת קלישאתי, אבל הספר הזה, שיצא בשנת 1951 ותורגם לעברית בשנת 1978 הצית לבבות של מיליוני קוראים ברחבי העולם, ועורר תהייה לגבי גלקסיות רחוקות הרבה לפני מלחמת הכוכבים'
+    });
+    this.favoritesBooks.push({
+      title: 'המוסד השמיימי',
+      authors: ['אייזק אסימוב'],
+      publishedDate: 1951,
+      publishers: ['עם עובד'],
+      pageCount: 300,
+      comments: '>אולי זה קצת קלישאתי, אבל הספר הזה, שיצא בשנת 1951 ותורגם לעברית בשנת 1978 הצית לבבות של מיליוני קוראים ברחבי העולם, ועורר תהייה לגבי גלקסיות רחוקות הרבה לפני מלחמת הכוכבים'
+    });
+    this.favoritesBooks.push({
+      title: 'המוסד השמיימי',
+      authors: ['אייזק אסימוב'],
+      publishedDate: 1951,
+      publishers: ['עם עובד'],
+      pageCount: 300,
+      comments: '>אולי זה קצת קלישאתי, אבל הספר הזה, שיצא בשנת 1951 ותורגם לעברית בשנת 1978 הצית לבבות של מיליוני קוראים ברחבי העולם, ועורר תהייה לגבי גלקסיות רחוקות הרבה לפני מלחמת הכוכבים'
+    });
+    this.favoritesBooks.push({
+      title: 'המוסד השמיימי',
+      authors: ['אייזק אסימוב'],
+      publishedDate: 1951,
+      publishers: ['עם עובד'],
+      pageCount: 300,
+      comments: '>אולי זה קצת קלישאתי, אבל הספר הזה, שיצא בשנת 1951 ותורגם לעברית בשנת 1978 הצית לבבות של מיליוני קוראים ברחבי העולם, ועורר תהייה לגבי גלקסיות רחוקות הרבה לפני מלחמת הכוכבים'
+    });
+    this.favoritesBooks.push({
+      title: 'המוסד השמיימי',
+      authors: ['אייזק אסימוב'],
+      publishedDate: 1951,
+      publishers: ['עם עובד'],
+      pageCount: 300,
+      comments: '>אולי זה קצת קלישאתי, אבל הספר הזה, שיצא בשנת 1951 ותורגם לעברית בשנת 1978 הצית לבבות של מיליוני קוראים ברחבי העולם, ועורר תהייה לגבי גלקסיות רחוקות הרבה לפני מלחמת הכוכבים'
+    })
+  }
 }
---
+++ modules/06 Angular/Assignments/11/answer11-p1-m6/src/app/books.ts
@@ -0,0 +1,11 @@
+
+export class Books{
+
+title : string;
+authors : string[];
+publishedDate : number;
+publishers : string[];
+pageCount : number;
+comments : string;
+
+}
