ed013a6bafbe8eae2c6e6ce159b691764d53ec65
gabybuchnik <gabybuchnik@gmail.com>
2019-04-08 22:56:05 +0300

    add localStorage

--- modules/06 Angular/Assignments/16/angular16a/src/app/app.component.ts
+++ modules/06 Angular/Assignments/16/angular16a/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   thumbnails: thumbnail[] = [];
   bigPicture: thumbnail;
   constructor() {
-    this.bigPicture = {id:null ,src:null};
+    this.bigPicture = { id: null, src: null };
   }
   ngOnInit() {
     this.thumbnails = [
@@ -28,13 +28,22 @@ export class AppComponent implements OnInit {
       { id: "tm8", src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsQN-mcmQVIi2HLnAZhvJ-JCYZ1Yf7EtNlA1wVBf8gAYkh6T2r" },
       { id: "tm9", src: "https://www.barraquer.com/wp-content/uploads/2017/07/el-ojo-del-surfista-p-08-2017.jpg" }
     ];
-    this.bigPicture = {
-      id : "tm1",
-      src : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqsw9q1gKQhk3M3krgRtn9vCgYaKDvQD_GgJdL-y-jNiJRLbgC"
+    if (localStorage.getItem("gallery")) {
+      this.bigPicture = {
+        id : JSON.parse(localStorage.getItem("gallery"))[0],
+        src : JSON.parse(localStorage.getItem("gallery"))[1]
+      }
+    }
+    else {
+      this.bigPicture = {
+        id: "tm1",
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqsw9q1gKQhk3M3krgRtn9vCgYaKDvQD_GgJdL-y-jNiJRLbgC"
+      }
     }
   }
   onaddPicEvent(img: thumbnail) {
     this.bigPicture = { id: img.id, src: img.src }
+    this.saveToStorage();
   }
   onClickBackEvent(id: string) {
     for (let key of this.thumbnails) {
@@ -42,6 +51,7 @@ export class AppComponent implements OnInit {
         this.bigPicture.src = key.src;
       }
     }
+    this.saveToStorage();
   }
   onClickforwardEvent(id: string) {
     for (let key of this.thumbnails) {
@@ -49,5 +59,9 @@ export class AppComponent implements OnInit {
         this.bigPicture.src = key.src;
       }
     }
+    this.saveToStorage();
+  }
+  saveToStorage() {
+    localStorage.setItem("gallery", JSON.stringify([this.bigPicture.id, this.bigPicture.src]));
   }
 }
