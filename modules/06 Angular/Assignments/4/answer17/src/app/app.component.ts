1287e11de2816c0224d55e28a9e510f4a58295b6
Shani Staretz <shakimi88@gmail.com>
2019-04-09 19:30:19 +0300

    answer angular 04, exs17

---
+++ modules/06 Angular/Assignments/4/answer17/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'answer17';
+  favoriteMovie = {
+    title: ' "IT" ',
+    duration: 180,
+    stars: ["Bill", "Sophia", "Chosen"]
+}
+}
