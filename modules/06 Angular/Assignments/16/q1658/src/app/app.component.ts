3a46cd580d2f6761f064df9af4be9f514cd4c4bd
danielbr08@gmail.com <daniel.brosh@sapiens.com>
2019-04-11 08:48:11 +0300

    06_q_16_58

--- modules/06 Angular/Assignments/16/q1658/src/app/app.component.ts
+++ modules/06 Angular/Assignments/16/q1658/src/app/app.component.ts
@@ -28,6 +28,18 @@ export class AppComponent {
     }
   }
   ngOnInit() {
+    this.videos = [
+      { title: "title1 funny movie ", desc: "short film ..... click and take a look", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "simply dummy text of the printing and typesetting industry. Lorem simply dummy text of the printing and typesetting industry. Lorem", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title2", desc: "desc2", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist2", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title3", desc: "desc3", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist3", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title4", desc: "desc4", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist4", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title5", desc: "desc5", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist5", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title6", desc: "desc6", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist6", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title7", desc: "desc7", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist7", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title8", desc: "desc8", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist8", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title9", desc: "desc9", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist9", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+      { title: "title10", desc: "desc10", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist10", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
+    ];
     let dataStorge = JSON.parse(localStorage.getItem("Itube"));
     //this.dataStorge = JSON.parse(localStorage.getItem("Itube"));
     if (dataStorge) {
@@ -40,22 +52,14 @@ export class AppComponent {
           imageSrc: dataStorge.imageSrc
         }
     }
-    this.videos = [
-      { title: "title1 funny movie ", desc: "short film ..... click and take a look", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "simply dummy text of the printing and typesetting industry. Lorem simply dummy text of the printing and typesetting industry. Lorem", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title2", desc: "desc2", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist2", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title3", desc: "desc3", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist3", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title4", desc: "desc4", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist4", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title5", desc: "desc5", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist5", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title6", desc: "desc6", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist6", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title7", desc: "desc7", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist7", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title8", desc: "desc8", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist8", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title9", desc: "desc9", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist9", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-      { title: "title10", desc: "desc10", url: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", artist: "artist10", imageSrc: "http://downloadicons.net/sites/default/files/windows-media-player-icon-66464.png" },
-    ]
+    else{
+      this.player = this.videos[0];
+    }
   }
 
   onChangeVideoEvent(video: Video){
     this.player = video;
+    localStorage.setItem("Itube",JSON.stringify(video));
   }
 
 
