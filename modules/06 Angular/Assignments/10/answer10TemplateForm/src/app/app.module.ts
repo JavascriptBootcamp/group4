0e5b4eeb1fa5c9ef11f05d44de4924b3c4999357
amitayl@gmail.com <amitayl@gmail.com>
2019-04-12 15:58:59 +0300

    I was acidently implemented both form the same way. this commit correct this.

--- modules/06 Angular/Assignments/10/answer10TemplateForm/src/app/app.module.ts
+++ modules/06 Angular/Assignments/10/answer10TemplateForm/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
@@ -10,7 +11,8 @@ import { TemplateFormComponent } from './template-form/template-form.component';
     TemplateFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- modules/06 Angular/Assignments/10/answer10TemplateForm/src/app/template-form/template-form.component.ts
+++ modules/06 Angular/Assignments/10/answer10TemplateForm/src/app/template-form/template-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-template-form',
@@ -7,29 +7,5 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent {
-
-  registrationForm = new FormGroup({
-    personalInformation : new FormGroup({
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      age : new FormControl('')
-    }),
-    credential: new FormGroup({
-      userName: new FormControl(''),
-      password: new FormControl(''),
-    }),
-    contact: new FormGroup({   
-      phone: new FormControl(''),
-      email: new FormControl('')
-    }),
-    misc: new FormGroup({   
-      comments: new FormControl(''),
-      hobbies: new FormControl('')    
-    })
-  });
-
-  onSubmit () {
-
-  }
 
 }
