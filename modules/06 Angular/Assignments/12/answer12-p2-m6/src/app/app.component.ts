f66b58138f3b4d580447119aa30302c5cbaabdb1
aviyot <aviyot@gmail.com>
2019-04-12 10:02:30 +0300

    answer12 2 m6

--- modules/06 Angular/Assignments/12/answer12-p2-m6/src/app/app.component.ts
+++ modules/06 Angular/Assignments/12/answer12-p2-m6/src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+interface Iactor {
+  name: string;
+  imdb: string;
+  age: number;
+  img: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'answer12-p2-m6';
+export class AppComponent implements OnInit {
+  actors: Iactor[] = [];
+
+  ngOnInit() {
+    this.actors = [
+      {
+        name: "Arnold Schwarzenegge",
+        imdb: "https://www.imdb.com/name/nm0000216",
+        age: 71,
+        img: "https://m.media-amazon.com/images/M/MV5BMTI3MDc4NzUyMV5BMl5BanBnXkFtZTcwMTQyMTc5MQ@@._V1_.jpg"
+      },
+      {
+        name: "Bruce Lee ",
+        imdb: "https://www.imdb.com/name/nm0000045",
+        age: 1973,
+        img: "https://m.media-amazon.com/images/M/MV5BMTUwMTg4NDkzMV5BMl5BanBnXkFtZTYwMjU4Nzc2._V1_.jpg"
+      },
+      {
+        name: "Sylvester Stallone",
+        imdb: "https://www.imdb.com/name/nm0000230",
+        age: 72,
+        img: "https://m.media-amazon.com/images/M/MV5BMTQwMTk3NDU2OV5BMl5BanBnXkFtZTcwNTA3MTI0Mw@@._V1_.jpg"
+      },
+      {
+        name: "Jean-Claude Van Damme",
+        imdb: "https://www.imdb.com/name/nm0000241/",
+        age: 58,
+        img: "https://m.media-amazon.com/images/M/MV5BMTA2OTE1Njg4NjVeQTJeQWpwZ15BbWU3MDAyNjU4MDM@._V1_SY1000_CR0,0,812,1000_AL_.jpg"
+      },
+      {
+        name: "Chuck Norris",
+        imdb: "https://www.imdb.com/name/nm0001569/",
+        age: 78,
+        img: "https://m.media-amazon.com/images/M/MV5BMTU4NjY3NzgyM15BMl5BanBnXkFtZTcwODI4OTEzNA@@._V1_SY1000_CR0,0,789,1000_AL_.jpg"
+      }
+
+    ]
+  }
 }
+
--- modules/06 Angular/Assignments/12/answer12-p2-m6/src/app/app.module.ts
+++ modules/06 Angular/Assignments/12/answer12-p2-m6/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ActorComponent } from './actor/actor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActorComponent
   ],
   imports: [
     BrowserModule
