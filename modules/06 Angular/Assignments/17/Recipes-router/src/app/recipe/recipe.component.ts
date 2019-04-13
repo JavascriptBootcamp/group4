8b101131436d5a66fe0a49b8e36ba79d6e4e3841
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-04-14 00:50:24 +0300

    Update recipe.component.ts

--- modules/06 Angular/Assignments/17/Recipes-router/src/app/recipe/recipe.component.ts
+++ modules/06 Angular/Assignments/17/Recipes-router/src/app/recipe/recipe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Input } from '@angular/core';
-import {Recipe} from '../recipe.module'
+import {Recipe} from '../recipe.module';
+import {ActivatedRoute} from '@angular/router'
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
@@ -7,11 +8,18 @@ import {Recipe} from '../recipe.module'
 })
 export class RecipeComponent implements OnInit {
 
-  @Input() recipe:Recipe;
-
-  constructor() {
-
-
+  recipeId:number;
+  recipes:Recipe[];
+  constructor(private route:ActivatedRoute) {
+    this.route = route;
+    this.recipeId = this.route.snapshot.params.id;
+    this.recipes = [
+  
+      {name:"Creamy Pasta Toss with Smoked Sausage",ings:["1 (8 ounce) package uncooked pappardelle pasta","1 tablespoon olive oil"],id:"0"},
+      {name:"Easy Meatloaf",ings:["1 1/2 pounds ground beef","1 tablespoon olive oil"],id:"1"},
+      {name:"humburger",ings:["cheese","bacon"],id:"2"}
+ 
+     ];
    }
 
   ngOnInit() {
