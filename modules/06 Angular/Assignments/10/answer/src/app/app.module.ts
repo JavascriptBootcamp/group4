50b96fafaf446b8de65d73b8004cc237e19a115b
Atiya <matana@medcon.com>
2019-04-14 12:17:21 +0300

    angular-ex-10 add reactive form

--- modules/06 Angular/Assignments/10/answer/src/app/app.module.ts
+++ modules/06 Angular/Assignments/10/answer/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserTemplateDrivenFormComponent } from './components/user-template-driven-form/user-template-driven-form.component';
+import { UserReactiveFormComponent } from './components/user-reactive-form/user-reactive-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserTemplateDrivenFormComponent
+    UserTemplateDrivenFormComponent,
+    UserReactiveFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
