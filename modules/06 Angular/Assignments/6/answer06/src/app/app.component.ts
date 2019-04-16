2fe3eea5bae4f8e8d54a5b0a27a940a1183667e0
mariyastash <mariyastashevski@gmail.com>
2019-04-16 04:03:22 +0300

    coments deleted

--- modules/06 Angular/Assignments/6/answer06/src/app/app.component.ts
+++ modules/06 Angular/Assignments/6/answer06/src/app/app.component.ts
@@ -28,33 +28,3 @@ export class AppComponent implements OnInit {
         return `mailto: ${email}`;
     }
 }
-
-
-
-/*use group4/Modules/03 Front End Development/Assignments/11
-generate the table using Typescript
-sort by first name using Array.prototype.sort().
-display the results in an Angular application.*/
-
-/*
-<body>
-    <table border="1">
-
-        <tr>
-            <td>Mariya</td>
-            <td>Stash</td>
-            <td>NY</td>
-            <td>+9087865914</td>
-            <td><a href="mailto:user@mail.com">user@mail.com</a></td>
-        </tr>
-
-        <tr>
-                <td>Sam</td>
-                <td>Smith</td>
-                <td>LA</td>
-                <td>+99843216514</td>
-                <td><a href="mailto:sam@gookle.com">sam@gookle.com</a></td>
-            </tr>
-    </table>
-</body>
-</html> */
