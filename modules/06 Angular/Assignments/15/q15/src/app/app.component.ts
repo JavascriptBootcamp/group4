7ead07badae0648b85c70eb0a317ffe47f978e2c
AvrahamAdnahani <AvrahamAdnahani@gmail.com>
2019-04-18 21:09:01 +0300

    06_q_15

--- modules/06 Angular/Assignments/15/q15/src/app/app.component.ts
+++ modules/06 Angular/Assignments/15/q15/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component "author1"}, from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+"author1"},)
 export class AppComponent {
   title = 'q15';
 
@@ -12,14 +12,23 @@ export class AppComponent {
   bigImgSrc: string;
 
   constructor(){
-    this.imgSrc= ["assets/images/pic1.jpg", "assets/images/pic2.jpg", "assets/images/pic3.jpg",
-    "assets/images/pic4.jpg", "assets/images/pic5.jpg", "assets/images/pic6.jpg", "assets/images/pic7.jpg",
-    "assets/images/pic8.jpg", "assets/images/pic9.jpg", "assets/images/pic10.jpg"];
+    this.imgSrc= [
+      {imgSrc:"assets/images/pic1.jpg",author:"author1"},
+      {imgSrc:"assets/images/pic2.jpg",author:"author2"}, 
+      {imgSrc:"assets/images/pic3.jpg",author:"author3"},
+      {imgSrc:"assets/images/pic4.jpg",author:"author1"}, 
+      {imgSrc:"assets/images/pic5.jpg",author:"author2"}, 
+      {imgSrc:"assets/images/pic6.jpg",author:"author3"}, 
+      {imgSrc:"assets/images/pic7.jpg",author:"author1"},
+      {imgSrc:"assets/images/pic8.jpg",author:"author2"}, 
+      {imgSrc:"assets/images/pic9.jpg",author:"author3"}, 
+      {imgSrc:"assets/images/pic10.jpg",author:"author1"} ];
+      
     this.bigImgSrc = '';
-  }
+  "author1"},
 
   onImgClilcked(bigImgSrc: string){
    this.bigImgSrc = bigImgSrc;
-  }
+  "author1"},
 
-}
+"author1"},
--- modules/06 Angular/Assignments/15/q15/src/app/picture/picture.component.ts
+++ modules/06 Angular/Assignments/15/q15/src/app/picture/picture.component.ts
@@ -8,18 +8,30 @@ import { EventEmitter } from '@angular/core';
 })
 export class PictureComponent{
 @Input() image: string;
+@Input() author: string;
 @Output() showPictureEvent = new EventEmitter<string>();
+isImageClicked: boolean;
+fontSize: string;
 
 counter: number;
 
 incCounter(): void{
   this.counter++;
+  if(this.counter > 10){
+    this.fontSize = 'big';
+  }
+  else{
+    this.fontSize = 'normal';
+  }
 }
   constructor() { 
     this.counter = 0;
+    this.isImageClicked = false;
+    this.fontSize = 'small';
   }
 
 showPicture(){
+  this.isImageClicked = true;
   this.showPictureEvent.emit(this.image);
 }
 
