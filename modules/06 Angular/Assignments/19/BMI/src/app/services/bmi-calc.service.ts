4895963192285e67eaea74d4e94f0a5235bceacd
matan <front4@sqlabs.co.il>
2019-04-21 20:30:03 +0300

    update error logs

--- modules/06 Angular/Assignments/19/BMI/src/app/services/bmi-calc.service.ts
+++ modules/06 Angular/Assignments/19/BMI/src/app/services/bmi-calc.service.ts
@@ -10,6 +10,13 @@ export class BmiCalcService {
 
   //BMI = weight (kg) /height (meters)2
   public calculateBmi(weight: number, height: number): number {
-    return Number(weight)/ Math.pow(Number(height),2);
+    let res = Number(weight)/ Math.pow(Number(height),2);
+    //check the input correctness
+    if(isNaN(res) || Math.round(res) === 0 || Math.round(res) === 1){
+      console.error(`problem with the bmi calculation, check your input - weight: '${weight}', height:'${height}'`);
+      return 0;
+    }
+
+    return res;
   }
 }
