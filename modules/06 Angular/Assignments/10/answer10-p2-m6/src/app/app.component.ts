fbc4d08c2bef10ea0df3fde21ded8c0a600a1678
aviyot <aviyot@gmail.com>
2019-04-22 15:18:36 +0300

    assigment answer

--- modules/06 Angular/Assignments/10/answer10-p2-m6/src/app/app.component.ts
+++ modules/06 Angular/Assignments/10/answer10-p2-m6/src/app/app.component.ts
@@ -6,33 +6,32 @@ import { FormGroup, FormControl } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+  registrationForm: FormGroup;
 
-  registrationForm:FormGroup;
-
-  ngOnInit(){
+  ngOnInit() {
     this.registrationForm = new FormGroup({
-      personInformtion:new FormGroup({
-             fristName:new FormControl(null),
-              lastName:new FormControl(null),
-              age:new FormControl(null)
+      personInformtion: new FormGroup({
+        fristName: new FormControl(null),
+        lastName: new FormControl(null),
+        age: new FormControl(null)
       }),
-      credentials:new FormGroup({
-        userName:new FormControl(null),
-        password:new FormControl(null)
+      credentials: new FormGroup({
+        userName: new FormControl(null),
+        password: new FormControl(null)
       }),
-      contact:new FormGroup({
-        tel:new FormControl(null),
-        email:new FormControl(null)
+      contact: new FormGroup({
+        tel: new FormControl(null),
+        email: new FormControl(null)
       }),
-      misc:new FormGroup({
-        comments:new FormControl(null),
-        hobbies:new FormControl(null)
+      misc: new FormGroup({
+        comments: new FormControl(null),
+        hobbies: new FormControl(null)
       })
-    })
+    });
   }
 
-  onSubmit(){
-   console.log(this.registrationForm);
+  onSubmit() {
+    console.log(this.registrationForm);
   }
 }
