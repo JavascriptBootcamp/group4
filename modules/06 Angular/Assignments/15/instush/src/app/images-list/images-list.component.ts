661eb9ed635d329167264e9ed3520d425d2514cc
front8 <front8@sqlabs.co.il>
2019-04-28 20:22:55 +0300

    add image-service

--- modules/06 Angular/Assignments/15/instush/src/app/images-list/images-list.component.ts
+++ modules/06 Angular/Assignments/15/instush/src/app/images-list/images-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Image} from '../image.model'
+import {ImageServiceService} from '../image-service.service'
 @Component({
   selector: 'app-images-list',
   templateUrl: './images-list.component.html',
@@ -9,26 +10,13 @@ export class ImagesListComponent implements OnInit {
   images:Image[];
   imageShowSrc:string;
   imageShowFlag:boolean;
-  constructor() { 
+  constructor(private imageServiceService:ImageServiceService) { 
 
     this.imageShowFlag = false;
     this.imageShowSrc = "";
 
-    this.images = [
-      
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:0,authorNmae:"ron",authorId:"0",imagesList:[""]},
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:1,authorNmae:"maya",authorId:"1",imagesList:[""]},
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:2,authorNmae:"lior",authorId:"2",imagesList:[""]},
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:3,authorNmae:"liran",authorId:"3",imagesList:[""]},
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:4,authorNmae:"inbal",authorId:"4",imagesList:[""]},
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:5,authorNmae:"inbar",authorId:"5",imagesList:[""]},
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:6,authorNmae:"gaby",authorId:"6",imagesList:[""]},
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:7,authorNmae:"daniel",authorId:"7",imagesList:[""]},
-      {src:"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",likeCount:0,id:8,authorNmae:"roy",authorId:"8",imagesList:[""]},
-      
-      
-      ]
-
+    this.images = [];
+    this.imageServiceService = imageServiceService;
 
   }
 
@@ -46,6 +34,8 @@ OnShowImage(image:Image){
   }
 
   ngOnInit() {
+
+    this.images = this.imageServiceService.getImages();
   }
 
 }
