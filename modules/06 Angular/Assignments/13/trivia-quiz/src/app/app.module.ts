558f89b58e87851e900576218372ff377016312b
aviyot <aviyot@gmail.com>
2019-05-01 06:21:44 +0300

    create file strcture

---
+++ modules/06 Angular/Assignments/13/trivia-quiz/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AnswersComponent } from './answers/answers.component';
+import { QuestionsComponent } from './questions/questions.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AnswersComponent,
+    QuestionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
