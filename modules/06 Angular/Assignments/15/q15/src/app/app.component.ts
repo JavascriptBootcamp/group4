c0f0878ff530c2273dd5dc8901fb7548bcb44db4
danielbr08@gmail.com <daniel.brosh@sapiens.com>
2019-05-01 10:50:46 +0300

    06_q_15

--- modules/06 Angular/Assignments/15/q15/src/app/app.component.ts
+++ modules/06 Angular/Assignments/15/q15/src/app/app.component.ts
@@ -3,28 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'q15';
-
-  imgSrc: string[];
-  bigImgSrc: string;
-
-  constructor(){
-    this.imgSrc= ["assets/images/pic1.jpg", "assets/images/pic2.jpg", "assets/images/pic3.jpg",
-    "assets/images/pic4.jpg", "assets/images/pic5.jpg", "assets/images/pic6.jpg", "assets/images/pic7.jpg",
-    "assets/images/pic8.jpg", "assets/images/pic9.jpg", "assets/images/pic10.jpg", "assets/images/pic11.jpg", "assets/images/pic12.jpg"];
-    this.bigImgSrc = '';
-  }
-
-  onImgClilcked(bigImgSrc: string){
-   this.bigImgSrc = bigImgSrc;
-  }
-=======
   styleUrls: ['./app.component.css']})
->>>>>>> b4e22d7284b171342d61ad89e8a94ae4805e8570
 
 export class AppComponent {
 }
--- modules/06 Angular/Assignments/15/q15/src/app/picture/picture.component.ts
+++ modules/06 Angular/Assignments/15/q15/src/app/picture/picture.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component, Input,Output, OnInit } from '@angular/core';
-=======
 import { Component, Input, Output, OnInit} from '@angular/core';
->>>>>>> b4e22d7284b171342d61ad89e8a94ae4805e8570
 import { EventEmitter } from '@angular/core';
 import { Picture } from '../picture.model';
 import { PicturesService } from '../pictures.service';
@@ -14,30 +10,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./picture.component.css']
 })
 export class PictureComponent implements OnInit{
-<<<<<<< HEAD
-@Input() image: string;
-@Output() showPictureEvent = new EventEmitter<string>();
-=======
   @Input() picture: Picture;
   @Output() showPictureEvent = new EventEmitter<Picture>();
   isImageClicked: boolean;
   fontSize: string;
->>>>>>> b4e22d7284b171342d61ad89e8a94ae4805e8570
 
   constructor(private activatedRoute: ActivatedRoute, private picturesService: PicturesService) {
     this.isImageClicked = false;
     this.fontSize = 'small';
   }
 
-<<<<<<< HEAD
-incCounter(): void{
-  this.counter++;
-  localStorage.setItem(this.image,this.counter.toString());
-}
-  constructor() { 
-    this.counter = 0;
-    console.log(this.image);
-=======
   ngOnInit(){
     this.isImageClicked = Boolean(localStorage.getItem("isClickedPic" + this.picture.id));
     const likeCounterLS = localStorage.getItem("likeCounterPic" + this.picture.id);
@@ -45,7 +27,6 @@ incCounter(): void{
       this.picturesService.setPictureLikeCounter(this.picture.id,+likeCounterLS);
 
     this.setFontSize();
->>>>>>> b4e22d7284b171342d61ad89e8a94ae4805e8570
   }
 
   onLikeClicked(){
@@ -58,12 +39,6 @@ incCounter(): void{
     this.picture.likeCounter++;
   }
 
-<<<<<<< HEAD
-ngOnInit(){
-  this.counter = Number(localStorage.getItem(this.image));
-}
-
-=======
   updateLikeCounterLS(){
     localStorage.setItem("likeCounterPic" + this.picture.id,this.picture.likeCounter.toString());
   }
@@ -83,6 +58,5 @@ ngOnInit(){
     localStorage.setItem("isClickedPic" + this.picture.id, JSON.stringify(this.isImageClicked));
 
   }
->>>>>>> b4e22d7284b171342d61ad89e8a94ae4805e8570
 
 }
