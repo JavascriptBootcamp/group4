e2e512ddf205da76bc08ef985dac4069a6035e81
danielbr08 <danielbr08@gmail.com>
2019-05-05 19:01:36 +0300

    06_q_15

--- modules/06 Angular/Assignments/15/q15/src/app/pictures.service.ts
+++ modules/06 Angular/Assignments/15/q15/src/app/pictures.service.ts
@@ -9,22 +9,22 @@ export class PicturesService{
   constructor() {
     let i=0;
     this.pictures = [
-      { id: i++, imgSrc:"assets/images/pic1.jpg", author:{ id:1, name:"author1", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'}, likeCounter:0 },
-      { id: i++, imgSrc:"assets/images/pic2.jpg", author:{ id:2, name:"author2", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ' } , likeCounter:0 }, 
-      { id: i++, imgSrc:"assets/images/pic3.jpg", author:{ id:3, name:"author3", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  },
-      { id: i++, imgSrc:"assets/images/pic4.jpg", author:{ id:1, name:"author1",profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'}, likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic5.jpg", author:{ id:2, name:"author2", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic6.jpg", author:{ id:3, name:"author3", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic7.jpg", author:{ id:1, name:"author1", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  },
-      { id: i++, imgSrc:"assets/images/pic8.jpg", author:{ id:2, name:"author2", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic9.jpg", author:{ id:3, name:"author3", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic10.jpg", author:{ id:1, name:"author1", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  },
-      { id: i++, imgSrc:"assets/images/pic4.jpg", author:{ id:1, name:"author1", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic5.jpg", author:{ id:2, name:"author2", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic6.jpg", author:{ id:3, name:"author3", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic7.jpg", author:{ id:1, name:"author1", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  },
-      { id: i++, imgSrc:"assets/images/pic8.jpg", author:{ id:2, name:"author2", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  }, 
-      { id: i++, imgSrc:"assets/images/pic9.jpg", author:{ id:3, name:"author3", profilePicture: 'XXX', facebookProfileURL: 'YYY', description: 'ZZZ'} , likeCounter:0  } ];
+      { id: i++, imgSrc:"assets/images/pic1.jpg", author:{ id:1, name:"author1", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author1'}, likeCounter:0 },
+      { id: i++, imgSrc:"assets/images/pic2.jpg", author:{ id:2, name:"author2", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author2' } , likeCounter:0 }, 
+      { id: i++, imgSrc:"assets/images/pic3.jpg", author:{ id:3, name:"author3", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author3'} , likeCounter:0  },
+      { id: i++, imgSrc:"assets/images/pic4.jpg", author:{ id:1, name:"author1",profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author1'}, likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic5.jpg", author:{ id:2, name:"author2", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author2'} , likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic6.jpg", author:{ id:3, name:"author3", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author3'} , likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic7.jpg", author:{ id:1, name:"author1", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author1'} , likeCounter:0  },
+      { id: i++, imgSrc:"assets/images/pic8.jpg", author:{ id:2, name:"author2", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author2'} , likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic9.jpg", author:{ id:3, name:"author3", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author3'} , likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic10.jpg", author:{ id:1, name:"author1", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author1'} , likeCounter:0  },
+      { id: i++, imgSrc:"assets/images/pic4.jpg", author:{ id:1, name:"author1", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author1'} , likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic5.jpg", author:{ id:2, name:"author2", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author2'} , likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic6.jpg", author:{ id:3, name:"author3", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author3'} , likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic7.jpg", author:{ id:1, name:"author1", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author1'} , likeCounter:0  },
+      { id: i++, imgSrc:"assets/images/pic8.jpg", author:{ id:2, name:"author2", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author2'} , likeCounter:0  }, 
+      { id: i++, imgSrc:"assets/images/pic9.jpg", author:{ id:3, name:"author3", profilePicture: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png', facebookProfileURL: 'https://www.facebook.com/daniel.brosh', description: 'Description author3'} , likeCounter:0  } ];
    }
 
    getPictures() :Picture[]{
