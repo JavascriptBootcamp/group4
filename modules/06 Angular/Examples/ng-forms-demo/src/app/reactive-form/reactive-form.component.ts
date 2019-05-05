508cf58f69d43911abe40d38427af177b4e954d3
JavascriptBootcamp <shai.mesisterano@agilesparks.com>
2019-05-05 20:05:09 +0300

    modify examples

--- modules/06 Angular/Examples/ng-forms-demo/src/app/reactive-form/reactive-form.component.ts
+++ modules/06 Angular/Examples/ng-forms-demo/src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,15 +7,37 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent {
-  formGroup: FormGroup;
+  f: FormGroup;
   constructor(builder: FormBuilder) {
-    this.formGroup = builder.group({
-      fullname: [
-        "Shai", [
-          Validators.required
+    this.f = builder.group({
+      // username
+      username: builder.group({
+        firstname: [
+          "Shai"
+        ],
+        lastname: [
+          "Mesisterano", [
+            Validators.required,
+            Validators.minLength(2),
+            this.myvalidator
+          ]
         ]
-      ]
+      })
     })
+  }
+
+  myvalidator(control: FormControl):{[key:string]:boolean} {
+    if (control.value.indexOf('b') > -1){
+      return null;
+    }
+    return {
+      'containsB': true
+    }
+  }
+
+  onSubmit() {
+    console.log('f', this.f);
+    console.log('f.get.lastname', this.f.get('username').get('lastname'));
   }
 
 }
