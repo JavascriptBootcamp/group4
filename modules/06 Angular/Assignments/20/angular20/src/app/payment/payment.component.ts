b910e27e4432a2c09d96a6ef23fb1a024afc6dfb
gabybuchnik <gabybuchnik@gmail.com>
2019-05-06 12:36:27 +0300

    angular20

--- modules/06 Angular/Assignments/20/angular20/src/app/payment/payment.component.ts
+++ modules/06 Angular/Assignments/20/angular20/src/app/payment/payment.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit ,ViewChild } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
@@ -7,21 +7,82 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
-  hidden: boolean;
-  submit: boolean;
-  profileForm : FormGroup;
-  constructor() {
-    this.hidden = true;
-    this.submit = false;
+  hidden = true;
+  profileForm: FormGroup;
+  constructor(builder: FormBuilder) {
+    this.profileForm = builder.group({
+      cardNumber: ['', [Validators.required, this.validateDigitAndNumber, this.validteDiffrentDigits, this.validteFinalDigitEven, this.validteSumGreater16]],
+      cardName: ['', Validators.required],
+      expiryDate: ['', Validators.required],
+      SecurityCode: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(4)]]
+    })
   }
 
-   ngOnInit() {
-    this.profileForm = new FormGroup({
-      
-    });
+  ngOnInit() {
+
+  }
+  validateDigitAndNumber(control: FormControl): { [key: string]: boolean } {
+    if (control.value.length === 16 && !isNaN(control.value)) {
+      return null
+    }
+    return {
+      "DigitAndNumber": true
+    }
+  }
+  validteFinalDigitEven(control: FormControl): { [key: string]: boolean } {
+    if (control.value[15] % 2 === 0) {
+      return null
+    }
+    return {
+      "FinalDigitEven": true
+    }
+  }
+  validteSumGreater16(control: FormControl): { [key: string]: boolean } {
+    let sum: number = 0;
+    for (let i = 0; i < control.value.length; i++) {
+      sum += Number(control.value[i]);
+    }
+    if (sum > 16) {
+      return null
+    }
+    return {
+      "SumGreater16": true
+    }
+  }
+  validteDiffrentDigits(control: FormControl): { [key: string]: boolean } {
+    let duplicates: number = 0;
+    let duplicatesArr: Array<number> = new Array(10).fill(0);
+    for (let i = 0; i < control.value.length; i++) {
+      duplicatesArr[control.value[i]]++;
+    }
+    for (let i = 0; i < duplicatesArr.length; i++) {
+      if (duplicatesArr[i] > 0) {
+        duplicates++;
+      }
+    }
+    if (duplicates >= 2) {
+      return null
+    }
+    return {
+      "DiffrentDigits": true
+    }
+  }
+  get f() { 
+    return this.profileForm.controls; 
   }
   onSubmit() {
-   
+    if (this.profileForm.valid) {
+      this.hidden = true;
+      fetch("http://localhost:3000/Order").then(function (response) {
+        return response.json();
+      })
+        .then(function (myJson) {
+          console.log(JSON.stringify(myJson));
+        });
+    }
+    else {
+      this.hidden = false;
+    }
   }
 
 }
