dd29ce92bc99281edf1cc833a7dbbb427ffabc97
OfekArbeli <ofekrbeli@gmail.com>
2019-05-08 14:17:52 +0300

    Angular-answer20

--- modules/06 Angular/Assignments/20/answer20/src/app/app-routing.module.ts
+++ modules/06 Angular/Assignments/20/answer20/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { PaymentFormComponent } from './payment-form/payment-form.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ShoppingCartComponent},
+  { path: 'payment', component: PaymentFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
