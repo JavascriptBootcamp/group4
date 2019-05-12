959abb4fba9b647dc1d1c10ff7dbd5e7bb00a31a
Shani Staretz <shakimi88@gmail.com>
2019-05-12 19:50:53 +0300

    merge

--- modules/06 Angular/Assignments/15/answer/src/app/components/main-gallery/main-gallery.component.ts
+++ modules/06 Angular/Assignments/15/answer/src/app/components/main-gallery/main-gallery.component.ts
@@ -14,22 +14,10 @@ export class MainGalleryComponent implements OnInit {
   bigPicSrc:string;
   topPicSrcs:string[];
 
-  constructor() {
-    // this.imgs = [
-    //   {src: 'https://images.unsplash.com/photo-1553901753-215db344677a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80', likes: 0},
-    //   {src: 'https://images.unsplash.com/photo-1553548146-50f0bdf09f0e?ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80', likes: 0},
-    //   {src: 'https://images.unsplash.com/photo-1551300263-00574a9d9b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', likes: 0},
-    //   {src: 'https://images.unsplash.com/photo-1551367456-16ac556dfef2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', likes: 0},
-    //   {src: 'https://images.unsplash.com/photo-1550795598-717619d32900?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', likes: 0},
-    //   {src: 'https://images.unsplash.com/photo-1550639264-38c3059c4620?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', likes: 0},
-    //   {src: 'https://images.unsplash.com/photo-1549945676-4fdf5f18a9fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', likes: 0},
-    //   {src: 'https://images.unsplash.com/photo-1508662790687-257eefcdccfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', likes: 0},
-    //   {src: 'https://images.unsplash.com/photo-1547974497-bb2a93b333ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', likes: 0}
-    // ];
-
-    // this.sortTop3();
-
-    // this.bigPicSrc = null;
+  constructor(private imgService: ImgService) {
+    this.topPicSrcs = this.imgService.get_Top3();
+    this.imgs = this.imgService.get_imgs();
+    this.bigPicSrc = null;
   }
 
   ngOnInit(): void {
