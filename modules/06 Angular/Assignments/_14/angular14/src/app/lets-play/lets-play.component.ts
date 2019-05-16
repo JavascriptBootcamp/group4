8e4e1317739b43b1f5f8da5ea2ef04c63573683f
gabybuchnik <gabybuchnik@gmail.com>
2019-05-16 11:10:05 +0300

    add timer

--- modules/06 Angular/Assignments/_14/angular14/src/app/lets-play/lets-play.component.ts
+++ modules/06 Angular/Assignments/_14/angular14/src/app/lets-play/lets-play.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { QuestionsService } from '../questions.service';
 import { questions } from '../questions.model';
+
+
 
 @Component({
   selector: 'app-lets-play',
@@ -8,6 +10,11 @@ import { questions } from '../questions.model';
   styleUrls: ['./lets-play.component.css']
 })
 export class LetsPlayComponent implements OnInit {
+  @ViewChild('r1') r1: ElementRef;
+  @ViewChild('r2') r2: ElementRef;
+  @ViewChild('r3') r3: ElementRef;
+  @ViewChild('r4') r4: ElementRef;
+  @ViewChild('myForm') myForm;
   id: number;
   hidden: boolean;
   hiddenBtn: boolean;
@@ -16,6 +23,8 @@ export class LetsPlayComponent implements OnInit {
   correctAnswers: number;
   indexQ: number;
   correct: boolean;
+  time: number;
+  interval: any;
   constructor(private questionsService: QuestionsService) {
     this.id = 0;
     this.hidden = false;
@@ -24,16 +33,43 @@ export class LetsPlayComponent implements OnInit {
     this.correctAnswers = 0;
     this.indexQ = 0;
     this.correct = null;
+    this.time = 0;
+    this.interval = null;
   }
   ngOnInit() {
     this.id = 0;
     this.questions = this.questionsService._getAllQuestionsAndAnswers;
     this.currentQuestion = this.questionsService.getQuestionById(this.id);
+    this.incTime();
+  }
+  incTime() {
+    this.interval = setInterval(() => {
+      this.time++;
+      if (this.time === 5) {
+        this.nextQuestion(null, { q: this.myForm.value.q });
+      }
+    }, 1000);
+  }
+  clearTime() {
+    this.time = 0;
+    clearInterval(this.interval);
+  }
+  clearRadioBtn() {
+    this.r1.nativeElement.checked = false;
+    this.r2.nativeElement.checked = false;
+    this.r3.nativeElement.checked = false;
+    this.r4.nativeElement.checked = false;
   }
   nextQuestion(event: Event, value: any) {
-    event.preventDefault();
+    this.clearRadioBtn();
+    if (event) {
+      event.preventDefault();
+    }
+    this.clearTime();
+    this.incTime();
     if (this.id === this.questions.length - 1) {
       this.hiddenBtn = true;
+      this.clearTime();
       return;
     }
     this.correct = this.questionsService.correctAnswer(value, this.id);
@@ -42,9 +78,12 @@ export class LetsPlayComponent implements OnInit {
     }
     this.id++;
     this.currentQuestion = this.questionsService.getQuestionById(this.id);
+    this.myForm.form.value.q = "";
   }
   prevQuestion(event: Event, value: any) {
     event.preventDefault();
+    this.clearTime();
+    this.incTime();
     if (this.id === 0) {
       return;
     }
--- modules/06 Angular/Assignments/_14/angular14/src/app/question/question.component.ts
+++ modules/06 Angular/Assignments/_14/angular14/src/app/question/question.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit, OnDestroy {
+  @Input() time: number;
+  @Input() interval: number;
   constructor() {
-
   }
   ngOnInit() {
-
-
+  
+  }
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 }
