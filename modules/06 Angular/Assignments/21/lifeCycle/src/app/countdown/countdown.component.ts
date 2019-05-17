f8d61b1fce72c626a632007b52fe4eaaf312bd28
EvyatarD <eviadk@gmail.com>
2019-05-17 17:17:01 +0300

    add

--- modules/06 Angular/Assignments/21/lifeCycle/src/app/countdown/countdown.component.ts
+++ modules/06 Angular/Assignments/21/lifeCycle/src/app/countdown/countdown.component.ts
@@ -7,12 +7,16 @@ import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/co
 })
 export class CountdownComponent implements OnInit {
   @ViewChild('actionBtn') actionBtn: ElementRef;
+  @ViewChild('min') minValue: ElementRef;
+  @ViewChild('sec') secValue: ElementRef;
+
   interval: any;
   sec: number;
   min: number;
   secString: string;
   minString: string;
   timeOut: boolean;
+  inValid: boolean = false;
 
   constructor() {
 
@@ -27,44 +31,46 @@ export class CountdownComponent implements OnInit {
     this.actionBtn.nativeElement.innerText = nameBtn;
   }
 
+  // ngAfterViewInit(){
+  //   console.log(this.minValue.nativeElement.value);
+  // }
+
   startTimer() {
-    if (this.actionBtn.nativeElement.innerText === "START") {
-      this.changeNameBtn("STOP");
-      this.timeOut = false;
+    if (this.minValue.nativeElement.value && this.secValue.nativeElement.value) {
 
-      this.interval = setInterval(() => {
-        if (this.sec === 0) {
-          console.log(this.sec);
-          this.sec = 60;
-          if (this.min > 0)
-            this.min -= 1;
-        }
-        this.sec--;
-        console.log(this.sec);
-        console.log(this.secString);
-        // string to 0+digit
-        if (this.sec < 10)
-          this.secString = "0" + this.sec;
-        else
-          this.secString = this.sec + "";
-        console.log(this.sec);
-        console.log(this.secString);
-        if (this.min < 10)
-          this.minString = "0" + this.min;
-        else
-          this.minString = this.min + "";
+      if (this.actionBtn.nativeElement.innerText === "START") {
+        this.changeNameBtn("STOP");
+        this.timeOut = false;
 
+        this.min = Number(this.minValue.nativeElement.value);
+        this.sec = Number(this.secValue.nativeElement.value);
+        this.formatTimer();
 
-        if ((this.min === 0) && (this.sec === 0)) {
-          this.resetTimer();
-          this.timeOut = true;
-        }
-      }, 1000);
-    }
-    
-    else {
-      this.changeNameBtn("START");
-      clearInterval(this.interval);
+        this.interval = setInterval(() => {
+
+          if ((this.min === 0) && (this.sec === 0)) {
+            this.resetTimer();
+            this.timeOut = true;
+          }
+
+          if (this.sec === 0) {
+            if (this.min > 0)
+              this.min -= 1;
+            this.sec = 60;
+          }
+
+          this.sec--;
+
+          // string to 0+digit
+          this.formatTimer();
+
+        }, 1000);
+      }
+
+      else {
+        this.changeNameBtn("START");
+        clearInterval(this.interval);
+      }
     }
   }
 
@@ -72,10 +78,11 @@ export class CountdownComponent implements OnInit {
     clearInterval(this.interval);
     this.changeNameBtn("START");
     this.timeOut = false;
-    this.min = 5;
-    this.sec = 0;
-    this.minString = "05";
+    this.minString = "00";
     this.secString = "00";
+    this.minValue.nativeElement.value=null;
+    this.secValue.nativeElement.value=null;
+
   }
 
   ngOnDestroy() {
@@ -83,8 +90,15 @@ export class CountdownComponent implements OnInit {
     this.resetTimer();
   }
 
-  ngAfterViewChecked() {
-
+  formatTimer(){
+    if (this.sec < 10)
+    this.secString = "0" + this.sec;
+  else
+    this.secString = this.sec + "";
+  if (this.min < 10)
+    this.minString = "0" + this.min;
+  else
+    this.minString = this.min + "";
   }
 
 }
