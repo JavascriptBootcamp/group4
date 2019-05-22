9d0493d7009e4316aebe381c327f048c6cc328db
gabybuchnik <gabybuchnik@gmail.com>
2019-05-22 11:46:13 +0300

    answer 1 - 6 need to implement 7,8

--- Good Coding Practices/src/js/scripts.js
+++ Good Coding Practices/src/js/scripts.js
@@ -1,17 +1,35 @@
 // piece object
-const piece = (function() {
+const piece = (function () {
   let el = null;
-  const init = function(el) {
+  const init = function (el) {
     this.el = el;
+    this.el.initX = el.getBoundingClientRect().x;
+    this.el.initY = el.getBoundingClientRect().y;
   };
-  const moveDelta = function(dx, dy) {
+  const moveDelta = function (dx, dy) {
     const pos = this.el.getBoundingClientRect();
     this.el.style.left = `${pos.left + dx}px`;
     this.el.style.top = `${pos.top + dy}px`;
   };
+  const resetLocation = function () {
+    this.el.style.left = `${this.el.initX}px`;
+    this.el.style.top = `${this.el.initY}px`;
+  };
+  const randomLocation = function () {
+    const CIRCLE_WIDTH = 100;
+    const CIRCLE_HEIGHT = 100;
+    const MAX_WIDTH = window.innerWidth;
+    const MAX_HEIGHT = window.innerHeight;
+    const randX = Math.random() * (MAX_WIDTH - (CIRCLE_WIDTH * 2)) + CIRCLE_WIDTH;
+    const randY = Math.random() * (MAX_HEIGHT - (CIRCLE_HEIGHT * 2)) + CIRCLE_HEIGHT
+    this.el.style.left = `${randX}px`;
+    this.el.style.top = `${randY}px`;
+  };
   return {
     init,
-    moveDelta
+    moveDelta,
+    resetLocation,
+    randomLocation
   };
 })();
 
@@ -20,22 +38,37 @@ function handleClick(ev) {
 }
 
 function init() {
+  const NUM_0 = 0;
+  const NUM_100 = 100;
+  const NUM_MINUS_100 = -100;
   const $btnUp = document.getElementById("btn-up");
-  $btnUp.dataset.dx = 0;
-  $btnUp.dataset.dy = -100;
-  $btnUp.addEventListener("click", handleClick);
+  setDxDy($btnUp, NUM_0, NUM_MINUS_100);
   const $btnRight = document.getElementById("btn-right");
-  $btnRight.dataset.dx = 100;
-  $btnRight.dataset.dy = 0;
-  $btnRight.addEventListener("click", handleClick);
+  setDxDy($btnRight, NUM_100, NUM_0);
   const $btnDown = document.getElementById("btn-down");
-  $btnDown.dataset.dx = 0;
-  $btnDown.dataset.dy = 100;
-  $btnDown.addEventListener("click", handleClick);
+  setDxDy($btnDown, NUM_0, NUM_100);
   const $btnLeft = document.getElementById("btn-left");
-  $btnLeft.dataset.dx = -100;
-  $btnLeft.dataset.dy = 0;
-  $btnLeft.addEventListener("click", handleClick);
+  setDxDy($btnLeft, NUM_MINUS_100, NUM_0);
+  addClickEventsMoveCircle($btnUp, $btnRight, $btnLeft, $btnDown);
+  const $btnReset = document.getElementById("btn-reset");
+  $btnReset.addEventListener("click", () => {
+    piece.resetLocation(NUM_0, NUM_0);
+  });
+  const $randomize = document.getElementById("btn-randomize");
+  $randomize.addEventListener("click", () => {
+    piece.randomLocation();
+  });
+}
+
+function setDxDy(el, dx, dy) {
+  el.dataset.dx = dx;
+  el.dataset.dy = dy;
+
+}
+function addClickEventsMoveCircle(...control) {
+  control.forEach((item) => {
+    item.addEventListener("click", handleClick);
+  });
 }
 
 window.addEventListener("DOMContentLoaded", event => {
