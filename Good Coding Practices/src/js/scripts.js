427cc0ea80373c66321a40a3b63c3a2c9d6d0868
aviyot <aviyot@gmail.com>
2019-05-23 21:20:57 +0300

    ---

--- Good Coding Practices/src/js/scripts.js
+++ Good Coding Practices/src/js/scripts.js
@@ -20,23 +20,49 @@ function handleClick(ev) {
 }
 
 function init() {
-  const $btnUp = document.getElementById("btn-up");
-  $btnUp.dataset.dx = 0;
-  $btnUp.dataset.dy = -100;
-  $btnUp.addEventListener("click", handleClick);
-  const $btnRight = document.getElementById("btn-right");
-  $btnRight.dataset.dx = 100;
-  $btnRight.dataset.dy = 0;
-  $btnRight.addEventListener("click", handleClick);
-  const $btnDown = document.getElementById("btn-down");
-  $btnDown.dataset.dx = 0;
-  $btnDown.dataset.dy = 100;
-  $btnDown.addEventListener("click", handleClick);
+  btnUpInit();
+  btnRightInit();
+  btnDownInit();
+  btnLeftInit();
+}
+function btnLeftInit(){
   const $btnLeft = document.getElementById("btn-left");
   $btnLeft.dataset.dx = -100;
   $btnLeft.dataset.dy = 0;
   $btnLeft.addEventListener("click", handleClick);
 }
+
+function btnUpInit(){
+  const $btnUp = document.getElementById("btn-up");
+  $btnUp.dataset.dx = 0;
+  $btnUp.dataset.dy = -100;
+  $btnUp.addEventListener("click", handleClick);
+
+}
+function btnRightInit(){
+  const $btnRight = document.getElementById("btn-right");
+  $btnRight.dataset.dx = 100;
+  $btnRight.dataset.dy = 0;
+  $btnRight.addEventListener("click", handleClick);
+}
+
+function btnDownInit(){
+  const $btnDown = document.getElementById("btn-down");
+  $btnDown.dataset.dx = 0;
+  $btnDown.dataset.dy = 100;
+  $btnDown.addEventListener("click", handleClick);
+}
+
+
+
+const $btnReset = document.getElementById("btn-reset");
+$btnReset.addEventListener("click",event=>{
+  init();
+  piece.init(document.getElementById("piece"));
+  piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
+});
+
+
 
 window.addEventListener("DOMContentLoaded", event => {
   piece.init(document.getElementById("piece"));
