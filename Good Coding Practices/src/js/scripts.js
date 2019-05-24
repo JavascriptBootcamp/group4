bbe4a67ee6c0a5580987399057cd83aa315b4582
EvyatarD <eviadk@gmail.com>
2019-05-24 17:36:59 +0300

    add

--- Good Coding Practices/src/js/scripts.js
+++ Good Coding Practices/src/js/scripts.js
@@ -1,25 +1,96 @@
 // piece object
-const piece = (function() {
+
+const piece = (function () {
   let el = null;
-  const init = function(el) {
+
+  const init = function (el) {
     this.el = el;
   };
-  const moveDelta = function(dx, dy) {
+
+  const moveDelta = function (dx, dy) {
     const pos = this.el.getBoundingClientRect();
-    this.el.style.left = `${pos.left + dx}px`;
-    this.el.style.top = `${pos.top + dy}px`;
+    switch (dx) {
+      case 0:
+        if ((pos.top + dy < window.innerHeight) && (pos.top + dy > 0))
+          this.el.style.top = `${pos.top + dy}px`;
+        else if (pos.top + dy <= 0) 
+          this.el.style.top = `${pos.top + dy + (pos.top + dy) * (-1)}px`;
+        break;
+      // when dx 100 / -100 
+      default:
+        if ((pos.left + dx < window.innerWidth) && (pos.left + dx > 0)) {
+          this.el.style.left = `${pos.left + dx}px`;
+          this.el.style.top = `${pos.top + dy}px`;
+        }
+        else if (pos.left + dx <= 0) {
+          this.el.style.left = `${pos.left + dx + (pos.left + dx) * (-1)}px`;
+          this.el.style.top = `${pos.top + dy}px`;
+        }
+        break;
+    }
+  }
+
+  const pieceColor = function (temp) {
+    let color = "";
+    if (temp <= 10) {
+      color = "blue";
+    }
+    else if (temp > 10 && temp < 21)
+      color = "Green";
+    else if (temp > 20 && temp < 31)
+      color = "Yellow";
+    else if (temp > 30)
+      color = "Red";
+    this.el.style.background = color;
+    this.el.style.borderColor = color;
+  }
+
+  const weatherApi = function () {
+    fetch('http://api.apixu.com/v1/current.json?key=dda6e762ae4f41efb7e173552192204&q=tel%20aviv')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        for (item in myJson) {
+          if (myJson[item].temp_c)
+            piece.pieceColor(Number(myJson[item].temp_c));
+        }
+      });
+  }
+
+  const resetLocation = function () {
+    console.log(this.el);
+    this.el.style.left = "50%";
+    this.el.style.top = "100px";
   };
+
+  const randomizeLocation = function () {
+    this.el.style.left = Math.floor(Math.random() * window.innerWidth - 100) + 50 + "px";
+    this.el.style.top = Math.floor(Math.random() * window.innerHeight - 100) + 50 + "px";
+    console.log(Math.floor(Math.random() * window.innerHeight - 100) + 50);
+  };
+
   return {
     init,
-    moveDelta
+    moveDelta,
+    pieceColor,
+    weatherApi,
+    resetLocation,
+    randomizeLocation
   };
+
 })();
 
-function handleClick(ev) {
+function handleClick() {
   piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
+
 }
 
+
 function init() {
+  console.log(window.innerHeight, window.innerWidth);
+  piece.resetLocation();
+  piece.weatherApi();
   const $btnUp = document.getElementById("btn-up");
   $btnUp.dataset.dx = 0;
   $btnUp.dataset.dy = -100;
@@ -36,6 +107,11 @@ function init() {
   $btnLeft.dataset.dx = -100;
   $btnLeft.dataset.dy = 0;
   $btnLeft.addEventListener("click", handleClick);
+  const $btnRandom = document.getElementById("btn-random");
+  $btnRandom.addEventListener("click", piece.randomizeLocation.bind(piece));
+  const $btnReset = document.getElementById("btn-reset");
+  $btnReset.addEventListener("click", piece.resetLocation.bind(piece));
+
 }
 
 window.addEventListener("DOMContentLoaded", event => {
