46a7bc9603edaaba5e6fe5c80ecc8edfb8dd6553
front8 <front8@sqlabs.co.il>
2019-05-26 21:04:31 +0300

    07_q_03

---
+++ modules/07 NodeJS/Assignments/03/modules/prom-fs/fs-readFile.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+function readFile(fileName){
+
+return new Promise((res,rej)=>{
+
+fs.readFile(fileName,(error,data)=>{
+
+if(error) rej(`error: ${error}`);
+
+res(data);
+
+});
+
+});
+
+
+}
+
+
+module.exports = readFile;
