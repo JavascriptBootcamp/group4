2f09182672c2ee2b39ff2c47f553840575fde344
amitayl@gmail.com <amitayl@gmail.com>
2019-06-04 17:22:12 +0300

    Good Practice Code

--- Good Coding Practices/src/js/scripts.js
+++ Good Coding Practices/src/js/scripts.js
@@ -1,17 +1,71 @@
 // piece object
-const piece = (function() {
+const piece = (function () {
+
   let el = null;
-  const init = function(el) {
+  let left = 0;
+  let top = 0;
+
+  const init = function (el) {
     this.el = el;
-  };
-  const moveDelta = function(dx, dy) {
+
     const pos = this.el.getBoundingClientRect();
-    this.el.style.left = `${pos.left + dx}px`;
-    this.el.style.top = `${pos.top + dy}px`;
+    this.left = pos.left
+    this.top = pos.top;
+
+    fetchColor().then(data => this.el.style.background = data);
   };
+
+
+  const fetchColor = async () => {
+
+    const response = await fetch('http://api.apixu.com/v1/current.json?key=dda6e762ae4f41efb7e173552192204&q=tel%20aviv');
+    const myJson = await response.json();
+
+    if (myJson["current"]["temp_c"] < 10)
+      return "blue";
+
+    if (myJson["current"]["temp_c"] >= 10 && myJson["current"]["temp_c"] < 20)
+      return "green";
+
+    if (myJson["current"]["temp_c"] >= 20 && myJson["current"]["temp_c"] < 30)
+      return "yellow";
+
+    if (myJson["current"]["temp_c"] >= 30)
+      return "red";
+
+  };
+
+  const fillXandY = function (x, y) {
+    this.el.style.left = `${x}px`;
+    this.el.style.top = `${y}px`;
+  };
+
+  const moveDelta = function (dx, dy) {
+    const pos = this.el.getBoundingClientRect();
+    const left = pos.left + dx / 50;
+    const top = pos.top + dy / 50;
+    if (left >= 0 && left <= 1278 && top >= 0 && top <= 478) {
+      this.fillXandY(left, top);
+    }
+  };
+
+  const reset = function () {
+    this.fillXandY(this.left, this.top);
+  };
+
+  const randomize = function () {
+    let left = Math.floor((Math.random() * 1278));;
+    let top = Math.floor((Math.random() * 478));;;
+    this.el.style.left = `${left}px`;
+    this.el.style.top = `${top}px`;
+  };
+
   return {
     init,
-    moveDelta
+    moveDelta,
+    reset,
+    randomize,
+    fillXandY
   };
 })();
 
@@ -19,23 +73,26 @@ function handleClick(ev) {
   piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
 }
 
+function randomize() {
+  piece.randomize();
+}
+
+function reset() {
+  piece.reset();
+}
+
 function init() {
-  const $btnUp = document.getElementById("btn-up");
-  $btnUp.dataset.dx = 0;
-  $btnUp.dataset.dy = -100;
-  $btnUp.addEventListener("click", handleClick);
-  const $btnRight = document.getElementById("btn-right");
-  $btnRight.dataset.dx = 100;
-  $btnRight.dataset.dy = 0;
-  $btnRight.addEventListener("click", handleClick);
-  const $btnDown = document.getElementById("btn-down");
-  $btnDown.dataset.dx = 0;
-  $btnDown.dataset.dy = 100;
-  $btnDown.addEventListener("click", handleClick);
-  const $btnLeft = document.getElementById("btn-left");
-  $btnLeft.dataset.dx = -100;
-  $btnLeft.dataset.dy = 0;
-  $btnLeft.addEventListener("click", handleClick);
+  const cordinates = [[0, -100], [100, 0], [0, 100], [-100, 0]];
+  const classID = ["btn-up", "btn-right", "btn-down", "btn-left"];
+  const buttons = [];
+
+  for (let index = 0; index < classID.length; index++) {
+    let item = document.getElementById(classID[index]);
+    item.addEventListener("click", handleClick);
+    item.dataset.dx = cordinates[index][0];
+    item.dataset.dy = cordinates[index][1];
+    buttons.push(item);
+  }
 }
 
 window.addEventListener("DOMContentLoaded", event => {
