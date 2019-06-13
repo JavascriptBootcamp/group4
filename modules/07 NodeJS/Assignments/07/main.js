f0d24d5501bc7187461902b6c0bf2eaf9941aa39
evyatar duek <front15@sqlabs.co.il>
2019-06-13 20:35:42 +0300

    add

--- modules/07 NodeJS/Assignments/07/main.js
+++ modules/07 NodeJS/Assignments/07/main.js
@@ -11,37 +11,39 @@ app.use(express.json()); // Middleware - for reading the BODY
 
 // Read
 app.get('/', (request, response) => {
-    init();
+    let folder = request.query.folder;
+
+    init(folder);
     responseJson(response, "ok");
-  
+
 });
 
 
-const zip = (source) => {
-    return new Promise( (resolve, reject) => {
-        try{
+const zip = (source, folder) => {
+    return new Promise((resolve, reject) => {
+        try {
             fs.createReadStream(source) //read the stream
-            .pipe(zlib.createGzip()) // zip the file
-            .pipe(fs.createWriteStream(path.join('backup', `${source}.gz`))) // create the zip file
-            .on('finish', () => ({message: 'File was zipped!!!'}));
+                .pipe(zlib.createGzip()) // zip the file
+                .pipe(fs.createWriteStream(path.join(folder, `${source}.gz`))) // create the zip file
+                .on('finish', () => ({ message: 'File was zipped!!!' }));
 
         }
-        catch(ex) {
+        catch (ex) {
             return new Error(ex);
         }
-    } )
+    })
 }
 
 
 
-const init =  async () => {
-    const fileName = 'book1.csv';
-    try{
+const init = async (folder) => {
+    const fileName = 'students.csv';
+    try {
         // resolve
-        const result = await zip(fileName);
+        const result = await zip(fileName, folder);
         console.log(result.message);
     }
-    catch(ex) {
+    catch (ex) {
         // reject
         console.error(ex);
     }
