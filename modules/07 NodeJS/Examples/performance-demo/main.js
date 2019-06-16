17a3094c316185ef7d6f9a4d88e4beaaeb50240a
JavascriptBootcamp <shai.mesisterano@agilesparks.com>
2019-06-16 19:44:58 +0300

    add examples

--- modules/07 NodeJS/Examples/performance-demo/main.js
+++ modules/07 NodeJS/Examples/performance-demo/main.js
@@ -15,16 +15,20 @@ app.use(function(req, res, next) {
     const before = performance.now();
     console.log(`BEGIN(${res.locals.requestId})`);
 
+
+    next();
+
+
     res.on("finish", function() {
         const after = performance.now();
         console.log(`END(${res.locals.requestId})`);
+        console.log(`TOOK: ${after - before}`);
     });
-    
-    next();
 });
 
 app.use( (req, res) => {
     console.log("Third middleware");
+    res.end("OK");
 } ) 
 
 app.listen(8888);
