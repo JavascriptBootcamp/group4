7a27392a72480719edf4f74a44455945b3ccc6c4
danielbr08@gmail.com <daniel.brosh@sapiens.com>
2019-06-19 15:23:22 +0300

    07_q_7

--- modules/07 NodeJS/Assignments/07/main.js
+++ modules/07 NodeJS/Assignments/07/main.js
@@ -1,47 +1,16 @@
-const fs = require('fs');
+const express = require("express");
 const zlib = require('zlib');
+const fs = require('fs');
 const path = require('path');
-const express = require('express');
-
 const app = express();
 
-const fileName = "students.csv";
-
-const zip = (source) => {
-    const filePath = path.join("backup", `${source}.gz`);
-
-    return new Promise((resolve, reject) => {
-        try {
-            fs.createReadStream(source) //read the stream
-                .pipe(zlib.createGzip()) // zip the file
-                .pipe(fs.createWriteStream(filePath)) // create the zip file
-                .on('finish', () => resolve({ message: 'File was zipped!!!' }));
-        }
-        catch (ex) {
-            reject(ex);
-        }
-    })
-}
-// Read
-app.get('/', async (request, response) => {
-    try {
-        const result = await zip(fileName);
-        console.log(result.message);
-        response.json({ mess: result.message });
-    } catch (error) {
-        console.log(error);
-        response.json({ mess: error });
-    }
+const csvFilePath = "students.csv";
+const csvBuckupFilePath = path.join("backup",`${csvFilePath}.gz`);
+app.get('/',(req,res)=>{
+    fs.createReadStream(csvFilePath)
+   .pipe(zlib.createGzip())
+   .pipe(fs.createWriteStream(csvBuckupFilePath))
+   .on("finish",()=>console.log("Backup successful!"));
 });
 
-// Download
-app.get('/download', (request, response) => {
-    const filePath = path.join("backup", `${fileName}.gz`);
-
-    response.setHeader('Content-Disposition', 'attachment; filename=\"' + filePath);
-
-    fs.createReadStream(filePath)
-    .pipe(response);
-})
-
-app.listen(8500, () => console.log("server is running in port 8500"));
+app.listen(5000,()=>console.log("Server is running in port 5000!"));
