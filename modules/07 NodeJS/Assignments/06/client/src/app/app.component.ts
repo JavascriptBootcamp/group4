cb19f39148a4de0f2d03c52f055711be21140932
aviyot <aviyot@gmail.com>
2019-06-19 22:12:30 +0300

    working..

---
+++ modules/07 NodeJS/Assignments/06/client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "client";
+
+  getResult(files) {
+    console.log(files);
+    const url = "http://localhost:8000/file";
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(files),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(response => console.log("Success:", JSON.stringify(response)))
+      .catch(error => console.error("Error:", error));
+  }
+}
---
+++ modules/07 NodeJS/Assignments/06/server/main.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+//const stream = fs.createReadStream(path.resolve(__dirname, "documents"));
+
+app.use(express.json()); // Middleware - for reading the BODY
+app.use(cors());
+
+const matchingFileNames = [];
+
+app.use("/file", (req, res, next) => {
+  console.log(req.body.files);
+  next();
+});
+app.post("/file", (req, res) => {
+  //const writeStream = fs.createWriteStream("./found_files.txt");
+  const serchFiles = req.body;
+  testFolder = path.resolve(__dirname, "documents");
+  fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+      serchFiles.files.forEach(searchFile => {
+        if (file === searchFile) {
+          matchingFileNames.push(file);
+          fs.appendFile("./found_files.txt", `${file} `, function(err) {
+            if (err) {
+              return console.log(err);
+            }
+          });
+          //req.pipe(writeStream);
+        }
+      });
+    });
+  });
+  console.log(matchingFileNames);
+  res.json(matchingFileNames);
+});
+/* fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    }); */
+/*  stream.on("data", function(data) {
+    const chunk = data.toString();
+    console.log(chunk); */
+/*   }); */
+
+/*  const serchFiles = req.body;
+  res.send(serchFiles.file1); */
+
+//I get error  - the order must being (req , res)
+/* app.get("/", (res, req) => {
+  res.send("hello world");
+});
+ */
+app.listen(8000, () => console.log("server is running in port 8000"));
