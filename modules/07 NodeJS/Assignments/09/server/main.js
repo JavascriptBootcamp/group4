f0ae4ad65ae5303a9e5d40590f48d8c4a2de8819
JavascriptBootcamp <shai.mesisterano@agilesparks.com>
2019-06-20 21:04:55 +0300

    add examples

--- modules/07 NodeJS/Assignments/09/server/main.js
+++ modules/07 NodeJS/Assignments/09/server/main.js
@@ -1,137 +1,137 @@
 const express = require('express');
 const app = express();
-const cors  = require('cors'); 
+const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
 
 const data = {
-    songs:[
+    songs: [
         {
-            id:1,
-            title:"xx",
-            singer:"xxxxxx",
-            words:"xxxxxxxxxxx xxx xxxxx",
-    },
-    {
-        id:2,
-        title:"yyy",
-        singer:"yyyyyy",
-        words:"yyyyy yyyy",
-    },
-    {
-        id:3,
-        title:"zzz",
-        singer:"zzzzzzzz",
-        words:"zzzzzz xxzzx xxxzzzzzzzzzzzxx",
-    },
-    {
-        id:4,
-        title:"eeeee",
-        singer:"eeeeeeeeee",
-        words:"eeeeeeeeeeeeeeee xxx xxxxx",
-    },
-    {
-        id:5,
-        title:"rrrrrrr",
-        singer:"rrrrrrrrrrr",
-        words:"rrrrrrrrrrrrrrrrr xxx xxxxx",
-    }
-]
+            id: 1,
+            title: "xx",
+            singer: "xxxxxx",
+            words: "xxxxxxxxxxx xxx xxxxx",
+        },
+        {
+            id: 2,
+            title: "yyy",
+            singer: "yyyyyy",
+            words: "yyyyy yyyy",
+        },
+        {
+            id: 3,
+            title: "zzz",
+            singer: "zzzzzzzz",
+            words: "zzzzzz xxzzx xxxzzzzzzzzzzzxx",
+        },
+        {
+            id: 4,
+            title: "eeeee",
+            singer: "eeeeeeeeee",
+            words: "eeeeeeeeeeeeeeee xxx xxxxx",
+        },
+        {
+            id: 5,
+            title: "rrrrrrr",
+            singer: "rrrrrrrrrrr",
+            words: "rrrrrrrrrrrrrrrrr xxx xxxxx",
+        }
+    ]
 };
 
 
 
-app.get('/songs',(req,res,next)=>{
-    
-const {sort} = req.query;
-if(sort){
-const songsArr = [...data.songs];
-songsArr.sort(compare);
-res.json(songsArr);
-}
-else
-next();
-})
+app.get('/songs', (req, res, next) => {
 
-app.get('/songs',(req,res)=>{
-    
-res.json(data["songs"]);
-    
-})
-
-
-
-
-function compare( a, b ) {
-    if ( a.title < b.title ){
-      return -1;
+    const { sort } = req.query;
+    if (sort) {
+        const songsArr = [...data.songs];
+        songsArr.sort(compare);
+        res.json(songsArr);
     }
-    if ( a.title > b.title ){
-      return 1;
+    else
+        next();
+})
+
+app.get('/songs', (req, res) => {
+
+    res.json(data["songs"]);
+
+})
+
+
+
+
+function compare(a, b) {
+    if (a.title < b.title) {
+        return -1;
+    }
+    if (a.title > b.title) {
+        return 1;
     }
     return 0;
-  }
+}
 
 
-  app.post('/song',(req,res)=>{
+app.post('/song', (req, res) => {
 
-    try{
-        const id = Math.floor(Math.random()*100000);
-        const {title,singer,words} = req.body;
-        const newSong = {id,title,singer,words}
+    try {
+        const id = Math.floor(Math.random() * 100000);
+        const { title, singer, words } = req.body;
+        const newSong = { id, title, singer, words }
         data.songs.push(newSong);
         res.status(200);
         res.json(data.songs);
     }
-    catch(ex){
+    catch (ex) {
         res.status(500);
     }
 
-  })
+})
 
-  app.delete('/song/:id',(req,res)=>{
+app.delete('/song/:id', (req, res) => {
 
-    try{
-    const {id} = req.params;
-    const index = getIndex(data.songs,+id);
-    data.songs.splice(index,1);
-    res.status(200);
-    res.json(data.songs);
+    try {
+        const { id } = req.params;
+        const index = getIndex(data.songs, +id);
+        data.songs.splice(index, 1);
+        res.status(200);
+        res.json(data.songs);
     }
-    catch(ex){
+    catch (ex) {
         res.status(500);
     }
-  })
+})
 
 
-  function getIndex(arr,identifyer){
+function getIndex(arr, identifyer) {
 
-   return arr.findIndex(s=>s.id===identifyer);
+    return arr.findIndex(s => s.id === identifyer);
 
-  }
+}
 
 
-  app.put('/song/:id',(req,res)=>{
+app.put('/song/:id', (req, res) => {
 
-    try{
-        const {id} = req.params;
-        const {title,singer,words} = req.body;
-        const newSong = {id,title,singer,words}
-        const index = getIndex(data.songs,+id);
+    try {
+        const { id } = req.params;
+        const { title, singer, words } = req.body;
+        const newSong = { id, title, singer, words }
+        const index = getIndex(data.songs, +id);
         data.songs[index] = newSong;
         res.status(200);
         res.json(data.songs);
     }
-    catch(ex){
+    catch (ex) {
         res.status(500);
     }
-   
-
-  })
 
 
-  app.listen(8000,()=>{console.log("server is running!")})
+})
 
 
- 
+app.listen(8000, () => { console.log("server is running!") })
+
+
+
