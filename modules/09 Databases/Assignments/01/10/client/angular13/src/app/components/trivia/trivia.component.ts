c766cfa07c2c55daf3bba3f26609077df11e0ddf
gabybuchnik <gabybuchnik@gmail.com>
2019-06-30 11:53:47 +0300

    database1

---
+++ modules/09 Databases/Assignments/01/10/client/angular13/src/app/components/trivia/trivia.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from 'src/app/service/questions.service';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-trivia',
+  templateUrl: './trivia.component.html',
+  styleUrls: ['./trivia.component.css']
+})
+export class TriviaComponent implements OnInit {
+  submit: boolean;
+  submitScore: boolean;
+  topTen: boolean;
+  scores: any[];
+  constructor(public questionsService: QuestionsService) {
+    this.submit = false;
+    this.submitScore = false;
+    this.scores = [];
+  }
+
+  ngOnInit() {
+  }
+  async onSubmit(form: NgForm) {
+    this.questionsService.checkAnswers(form);
+    this.topTen = await this.questionsService.isTopTen();
+    this.submit = true;
+  }
+  sendScore(form: NgForm) {
+    this.submitScore = true;
+    const obj = { name: form.value.name, score: this.questionsService.correctAnswers };
+    fetch("http://localhost:8000/score", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(obj)
+    })
+    .then(() =>{
+      fetch("http://localhost:8000/highscore").then(res => res.json()).then(data => this.scores = data);
+    })
+  }
+}
