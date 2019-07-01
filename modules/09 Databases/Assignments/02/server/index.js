9fe4fad96d6157e2131c84748e697d60a49518ae
gabybuchnik <gabybuchnik@gmail.com>
2019-07-01 09:31:01 +0300

    finish server side

--- modules/09 Databases/Assignments/02/server/index.js
+++ modules/09 Databases/Assignments/02/server/index.js
@@ -16,16 +16,29 @@ MongoClient.connect(url, (err, dbs) => {
 
     app.post('/car', (req, res) => {
         const { lisence, manufacturer, model, year, km, price } = req.body;
-        const obj = { lisence, manufacturer, model, year, km, price };
-        collection.insertOne(obj);
-        responseWithStatus(200, res, "car added");
+        if (!lisence || !manufacturer || !model || !year || !km || !price) return responseWithStatus(500, res, "Missing Data");
+        try {
+            const obj = { lisence, manufacturer, model, year, km, price };
+            collection.insertOne(obj);
+            responseWithStatus(200, res, "car added");
+        } catch (error) {
+            responseWithStatus(500, res, "something wrong..." + error);
+        }
+
     });
 
-    app.get('/car', async (req, res) => {
+    app.get('/car', async (req, res, next) => {
         const { lisence } = req.query;
-        if (!lisence) responseWithStatus(500, res, "Missing Data");
-        const car = await collection.find({ lisence }).toArray();
-        responseWithStatus(200, res, car);
+        if (!lisence) next();
+        else {
+            try {
+                const car = await collection.find({ lisence }).toArray();
+                responseWithStatus(200, res, car);
+            } catch (error) {
+                responseWithStatus(500, res, "something wrong..." + error);
+            }
+
+        }
     });
 
     app.get('/cars', async (req, res, next) => {
@@ -34,8 +47,13 @@ MongoClient.connect(url, (err, dbs) => {
             next();
         }
         else {
-            const cars = await collection.find({ year: { $gt: yearStart, $lt: yearEnd } }).toArray();
-            responseWithStatus(200, res, cars);
+            try {
+                const cars = await collection.find({ year: { $gte: yearStart, $lte: yearEnd } }).toArray();
+                responseWithStatus(200, res, cars);
+            } catch (error) {
+                responseWithStatus(500, res, "something wrong..." + error);
+            }
+
         }
     });
 
@@ -46,27 +64,51 @@ MongoClient.connect(url, (err, dbs) => {
             next();
         }
         else {
-            const cars = await collection.find({ manufacturer }).toArray();
-            cars.forEach((item) => {
-                if (item.manufacturer === manufacturer) {
-                    models.push({ model: item.model });
-                }
-            });
-            responseWithStatus(200, res, models);
+            try {
+                const cars = await collection.find({ manufacturer }).toArray();
+                cars.forEach((item) => {
+                    if (item.manufacturer === manufacturer) {
+                        models.push({ model: item.model });
+                    }
+                });
+                responseWithStatus(200, res, models);
+            } catch (error) {
+                responseWithStatus(500, res, "something wrong..." + error);
+            }
+
         }
     });
 
     app.get('/cars', async (req, res) => {
         const { model } = req.query;
         const carsModelAndPrices = [];
-        if (!model) responseWithStatus(500, res, "Missing Data");
-        const cars = await collection.find({ model }).toArray();
-        cars.forEach((item) => {
-            if (item.model === model) {
-                carsModelAndPrices.push({ model: item.model, price: item.price });
-            }
-        });
-        responseWithStatus(200, res, carsModelAndPrices);
+        if (!model) return responseWithStatus(500, res, "Missing Data");
+        try {
+            const cars = await collection.find({ model }).toArray();
+            cars.forEach((item) => {
+                if (item.model === model) {
+                    carsModelAndPrices.push({ model: item.model, price: item.price });
+                }
+            });
+            responseWithStatus(200, res, carsModelAndPrices);
+        } catch (error) {
+            responseWithStatus(500, res, "something wrong..." + error);
+        }
+
+    });
+
+    app.get('/car', async (req, res) => {
+        const { lisenceOne, lisenceTwo } = req.query;
+        if (!lisenceOne || !lisenceTwo) return responseWithStatus(500, res, "Missing Data");
+        try {
+            const car1 = await collection.find({ lisence: lisenceOne }).toArray();
+            const car2 = await collection.find({ lisence: lisenceTwo }).toArray();
+            const car = Number(car1[0].price) > Number(car2[0].price) ? car1[0] : car2[0];
+            responseWithStatus(200, res, car);
+        } catch (error) {
+            responseWithStatus(500, res, "something wrong..." + error);
+        }
+
     });
 
 });
