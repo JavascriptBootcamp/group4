4502c309866d20d479d22665c8909b0383ae8c49
gabybuchnik <gabybuchnik@gmail.com>
2019-07-04 16:20:09 +0300

    database5

--- modules/09 Databases/Assignments/05/client/answer56/app.js
+++ modules/09 Databases/Assignments/05/client/answer56/app.js
@@ -1,15 +1,7 @@
-var tm1 = new Thumbnails("tm1", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqsw9q1gKQhk3M3krgRtn9vCgYaKDvQD_GgJdL-y-jNiJRLbgC");
-var tm2 = new Thumbnails("tm2", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKIN7mbSbgvFXj2PXLzjXbC_NnVVOBoqHMgv9p8tw1KZuV-ngJGw");
-var tm3 = new Thumbnails("tm3", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ82DqniYRM-q3SKuf-ziG1u9Fc4gGuD8O8exGkZazb6SurMllSmw");
-var tm4 = new Thumbnails("tm4", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ4mFHp-axQIxtrs-yV_DRePjPn6C2tCT7EFnVAJUayne7ZfaG");
-var tm5 = new Thumbnails("tm5", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf6ltr-w2OSqtk-nh4eVIpndpQncu3r6M3EfiVVR5s5cuEGVOk");
-var tm6 = new Thumbnails("tm6", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa0mrze3yjNQjIjRtfxVHYeRrDZwNrNV3Lq4FHq_l-Z1eKWrm71w");
-var tm7 = new Thumbnails("tm7", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI7arfj-4VZnRls18ELncSRFACyqBCMcJLjZcxyCmr9DVC2qNrbA");
-var tm8 = new Thumbnails("tm8", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfgKOVXDVIY2Yaq0a0rDLWoV0ks0zcQVDa6aCQVJVGcD6llupPPw");
-var tm9 = new Thumbnails("tm9", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsQN-mcmQVIi2HLnAZhvJ-JCYZ1Yf7EtNlA1wVBf8gAYkh6T2r");
-var tm10 = new Thumbnails("tm10", "https://www.barraquer.com/wp-content/uploads/2017/07/el-ojo-del-surfista-p-08-2017.jpg");
-var obj = { tm1, tm2, tm3, tm4, tm5, tm6, tm7, tm8, tm9, tm10 }
-var intervalName;
+var tm1 = new Thumbnails("tm1", "blank.png");
+var tm2 = new Thumbnails("tm2", "blank.png");
+var tm3 = new Thumbnails("tm3", "blank.png");
+var obj = { tm1, tm2, tm3 };
 
 var data = getData();
 if (data) {
@@ -23,24 +15,7 @@ else {
 
 function initialThumbnails() {
     var thumbnails = document.getElementById("thumbnails");
-    var ol1 = document.createElement("div");
-    var ol2 = document.createElement("div");
-    var thumbnails = document.getElementById("thumbnails");
-    var thumbnailsCarousle = document.createElement("div");
-    var arrowLeftDiv = document.createElement("div");
-    var arrowRightDiv = document.createElement("div");
-    ol1.id = "overlayer1";
-    ol2.id = "overlayer2";
-    thumbnails.appendChild(ol1);
-    thumbnails.appendChild(ol2);
-    arrowLeftDiv.className = "arrow arrow-left";
-    arrowLeftDiv.onmouseover = scrollOver;
-    arrowLeftDiv.onmouseleave = scrollLeave;
-    arrowRightDiv.onmouseover = scrollOver;
-    arrowRightDiv.onmouseleave = scrollLeave;
-    arrowRightDiv.className = "arrow arrow-right";
-    thumbnailsCarousle.id = "thumbnails-carousle";
-    thumbnails.appendChild(arrowLeftDiv);
+    thumbnails.innerHTML = "";
     for (var key in obj) {
         if (obj.hasOwnProperty(key)) {
             var img = document.createElement("img");
@@ -50,42 +25,8 @@ function initialThumbnails() {
                 var ins = obj[key].name;
                 obj[ins].setimage(e);
             };
-            thumbnailsCarousle.appendChild(img);
+            thumbnails.appendChild(img);
         }
-    }
-    thumbnails.appendChild(thumbnailsCarousle);
-    thumbnails.appendChild(arrowRightDiv);
-}
-function scrollLeave(e) {
-    clearInterval(intervalName);
-}
-function scrollOver(e) {
-    var arrow = e.target.className;
-    var thumbnailsCarousle = document.getElementById("thumbnails-carousle");
-    var pos = thumbnailsCarousle.style.left;
-    var computedStyle = window.getComputedStyle(thumbnailsCarousle, null);
-    pos = computedStyle.left;
-    pos = pos.split("px");
-    pos = pos[0];
-    intervalName = setInterval(frame, 5);
-    function frame() {
-        if (arrow === "arrow arrow-left") {
-            if (pos <= -200) {
-                clearInterval(intervalName);
-            } else {
-                pos--;
-                thumbnailsCarousle.style.left = pos + 'px';
-            }
-        }
-        else {
-            if (pos >= 500) {
-                clearInterval(intervalName);
-            } else {
-                pos++;
-                thumbnailsCarousle.style.left = pos + 'px';
-            }
-        }
-
     }
 }
 function setNextImg(e) {
@@ -93,7 +34,7 @@ function setNextImg(e) {
     var numberImg = Img.getAttribute("imgnumber");
     var newImg = "tm" + (parseInt(numberImg.substring(2)) - 1);
     if (parseInt(numberImg.substring(2)) === 1) {
-        newImg = "tm10";
+        newImg = "tm3";
     }
     Img.src = obj[newImg].src;
     Img.setAttribute("imgnumber", newImg);
@@ -102,7 +43,7 @@ function setPrevImg(e) {
     var Img = document.getElementById("big-picture-img");
     var numberImg = Img.getAttribute("imgnumber");
     var newImg = "tm" + (parseInt(numberImg.substring(2)) + 1);
-    if (parseInt(numberImg.substring(2)) === 10) {
+    if (parseInt(numberImg.substring(2)) === 3) {
         newImg = "tm1";
     }
     Img.src = obj[newImg].src;
@@ -123,6 +64,26 @@ Thumbnails.prototype.setimage = function (e) {
 function saveBigPic() {
     var pic = document.getElementById("big-picture-img");
     saveData(pic.src);
+}
+
+async function getAlbum(e) {
+    e.preventDefault();
+    let album = document.getElementById('select-album').selectedIndex;
+    album = +album + 1;
+    const res = await fetch('http://localhost:8000/album?id=' + album);
+    const data = await res.json();
+    loadPicturesFromAlbum(data.message);
+}
+
+function loadPicturesFromAlbum(data) {
+    let number = 1;
+    data.forEach(element => {
+        let name = "tm" + number;
+        let tm = new Thumbnails(name, element.src);
+        number++;
+        obj[name] = tm;
+    });
+    initialThumbnails();
 }
 
 window.onbeforeunload = function (event) {
