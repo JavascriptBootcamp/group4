6b808b1cb1defbfd26b534761687f9b0c28731bd
OfekArbeli <front11@sqlabs.co.il>
2019-07-04 20:50:31 +0300

    fixed small issue

--- modules/10 Advanced Angular/Assignments/01/answer1/src/app/month-to-color.directive.ts
+++ modules/10 Advanced Angular/Assignments/01/answer1/src/app/month-to-color.directive.ts
@@ -19,40 +19,52 @@ export class MonthToColorDirective implements AfterViewInit{
   checkColorOfMonth(){
     switch(this.month){
       case 'January':{
-        this.renderer.setStyle(this.el.nativeElement, "color", 'red');
+        this.renderer.setStyle(this.el.nativeElement, "color", "red");
+        break;
       }
       case 'February':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'blue');
+        break;
       }
       case 'March':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'green');
+        break;
       }
       case 'April':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'black');
+        break;
       }
       case 'May':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'purple');
+        break;
       }
       case 'June':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'yellow');
+        break;
       }
       case 'July':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'pink');
+        break;
       }
       case 'August':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'gray');
+        break;
       }
       case 'September':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'brown');
+        break;
       }
       case 'October':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'cyan');
+        break;
       }
       case 'November':{
         this.renderer.setStyle(this.el.nativeElement, "color", 'beige');
+        break;
       }
       case 'December':{
         this.renderer.setStyle(this.el.nativeElement, "color", "orange");
+        break;
       }
     }
   }
