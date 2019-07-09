4b6db39480832d3691a9fd794ac8ae58497a82c4
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-07-09 11:05:57 +0300

    Delete small-img.directive.spec.ts

--- modules/09 Databases/Assignments/05/q1656/src/app/directives/small-img.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SmallImgDirective } from './small-img.directive';
-
-describe('SmallImgDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SmallImgDirective();
-    expect(directive).toBeTruthy();
-  });
-});
