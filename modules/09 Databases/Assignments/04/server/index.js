c7e406e9f7f07bc2cd9afd3efab41993abf1dc59
David Lev <davidlevman@gmail.com>
2019-07-17 23:06:31 +0300

    DB answer 4

---
+++ modules/09 Databases/Assignments/04/server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql');  
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+const mysqlConnection = mysql.createConnection({
+    host      : 'localhost',
+    user      : 'root',
+    password  : '',
+    database : 'instush'
+});
+
+
+mysqlConnection.connect( (err, result) => {
+    if(!err) console.log('MySQL Connection succeed');
+    else console.log('MySQL Connection failed \n Error: ' + JSON.stringify(err, undefined, 2));
+});
+
+
+app.post('/addLike', (req, resp) => {
+    const {imageSrc, likesNum} =  req.body;
+
+    const sql = `INSERT INTO app_counts (image_src, likes_num) 
+                 VALUES (${imageSrc}, ${likesNum})`;
+
+    resp.status(200).send("Like has been added")
+})
+
+
+port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server running on port " + port));
