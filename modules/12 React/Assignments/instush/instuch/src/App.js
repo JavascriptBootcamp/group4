561a04485a67c8d1591a25088cf18b7843483c17
aviyot <aviyot@gmail.com>
2019-07-29 14:27:40 +0300

    final

--- modules/12 React/Assignments/instush/instuch/src/App.js
+++ modules/12 React/Assignments/instush/instuch/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Img from "./Img.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="Header"> Instush </div>
+      <Img />
       <div className="Fotter"> All Rights Reserved</div>
     </div>
   );
