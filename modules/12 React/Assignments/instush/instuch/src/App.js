083499a2e0a67919edc1ab5e749d149f2e163a6d
aviyot <aviyot@gmail.com>
2019-07-29 12:29:12 +0300

    react

---
+++ modules/12 React/Assignments/instush/instuch/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header"> Instush </div>
+      <div className="Fotter"> All Rights Reserved</div>
+    </div>
+  );
+}
+
+export default App;
